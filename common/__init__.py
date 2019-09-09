59e2420795dc504c439dfb74c32ab2f99759ef94
jb3-2 <jonas.bieri@gmail.com>
2019-09-09 14:32:36 +0200

    Specify remote path

---
+++ common/__init__.py
@@ -0,0 +1,18 @@
+from ftplib import FTP
+import os
+
+# Upload file to FTP Server
+def upload_ftp(filename, server, user, password, remote_path):
+    # change to desired directory first
+    currdir = os.getcwd()
+    relpath, filename_nopath = os.path.split(filename)
+    os.chdir(relpath)
+    ftp = FTP(server)
+    ftp.login(user, password)
+    ftp.cwd(remote_path)
+    print ("Uploading " + filename_nopath + " to FTP server directory " + remote_path + '...')
+    with open(filename_nopath, 'rb') as f:
+        ftp.storbinary('STOR %s' % filename_nopath, f)
+    ftp.quit()
+    os.chdir(currdir)
+    return
