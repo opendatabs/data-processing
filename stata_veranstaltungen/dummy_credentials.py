ac0d2bb66c35b2208d951701cbf931c975401c04
jb3-2 <jonas.bieri@gmail.com>
2020-03-23 11:23:13 +0100

    Ignore csv files

---
+++ stata_veranstaltungen/dummy_credentials.py
@@ -0,0 +1,11 @@
+ftp_server = 'FTP-GATEWAY'
+ftp_user = 'USERNAME@data-bs.ch@data-bs.ch'
+ftp_pass = 'DUMMYPASSWORD'
+
+path_orig = '/code/data-processing/aue_umweltlabor/data_orig/'
+path_work = '/code/data-processing/aue_umweltlabor/data/'
+
+api_key = 'dummy_ods_api_key'
+
+proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
+
---
+++ stata_veranstaltungen/etl.sh
@@ -0,0 +1,4 @@
+cd /code/data-processing || exit
+# python3 -m aue_umweltlabor.etl no_file_copy
+python3 -m aue_umweltlabor.etl
+
