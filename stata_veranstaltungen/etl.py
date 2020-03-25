754d5874e6d7015858f53a016ce728569eb98610
jb3-2 <jonas.bieri@gmail.com>
2020-03-25 10:12:06 +0100

    Update ODS dataset uid

--- stata_veranstaltungen/etl.py
+++ stata_veranstaltungen/etl.py
@@ -5,4 +5,4 @@ filename = 'veranstaltungen.csv'
 common.upload_ftp(credentials.path_work + filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'veranstaltungen')
 
 print('Publishing ODS dataset...')
-common.publish_ods_dataset('da_0dpdmd', credentials)
+common.publish_ods_dataset('da_so5l56', credentials)
