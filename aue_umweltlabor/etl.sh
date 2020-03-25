1a7e0cb09d5c7673f1b4f6b6db8dff77c67fdccd
jb3-2 <jonas.bieri@gmail.com>
2020-03-25 15:00:32 +0100

    Optimize pip install

--- aue_umweltlabor/etl.sh
+++ aue_umweltlabor/etl.sh
@@ -1,5 +1,5 @@
 cd /code/data-processing || exit
+# pip freeze > /code/data-processing/aue_umweltlabor/requirements-in-docker.txt
 # python3 -m aue_umweltlabor.etl no_file_copy
 python3 -m aue_umweltlabor.etl
-pip freeze > /code/data-processing/requirements-in-docker.txt
 
