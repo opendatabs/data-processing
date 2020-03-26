6ada4c5233516aa7ff66393919dc077132209c7a
jb3-2 <jonas.bieri@gmail.com>
2020-03-26 12:00:59 +0100

    Remove ods_upload

--- stata_veranstaltungen/etl.py
+++ stata_veranstaltungen/etl.py
@@ -4,5 +4,7 @@ from stata_veranstaltungen import credentials
 filename = 'veranstaltungen.csv'
 common.upload_ftp(credentials.path_work + filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'veranstaltungen')
 
-print('Publishing ODS dataset...')
-common.publish_ods_dataset('da_so5l56', credentials)
+# print('Publishing ODS dataset...')
+# common.publish_ods_dataset('da_so5l56', credentials)
+
+print('Job successful!')
