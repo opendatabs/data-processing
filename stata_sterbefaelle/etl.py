f111f8c8997a7a6675059d9a0b6900e150ed1ff9
jb3-2 <jonas.bieri@gmail.com>
2020-04-15 14:43:43 +0200

    Fix typo

--- stata_sterbefaelle/etl.py
+++ stata_sterbefaelle/etl.py
@@ -1,5 +1,5 @@
 import common
-from stata_veranstaltungen import credentials
+from stata_sterbefaelle import credentials
 
 filename = 'sterbefaelle.csv'
 common.upload_ftp(credentials.path_work + filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'bevoelkerung')
