481151c6ac57b6cf2809a9d2b58b3cfeaf416243
jb3-2 <jonas.bieri@gmail.com>
2020-05-13 17:48:30 +0200

    Add ETL code

---
+++ euroairport/dummy-credentials.py
@@ -0,0 +1,10 @@
+ftp_server = 'FTP-GATEWAY'
+ftp_user = 'USERNAME@data-bs.ch@data-bs.ch'
+ftp_pass = 'DUMMYPASSWORD'
+
+path = '/code/data-processing/euroairport/data/'
+data_orig = 'FILENAME.XLSX'
+data_export = 'FILENAME.CSV'
+
+proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
+
