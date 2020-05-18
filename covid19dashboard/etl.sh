b4c92972e89bc093c52fcda3998f670e926cecf4
jb3-2 <jonas.bieri@gmail.com>
2020-05-18 21:08:45 +0200

    Fix module name

--- covid19dashboard/etl.sh
+++ covid19dashboard/etl.sh
@@ -1,4 +1,4 @@
 cd /code/data-processing || exit
 # pip freeze > /code/data-processing/covid19dashboard/requirements-in-docker.txt
-python3 -m covid_19_dashboard.etl
+python3 -m covid19dashboard.etl
 
