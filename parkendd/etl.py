5eff0cce991e45d985f4adac5c38fdd8bdfb4ee0
jb3-2 <jonas.bieri@gmail.com>
2020-05-28 12:09:23 +0200

    Create new columns to resemble existing ods dataset

--- parkendd/etl.py
+++ parkendd/etl.py
@@ -20,12 +20,19 @@ resp_file.write(pretty_resp)
 resp_file.close()
 
 csv_file_name = f'{credentials.path}csv/parkendd-{str(datetime.now()).replace(":", "")}.csv'
-print(f'Adding timestamp to each lot, then saving as {csv_file_name}...')
+print(f'Processing data...')
 for lot in parsed['lots']:
     lot['last_downloaded'] = parsed['last_downloaded']
     lot['last_updated'] = parsed['last_updated']
 
 normalized = pd.json_normalize(parsed, record_path='lots')
+normalized['title'] = "Parkhaus " + normalized['name']
+normalized['id2'] = normalized['id'].str.replace('baselparkhaus', '')
+normalized['link'] = "https://www.parkleitsystem-basel.ch/parkhaus/" + normalized['id2']
+normalized['description'] = 'Anzahl freie Parkplätze: ' + normalized['free'].astype(str)
+normalized['published'] = normalized['last_downloaded']
+
+print(f'Saving as {csv_file_name}...')
 normalized.to_csv(csv_file_name, index=False)
 
 print('Uploading json and csv file to ftp server...')
