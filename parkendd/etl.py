c822d41e2a4f92162ff3aa3316f330ec09775009
jb3-2 <jonas.bieri@gmail.com>
2020-05-28 16:28:08 +0200

    Create files for lots and values separately

--- parkendd/etl.py
+++ parkendd/etl.py
@@ -9,17 +9,16 @@ apiUrl = 'https://api.parkendd.de/Basel'
 print(f'Getting latest data from {apiUrl}...')
 response = requests.get(apiUrl)
 
-json_file_name = f'{credentials.path}json/parkendd-{str(datetime.now()).replace(":", "")}.json'
-print(f'Parsing json and saving to {json_file_name}...')
-parsed = json.loads(response.text)
-#print(response.json())
-pretty_resp = json.dumps(parsed, indent=4, sort_keys=True)
-#print(pretty_resp)
-resp_file = open(json_file_name, 'w+')
-resp_file.write(pretty_resp)
-resp_file.close()
 
-csv_file_name = f'{credentials.path}csv/parkendd-{str(datetime.now()).replace(":", "")}.csv'
+print(f'Parsing json...')
+parsed = json.loads(response.text)
+pretty_resp = json.dumps(parsed, indent=4, sort_keys=True)
+# json_file_name = f'{credentials.path}json/parkendd-{str(datetime.now()).replace(":", "")}.json'
+# resp_file = open(json_file_name, 'w+')
+# resp_file.write(pretty_resp)
+# resp_file.close()
+
+lots_file_name = f'{credentials.path}csv/lots/parkendd-lots.csv'
 print(f'Processing data...')
 for lot in parsed['lots']:
     lot['last_downloaded'] = parsed['last_downloaded']
@@ -32,12 +31,17 @@ normalized['link'] = "https://www.parkleitsystem-basel.ch/parkhaus/" + normalize
 normalized['description'] = 'Anzahl freie Parkplätze: ' + normalized['free'].astype(str)
 normalized['published'] = normalized['last_downloaded']
 
-print(f'Saving as {csv_file_name}...')
-normalized.to_csv(csv_file_name, index=False)
+print(f'Creating lots file and saving as {lots_file_name}...')
+lots = normalized[['address','id','lot_type','name','total','last_downloaded','last_updated','coords.lat','coords.lng','title','id2','link','published']]
+lots.to_csv(lots_file_name, index=False)
 
-print('Uploading json and csv file to ftp server...')
-common.upload_ftp(csv_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'parkendd/csv')
-common.upload_ftp(json_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'parkendd/json')
+values_file_name = f'{credentials.path}csv/values/parkendd-{str(datetime.now()).replace(":", "")}.csv'
+print(f'Creating values file and saving as {values_file_name}...')
+values = normalized[['published', 'free', 'id', 'id2']]
+values.to_csv(values_file_name, index=False)
+
+common.upload_ftp(lots_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'parkendd/csv/lots')
+common.upload_ftp(values_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'parkendd/csv/values')
 
 print('Job successful!')
 
