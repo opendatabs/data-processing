18d9bebe1890bccf3ecc538ea15c87782347d345
jb3-2 <jonas.bieri@gmail.com>
2020-06-11 14:09:39 +0200

    Update for Docker usage

--- gva_geodatenshop/etl.sh
+++ gva_geodatenshop/etl.sh
@@ -1,5 +1,12 @@
+cd /code/data-processing || exit
+# pip freeze > /code/data-processing/gva_geodatenshop/requirements-in-docker.txt
+python3 -m gva_geodatenshop.etl
+
+
 # scl enable rh-python36 bash
-cd ~/dev/workspace/GVA-Geodatenshop
-. ../../venv/GVA-Geodatenshop/bin/activate
-python3 etl.py
-deactivate
+#cd ~/dev/workspace/GVA-Geodatenshop
+#. ../../venv/GVA-Geodatenshop/bin/activate
+#python3 etl.py
+#deactivate
+
+
