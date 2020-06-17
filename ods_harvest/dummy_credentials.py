1a696a570c382955d435d7c945c78cfa2e76aa0d
jb3-2 <jonas.bieri@gmail.com>
2020-06-17 15:29:00 +0200

    Add ods_harvest module

---
+++ ods_harvest/dummy_credentials.py
@@ -0,0 +1,4 @@
+api_key = 'dummy_ods_api_key'
+proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
+ods_user = 'USERNAME@BS.CH'
+ods_password = 'PASSWORD'
---
+++ ods_harvest/etl.py
@@ -0,0 +1,44 @@
+from ods_harvest import credentials
+import time
+import requests
+
+import sys
+ods_harvester_ids = sys.argv[1].split(',')
+
+
+def wait_for_idle(harvester_id):
+    while True:
+        print(f'Checking status of harvester "{harvester_id}"...')
+        resp = requests.get(f'https://basel-stadt.opendatasoft.com/api/management/v2/harvesters/{harvester_id}/', auth=(credentials.ods_user, credentials.ods_password), proxies={'https': credentials.proxy})
+        handle_http_errors(resp)
+        status = resp.json()['status']
+        print(f'Harvester "{harvester_id}" is "{status}".')
+        if status == 'idle':
+            break
+        else:
+            print('Waiting 5 seconds before trying again...')
+            time.sleep(5)
+
+
+def handle_http_errors(resp):
+    if resp.status_code == 200:
+        print(f'ODS command successful: HTTP Code "{resp.status_code}"')
+    else:
+        print(f'Problem with ODS command: HTTP Code "{resp.status_code}"')
+        raise RuntimeError('Problem with OpenDataSoft Management API: ' + resp.text)
+
+
+for harv_id in ods_harvester_ids:
+    wait_for_idle(harv_id)
+    print(f'Sending harvester "{harv_id}" the "start" signal...')
+    response = requests.put(f'https://basel-stadt.opendatasoft.com/api/management/v2/harvesters/{harv_id}/start/', auth=(credentials.ods_user, credentials.ods_password), proxies={'https': credentials.proxy})
+    handle_http_errors(response)
+    wait_for_idle(harv_id)
+
+    print(f'Sending harvester "{harv_id}" the "publish" signal...')
+    response = requests.put(f'https://basel-stadt.opendatasoft.com/api/management/v2/harvesters/{harv_id}/publish/', auth=(credentials.ods_user, credentials.ods_password), proxies={'https': credentials.proxy})
+    handle_http_errors(response)
+    wait_for_idle(harv_id)
+
+
+print('Job successful!')
