a7a6c0d763eb5a4d3de40f4a0526ce054fd15a73
jb3-2 <jonas.bieri@gmail.com>
2020-06-22 11:48:37 +0200

    Add columns with diff data between last and current row

--- covid19dashboard/etl.py
+++ covid19dashboard/etl.py
@@ -6,6 +6,9 @@ import common
 sourcefile = 'https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv'
 print(f'Reading date from {sourcefile}...')
 df = pd.read_csv(sourcefile)
+print('Getting rid of unnecessary columns...')
+df.drop(columns=['time', 'source', 'ncumul_tested', 'new_hosp', 'current_vent'], inplace=True)
+
 print('Calculating date range...')
 df.date = pd.to_datetime(df.date)
 date_range = pd.date_range(start=df.date.min(), end=df.date.max())
@@ -18,11 +21,17 @@ df_filled = pd.DataFrame(columns=df.columns)
 for canton in cantons:
     df_canton = df[df.abbreviation_canton_and_fl == canton].sort_values(by='date')
     df_canton_filled = df_canton.set_index('date').reindex(date_range).ffill().reset_index().rename(columns={'index': 'date'})
-    df_filled = df_filled.append(df_canton_filled, ignore_index=True)
 
-print('Getting rid of unnecessary columns and rows with empty date...')
-df_filled.drop(columns=['time', 'source', 'ncumul_tested', 'new_hosp', 'current_vent'], inplace=True)
-df_filled.dropna(subset=['abbreviation_canton_and_fl'], inplace=True)
+    print('Getting rid of rows with empty date...')
+    df_canton_filled.dropna(subset=['abbreviation_canton_and_fl'], inplace=True)
+
+    print('Calculating differences between rows in new columns...')
+    df_canton_diff = df_canton_filled.drop(columns=['abbreviation_canton_and_fl']).diff()
+    df_canton_filled['ndiff_conf'] = df_canton_diff.ncumul_conf
+    df_canton_filled['ndiff_released'] = df_canton_diff.ncumul_released
+    df_canton_filled['ndiff_deceased'] = df_canton_diff.ncumul_deceased
+
+    df_filled = df_filled.append(df_canton_filled, ignore_index=True)
 
 filename = os.path.join(credentials.path, credentials.filename)
 print(f'Exporting data to {filename}')
