32789020d17ad8767b3f2657453468931589d51b
jb3-2 <jonas.bieri@gmail.com>
2020-06-24 15:08:28 +0200

    Adapt to new source file format, create stations csv file

--- aue_schall/etl.py
+++ aue_schall/etl.py
@@ -16,9 +16,10 @@ print(f'Changing to remote dir {credentials.ftp_read_remote_path}...')
 ftp.cwd(credentials.ftp_read_remote_path)
 print('Retrieving list of files...')
 for file_name, facts in ftp.mlsd():
-    if date_string in file_name:
-        print(f'File {file_name} has current date ({date_string}) in its filename. Parsing station name from filename...')
-        station = file_name.replace(f'_{date_string}.csv', '').replace('airmet_auebs_', '')
+    if date_string in file_name and 'OGD' in file_name:
+        print(f'File {file_name} has "OGD" and current date ({date_string}) in its filename. '
+              f'Parsing station name from filename...')
+        station = file_name.replace(f'_{date_string}.csv', '').replace('airmet_auebs_', '').replace('_OGD', '')
         stations.append(station)
 
         print(f'Downloading {file_name} for station {station}...')
@@ -30,24 +31,29 @@ for file_name, facts in ftp.mlsd():
 ftp.quit()
 
 dfs = {}
-all_data = pd.DataFrame(columns=['station_id', 'timestamp', 'dB'])
+all_data = pd.DataFrame(columns=['LocalDateTime', 'Value', 'Latitude', 'Longitude', 'EUI'])  #, 'station_id', 'timestamp'])
 print('Reading csv files into data frames...')
 for station in stations:
     print(f"Reading {local_files[station]}...")
-    df = pd.read_csv(local_files[station], sep=';', skiprows=1, na_filter=False)
-    df.columns = ['timestamp_text', 'dB']
+    df = pd.read_csv(local_files[station], sep=';', na_filter=False)
     print(f'Calculating ISO8601 time string...')
-    df['timestamp'] = pd.to_datetime(df.timestamp_text, format='%d.%m.%Y %H:%M').dt.tz_localize('Europe/Zurich')
+    df['timestamp'] = pd.to_datetime(df.LocalDateTime, format='%d.%m.%Y %H:%M').dt.tz_localize('Europe/Zurich')
     df.set_index('timestamp', drop=False, inplace=True)
-    df = df[['timestamp', 'dB']]
     df['station_id'] = station
     all_data = all_data.append(df, sort=True)
     dfs[station] = df
 
-all_data = all_data[['station_id', 'timestamp', 'dB']]
+all_data = all_data[['station_id', 'timestamp', 'Value', 'Latitude', 'Longitude', 'EUI', 'LocalDateTime']]
 today_data_file = os.path.join(credentials.path, f'schall_{date_string}.csv')
 print(f"Exporting today's data to {today_data_file}...")
 all_data.to_csv(today_data_file, index=False)
 
+print('Creating stations file from current data file...')
+df_stations = all_data.drop_duplicates(['EUI'])[['station_id', 'Latitude', 'Longitude', 'EUI']]
+stations_file = os.path.join(credentials.path, 'stations/stations.csv')
+print(f'Exporting stations file to {stations_file}...')
+df_stations.to_csv(stations_file, index=False)
+
+common.upload_ftp(stations_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'{credentials.ftp_remote_path}/stations')
 common.upload_ftp(today_data_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, credentials.ftp_remote_path)
 print('Job successful!')
