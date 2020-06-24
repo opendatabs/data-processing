bed60fc0e607833482353f89d7089c9720fcd349
jb3-2 <jonas.bieri@gmail.com>
2020-06-24 17:25:11 +0200

    Read yesterday's data too in order not to forget last measures before midnight

--- aue_schall/etl.py
+++ aue_schall/etl.py
@@ -2,11 +2,11 @@ import pandas as pd
 import os
 from ftplib import FTP
 import common
-from datetime import datetime
+from datetime import datetime, timedelta
 from aue_schall import credentials
 
-date_string = datetime.today().strftime('%Y%m%d')
-
+today_string = datetime.today().strftime('%Y%m%d')
+yesterday_string = datetime.strftime(datetime.today() - timedelta(1), '%Y%m%d')
 local_files = {}
 stations = []
 
@@ -16,10 +16,15 @@ print(f'Changing to remote dir {credentials.ftp_read_remote_path}...')
 ftp.cwd(credentials.ftp_read_remote_path)
 print('Retrieving list of files...')
 for file_name, facts in ftp.mlsd():
-    if date_string in file_name and 'OGD' in file_name:
-        print(f'File {file_name} has "OGD" and current date ({date_string}) in its filename. '
+    # If we only use today's date we might lose some values just before midnight yesterday.
+    if (today_string in file_name or yesterday_string in file_name) and 'OGD' in file_name:
+        print(f"File {file_name} has 'OGD' and (yesterday's or today's date) in its filename. "
               f'Parsing station name from filename...')
-        station = file_name.replace(f'_{date_string}.csv', '').replace('airmet_auebs_', '').replace('_OGD', '')
+        station = file_name\
+            .replace(f'_{today_string}.csv', '') \
+            .replace(f'_{yesterday_string}.csv', '') \
+            .replace('airmet_auebs_', '') \
+            .replace('_OGD', '')
         stations.append(station)
 
         print(f'Downloading {file_name} for station {station}...')
@@ -31,7 +36,7 @@ for file_name, facts in ftp.mlsd():
 ftp.quit()
 
 dfs = {}
-all_data = pd.DataFrame(columns=['LocalDateTime', 'Value', 'Latitude', 'Longitude', 'EUI'])  #, 'station_id', 'timestamp'])
+all_data = pd.DataFrame(columns=['LocalDateTime', 'Value', 'Latitude', 'Longitude', 'EUI'])
 print('Reading csv files into data frames...')
 for station in stations:
     print(f"Reading {local_files[station]}...")
@@ -44,8 +49,8 @@ for station in stations:
     dfs[station] = df
 
 all_data = all_data[['station_id', 'timestamp', 'Value', 'Latitude', 'Longitude', 'EUI', 'LocalDateTime']]
-today_data_file = os.path.join(credentials.path, f'schall_{date_string}.csv')
-print(f"Exporting today's data to {today_data_file}...")
+today_data_file = os.path.join(credentials.path, f'schall_{today_string}.csv')
+print(f"Exporting yesterday's and today's data to {today_data_file}...")
 all_data.to_csv(today_data_file, index=False)
 
 print('Creating stations file from current data file...')
@@ -54,6 +59,8 @@ stations_file = os.path.join(credentials.path, 'stations/stations.csv')
 print(f'Exporting stations file to {stations_file}...')
 df_stations.to_csv(stations_file, index=False)
 
-common.upload_ftp(stations_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'{credentials.ftp_remote_path_stations}')
-common.upload_ftp(today_data_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, credentials.ftp_remote_path_vals)
+common.upload_ftp(stations_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                  f'{credentials.ftp_remote_path_stations}')
+common.upload_ftp(today_data_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                  credentials.ftp_remote_path_vals)
 print('Job successful!')
