427a600e79aef90ce03505ebb0e3b986b65edc06
jb3-2 <jonas.bieri@gmail.com>
2020-06-25 13:35:53 +0200

    Merge files, set correct timezone

---
+++ bafu_hydrodaten/dummy-credentials.py
@@ -0,0 +1,16 @@
+ftp_server = 'FTP-GATEWAY'
+ftp_user = 'USERNAME@data-bs.ch@data-bs.ch'
+ftp_pass = 'DUMMYPASSWORD'
+ftp_remote_dir = 'some/folder'
+
+path = '/code/data-processing/aue_schall/data/'
+
+proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
+
+ftp_read_server = 'FTP-GATEWAY'
+ftp_read_user = 'USERNAME@data-bs.ch@data-bs.ch'
+ftp_read_pass = 'DUMMYPASSWORD'
+ftp_read_remote_path = 'some/folder'
+
+abfluss_file = 'HBCHa2289_10_10_02.txt'
+pegel_file = 'HBCHa2289_02_00_02.txt'
--- bafu_hydrodaten/etl.py
+++ bafu_hydrodaten/etl.py
@@ -1,4 +1,6 @@
 from ftplib import FTP
+from datetime import datetime
+import numpy as np
 import os
 import pandas as pd
 import common
@@ -19,20 +21,26 @@ for file in files:
         ftp.retrbinary(f"RETR {file}", f.write)
 ftp.quit()
 
-print('Loading and processing data...')
-abfluss_df = pd.read_csv(local_files[0], sep='\t', skiprows=4, names=['datum', 'zeit', 'abfluss', 'intervall', 'qualitaet', 'messart'], header=None)
-pegel_df = pd.read_csv(local_files[1], sep='\t', skiprows=4, names=['datum', 'zeit', 'pegel', 'intervall', 'qualitaet', 'messart'], header=None)
+print('Loading data...')
+abfluss_df = pd.read_csv(local_files[0], sep='\t', skiprows=4, names=['datum', 'zeit', 'abfluss', 'intervall', 'qualitaet', 'messart'], usecols=['datum', 'zeit', 'abfluss', 'intervall'], header=None)
+pegel_df = pd.read_csv(local_files[1], sep='\t', skiprows=4, names=['datum', 'zeit', 'pegel', 'intervall', 'qualitaet', 'messart'], usecols=['datum', 'zeit', 'pegel', 'intervall'], header=None)
 
-abfluss_df = abfluss_df[abfluss_df.intervall == 5]
-print('Replacing 24:00 with 23:59...')
-abfluss_df.loc[abfluss_df.zeit == '24:00', 'zeit'] = '23:59'
-abfluss_df['timestamp'] = pd.to_datetime(abfluss_df.datum + ' ' + abfluss_df.zeit, format='%d.%m.%Y %H:%M').dt.tz_localize('Europe/Zurich')
-print('Adding a minute to entries with time 23:59...')
-# todo: Add a minute to entries with zeit == 23:59
-abfluss_df.to_csv(os.path.join(local_path, 'abfluss.csv'), index=False)
+print(f'Merging data...')
+merged_df = abfluss_df.merge(pegel_df, on=['datum', 'zeit', 'intervall'], how='outer')
 
-# Merge into a single df, then export to csv
+print('Processing data...')
+merged_df = merged_df.loc[merged_df.intervall == 5]
+print(f'Fixing entries with zeit == 24:00...')
+# Replacing 24:00 with 23:59
+merged_df.loc[merged_df.zeit == '24:00', 'zeit'] = '23:59'
+# Time is given in MEZ (UTC+1) thus use 'Etc/GMT-1' according to https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# merged_df['timestamp'] = pd.to_datetime(merged_df.datum + ' ' + merged_df.zeit, format='%d.%m.%Y %H:%M').dt.tz_localize('Europe/Zurich')
+merged_df['timestamp'] = pd.to_datetime(merged_df.datum + ' ' + merged_df.zeit, format='%d.%m.%Y %H:%M').dt.tz_localize('Etc/GMT-1')
+# Adding a minute to entries with time 23:59 then replacing 23:59 with 24:00 again
+merged_df.timestamp = np.where(merged_df.zeit != '23:59', merged_df.timestamp, merged_df.timestamp + pd.Timedelta(minutes=1))
+merged_df.zeit = np.where(merged_df.zeit == '23:59', '24:00', merged_df.zeit)
+merged_filename = os.path.join(local_path, f'2289_pegel_abfluss_{datetime.today().strftime("%Y-%m-%dT%H-%M-%S")}.csv')
+merged_df.to_csv(merged_filename, index=False)
 
-# for local_file in local_files:
-#     common.upload_ftp(local_file, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, credentials.ftp_remote_dir)
-# print('Job successful!')
+common.upload_ftp(merged_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, credentials.ftp_remote_dir)
+print('Job successful!')
