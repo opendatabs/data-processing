5e873f757b4233eeb026b59819179ea85a2633aa
jb3-2 <jonas.bieri@gmail.com>
2020-07-02 12:08:27 +0200

    Outline alternative plan

--- tba_wildedeponien/etl.py
+++ tba_wildedeponien/etl.py
@@ -68,8 +68,9 @@ if r.status_code == 200:
         print('Dropping unnecessary columns...')
         gdf_wv_bez.drop(columns=['index_wv', 'index_bez', 'wov_id_points'], inplace=True)
 
-        # todo: Find nearest Wohnviertel / Bezirk of points outside ofthos shapes (Rhein, Outside of BS territory)
+        # todo: Find nearest Wohnviertel / Bezirk of points outside of those shapes (Rhein, Outside of BS territory)
         # e.g. see https://gis.stackexchange.com/a/342489
+        # Alternative: For points where rastered coords have no Bezirk: use Bezirk of original coords
 
         timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
         file_path = os.path.join(credentials.path, f'{timestamp}_{credentials.filename}')
