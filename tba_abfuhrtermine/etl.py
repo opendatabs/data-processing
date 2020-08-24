aefbf5e17517661cb921eb89a163f509765fca7c
jb3-2 <jonas.bieri@gmail.com>
2020-08-24 14:46:31 +0200

    Simplify code, remove duplicate comment

--- tba_abfuhrtermine/etl.py
+++ tba_abfuhrtermine/etl.py
@@ -2,7 +2,5 @@ import common
 import os
 from tba_abfuhrtermine import credentials
 
-file_path = os.path.join(credentials.path, credentials.filename)
-print(f'Uploading {file_path} to FTP server...')
-common.upload_ftp(file_path, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'tba/abfuhrtermine')
+common.upload_ftp(os.path.join(credentials.path, credentials.filename), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'tba/abfuhrtermine')
 print('Job successful!')
