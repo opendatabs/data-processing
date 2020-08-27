a2efdc164342dfcee6c08a90b356c33d478de1a1
jb3-2 <jonas.bieri@gmail.com>
2020-08-27 13:24:50 +0200

    Update folder name

--- bag_coronavirus_tests/etl.py
+++ bag_coronavirus_tests/etl.py
@@ -8,6 +8,6 @@ print(f'Searching for files matching "{search_string}"...')
 files = glob.glob(search_string)
 print(f'Found {len(files)} matching files. Uploading to FTP server...')
 for file in files:
-    common.upload_ftp(os.path.join(credentials.path, file), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'BAG_Coronavirus_Tests')
+    common.upload_ftp(os.path.join(credentials.path, file), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'bag_coronavirus_tests')
 
 print(f'Job successful!')
