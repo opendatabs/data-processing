14d7cb2662696b966448d7d74185f1abe6e02252
jb3-2 <jonas.bieri@gmail.com>
2020-08-28 10:45:41 +0200

    Upload files sorted by filename

--- bag_coronavirus_tests/etl.py
+++ bag_coronavirus_tests/etl.py
@@ -7,7 +7,7 @@ search_string = os.path.join(credentials.path, '*CovidTests_BS.xlsx')
 print(f'Searching for files matching "{search_string}"...')
 files = glob.glob(search_string)
 print(f'Found {len(files)} matching files. Uploading to FTP server...')
-for file in files:
+for file in sorted(files):
     common.upload_ftp(os.path.join(credentials.path, file), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'bag_coronavirus_tests')
 
 print(f'Job successful!')
