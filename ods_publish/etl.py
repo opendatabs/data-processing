2c64e2fde8bd2eeca4651d03df213f4f60a31ab5
jb3-2 <jonas.bieri@gmail.com>
2020-09-27 14:48:42 +0200

    Skip waiting time after last dataset, reduce waiting time to 30 s

--- ods_publish/etl.py
+++ ods_publish/etl.py
@@ -5,11 +5,12 @@ import time
 
 import sys
 ods_dataset_uids = sys.argv[1].split(',')
-
+count = len(ods_dataset_uids)
 print('Publishing ODS datasets...')
-for datasetuid in ods_dataset_uids:
-    common.publish_ods_dataset(datasetuid, credentials)
-    print('Waiting 1 minute before sending next publish request to ODS...')
-    time.sleep(60)
+for i, dataset_uid in enumerate(ods_dataset_uids):
+    common.publish_ods_dataset(dataset_uid, credentials)
+    if i < (count-1):
+        print('Waiting 30 seconds before sending next publish request to ODS...')
+        time.sleep(30)
 
 print('Job successful!')
