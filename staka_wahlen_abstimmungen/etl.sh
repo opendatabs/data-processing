ee4c20d81cf368e6108a3c25142d2b79a0a311df
jb3-2 <jonas.bieri@gmail.com>
2020-11-19 17:16:00 +0100

    Fix typo

--- staka_wahlen_abstimmungen/etl.sh
+++ staka_wahlen_abstimmungen/etl.sh
@@ -2,4 +2,4 @@ cd /code/data-processing || exit
 # pip freeze > /code/data-processing/staka_wahlen_abstimmungen/requirements-in-docker.txt
 
 # python3 -m staka_wahlen_abstimmungen.etl_details && python3 -m staka_wahlen_abstimmungen.etl_kennzahlen
-python3 -m python3 -m staka_wahlen_abstimmungen.etl_kennzahlen
+python3 -m staka_wahlen_abstimmungen.etl_kennzahlen
