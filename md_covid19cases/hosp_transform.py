9a838751340a0f4e6b56a45e01cd11922ff1c46e
jb3-2 <jonas.bieri@gmail.com>
2020-12-09 18:02:41 +0100

    Use rename columns to match openZH/covid_19 dataset

--- md_covid19cases/hosp_transform.py
+++ md_covid19cases/hosp_transform.py
@@ -12,23 +12,23 @@ def parse_data_file(file_id):
 
 
 df0 = parse_data_file(0)
-df0['Total'] = df0.sum(axis=1, skipna=True, numeric_only=True)
+df0['current_hosp'] = df0.sum(axis=1, skipna=True, numeric_only=True)
 
 df1 = parse_data_file(1)
-df1['Total_non_resident'] = df1[credentials.hosp_df1_total_non_resident_columns].sum(axis=1, skipna=True, numeric_only=True)
-df1['Total_resident'] = df1[credentials.hosp_df1_total_resident_columns]
+df1['current_hosp_non_resident'] = df1[credentials.hosp_df1_total_non_resident_columns].sum(axis=1, skipna=True, numeric_only=True)
+df1['current_hosp_resident'] = df1[credentials.hosp_df1_total_resident_columns]
 
 df2 = parse_data_file(2)
-df2['Total_ips'] = df2[credentials.hosp_df2_total_ips_columns].sum(axis=1, skipna=True, numeric_only=True)
+df2['current_icu'] = df2[credentials.hosp_df2_total_ips_columns].sum(axis=1, skipna=True, numeric_only=True)
 
 
 print(f'Merging datasets...')
 dfs = [df0, df1, df2]
 df_merged = reduce(lambda left,right: pd.merge(left, right, how='outer', on='Datum'), dfs)
 print(f'Reformatting date...')
-df_merged['Datum'] = pd.to_datetime(df_merged['Datum'], format='%d-%m-%Y', errors='coerce')
+df_merged['date'] = pd.to_datetime(df_merged['Datum'], format='%d-%m-%Y', errors='coerce')
 print(f'Filtering columns...')
-df_public = df_merged[['Datum', 'Total', 'Total_non_resident', 'Total_resident', 'Total_ips']]
+df_public = df_merged[['date', 'current_hosp', 'current_hosp_resident', 'current_hosp_non_resident', 'current_icu']]
 
 export_filename = os.path.join(credentials.export_path,credentials.export_filename_hosp)
 print(f'Exporting merged dataset to file {export_filename}...')
