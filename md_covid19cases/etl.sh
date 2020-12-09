c38d1c8d21e3d218f4eed2a37a59297a257207fa
jb3-2 <jonas.bieri@gmail.com>
2020-12-09 18:03:09 +0100

    Add hosp_extract and hosp_transform python modules

--- md_covid19cases/etl.sh
+++ md_covid19cases/etl.sh
@@ -1,4 +1,6 @@
 cd /code/data-processing || exit
 # pip freeze > /code/data-processing/md_covid19cases/requirements-in-docker.txt
 python3 -m md_covid19cases.etl_covid_faelle_detail && python3 -m md_covid19cases.etl_covid_faelle
+# python3 -m md_covid19cases.hosp_extract && python3 -im md_covid19cases.hosp_transform
+
 
