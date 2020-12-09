3eeb6d663b469694be7a5eaf7e2e141cb27236ca
jb3-2 <jonas.bieri@gmail.com>
2020-12-09 13:23:50 +0100

    Scrape hosp data from official source

---
+++ md_covid19cases/hosp_extract.py
@@ -0,0 +1,44 @@
+from md_covid19cases import credentials
+import common
+import requests
+import os
+from bs4 import BeautifulSoup
+
+# use session to retain cookie infos
+session = requests.Session()
+
+# extract necessary info from the login form
+login_form_url = credentials.hosp_domain + credentials.hosp_url_path
+print(f'Getting content of login form at {login_form_url}...')
+resp_loginform = session.get(login_form_url)
+soup_login = BeautifulSoup(resp_loginform.content, 'html.parser')
+# print(soup_login.prettify())
+action_url = soup_login.find('form').get('action')
+inputs = soup_login.find_all('input')
+token = soup_login.find_all(attrs={"name": "csrfmiddlewaretoken"})[0].get('value')
+next_url = soup_login.find_all(attrs={"name": "next"})[0].get('value')
+action_url = soup_login.find(id='login-form').get('action')
+# print(f'Cookies: {resp_loginform.cookies}')
+
+login_form_action_url = credentials.hosp_domain + action_url
+print(f'Posting login form to {login_form_action_url}...')
+payload = dict(username=credentials.hosp_username,
+               password=credentials.hosp_password,
+               csrfmiddlewaretoken=token,
+               next=next_url)
+
+req_spital_bs = session.post(login_form_action_url, data=payload, headers=dict(Referer=login_form_url))
+soup_spital_bs = BeautifulSoup(req_spital_bs.content, 'html.parser')
+# print(soup_spital_bs.prettify())
+
+for data_spec in credentials.hosp_data_files:
+    print(f'Retrieving data from {data_spec["id"]}...')
+    data_from_html = soup_spital_bs.find(id=data_spec['id']).text
+    # print(f'{data_spec["id"]}: {data_from_html}')
+    export_file_path = os.path.join(credentials.export_path, data_spec['filename'])
+    print(f'Saving data to file {export_file_path}...')
+    f = open(export_file_path, 'w')
+    f.write(data_from_html)
+    f.close()
+
+
---
+++ md_covid19cases/hosp_transform.py
@@ -0,0 +1,43 @@
+from md_covid19cases import credentials
+import common
+import os
+import pandas as pd
+from functools import reduce
+
+
+def parse_data_file(file_id):
+    filename = os.path.join(credentials.export_path, credentials.hosp_data_files[file_id]['filename'])
+    print(f'reading file {filename} into dataframe...')
+    return pd.read_csv(filename)
+
+
+df0 = parse_data_file(0)
+df0['Total'] = df0.sum(axis=1, skipna=True, numeric_only=True)
+
+df1 = parse_data_file(1)
+df1['Total_non_resident'] = df1[credentials.hosp_df1_total_non_resident_columns].sum(axis=1, skipna=True, numeric_only=True)
+df1['Total_resident'] = df1[credentials.hosp_df1_total_resident_columns]
+
+df2 = parse_data_file(2)
+df2['Total_ips'] = df2[credentials.hosp_df2_total_ips_columns].sum(axis=1, skipna=True, numeric_only=True)
+
+
+print(f'Merging datasets...')
+dfs = [df0, df1, df2]
+df_merged = reduce(lambda left,right: pd.merge(left, right, how='outer', on='Datum'), dfs)
+print(f'Reformatting date...')
+df_merged['Datum'] = pd.to_datetime(df_merged['Datum'], format='%d-%m-%Y', errors='coerce')
+print(f'Filtering columns...')
+df_public = df_merged[['Datum', 'Total', 'Total_non_resident', 'Total_resident', 'Total_ips']]
+
+export_filename = os.path.join(credentials.export_path,credentials.export_filename_hosp)
+print(f'Exporting merged dataset to file {export_filename}...')
+df_public.to_csv(export_filename, index=False)
+
+common.upload_ftp(export_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'md/covid19_cases')
+print('Job successful!')
+
+
+
+
+
