d907cce9463a7dc1332168f1a0b36435f2a84732
jb3-2 <jonas.bieri@gmail.com>
2020-12-16 16:39:46 +0100

    Adapt to new source data schema

--- covid19bs/etl_bs.py
+++ covid19bs/etl_bs.py
@@ -4,47 +4,44 @@ import os
 import common
 import numpy as np
 
-
 print(f'Starting processing python script {__file__}...')
-hosp_url = 'https://data.bs.ch/explore/dataset/100109/download/?format=csv'
-print(f'Reading hosp data from {hosp_url}...')
-df_hosp = pd.read_csv(hosp_url, sep=';')
-print(f'Keeping only necessary hosp columns...')
-df_hosp = df_hosp[['date', 'current_hosp', 'current_hosp_resident', 'current_hosp_non_resident', 'current_icu']]
 
-sourcefile = os.path.join(credentials.path_orig, credentials.filename_faelle)
-print(f'Reading case data from {sourcefile}...')
-df_cases = pd.read_csv(sourcefile, sep=';')
+pub_file = os.path.join(credentials.path_orig, credentials.filename_pub_date)
+print(f'Reading data from {pub_file}...')
+df_pubdate = pd.read_csv(pub_file, sep=';')
+print(f'Renaming columns to match openZH dataset...')
+df_pubdate = df_pubdate.rename(columns={
+    'datum': 'date',
+    'meldezeit': 'time',
+    'publizierte_neue_faelle_kum': 'ncumul_conf',
+    'hospitalisierte_bs': 'current_hosp_resident',
+    'hospitalisierte_icu': 'current_icu',
+    'hospitalisierte_total': 'current_hosp'
+    })
+
+test_file = os.path.join(credentials.path_orig, credentials.filename_test_date)
+print(f'Reading data from {test_file}...')
+df_testdate = pd.read_csv(test_file, sep=';')
 print(f'Keeping only certain case columns...')
-df_cases = df_cases[['publikationsdatum', 'meldezeit', 'datum', 'faelle_bs_kum', 'hospitalisierte_total', 'hospitalisierte_icu',
-         'erholt_bs', 'gestorbene_bs_kum', 'isoliert_bs', 'quarantaene_kontakt_bs',
-         'quarantaene_reise_bs', 'quarantaene_bs', 'hospitalisierte_bs']]
+df_testdate = df_testdate.rename(columns={
+    'datum': 'test_date',
+    'erholt_bs': 'ncumul_released',
+    'gestorbene_bs_kum': 'ncumul_deceased',
+    'isoliert_bs': 'current_isolated',
+    'quarantaene_bs': 'current_quarantined_total',
+    'quarantaene_reise_bs': 'current_quarantined_riskareatravel',
+    'quarantaene_kontakt_bs': 'current_quarantined'
+})
+print(f'Calculating pub date...')
+df_testdate['date'] = (pd.to_datetime(df_testdate['test_date']) + pd.Timedelta(days=1)).dt.strftime('%Y-%m-%d')
 
-print(f'Left-joining case and hosp datasets, then dropping date from hosp...')
-df_merged = pd.merge(df_cases, df_hosp, left_on=['publikationsdatum'], right_on=['date'], how='left')
-df_merged = df_merged.drop(columns=['date'])
-
-print(f'Renaming columns to match openZH file...')
-df_merged = df_merged.rename(columns={
-                        'publikationsdatum': 'date',
-                        'meldezeit': 'time',
-                        'datum': 'test_date',
-                        'faelle_bs_kum': 'ncumul_conf',
-                        #'hospitalisierte_total': 'current_hosp',
-                        #'hospitalisierte_icu': 'current_icu',
-                        'erholt_bs': 'ncumul_released',
-                        'gestorbene_bs_kum': 'ncumul_deceased',
-                        'isoliert_bs': 'current_isolated',
-                        'quarantaene_kontakt_bs': 'current_quarantined',
-                        'quarantaene_reise_bs': 'current_quarantined_riskareatravel',
-                        'quarantaene_bs': 'current_quarantined_total'
-                        #'hospitalisierte_bs': 'current_hosp_resident'
-                        })
+print(f'Joining test and pub datasets...')
+df_merged = pd.merge(df_pubdate, df_testdate, on=['date'], how='outer')
 
 print(f'Calculating columns...')
 df_merged['abbreviation_canton_and_fl'] = 'BS'
 df_merged['source'] = 'https://www.gesundheit.bs.ch'
-# df_cases['current_hosp_non_resident'] = df_cases['current_hosp'] - df_cases['current_hosp_resident']
+df_merged['current_hosp_non_resident'] = df_merged['current_hosp'] - df_merged['current_hosp_resident']
 # values for some columns are currently not available
 df_merged['ncumul_tested'] = np.nan
 df_merged['new_hosp'] = np.nan
@@ -60,18 +57,19 @@ df_merged['ndiff_confirmed_non_resident'] = np.nan
 
 print(f'Change column order...')
 df_merged = df_merged[['date', 'time', 'abbreviation_canton_and_fl', 'ncumul_tested', 'ncumul_conf', 'new_hosp', 'current_hosp',
-         'current_icu', 'current_vent', 'ncumul_released', 'ncumul_deceased', 'source', 'current_isolated',
-         'current_quarantined', 'ncumul_confirmed_non_resident', 'current_hosp_non_resident',
-         'current_quarantined_riskareatravel', 'current_quarantined_total', 'current_hosp_resident',
-         'ndiff_conf', 'ndiff_released', 'ndiff_deceased', 'ndiff_confirmed_non_resident', 'test_date']]
+        'current_icu', 'current_vent', 'ncumul_released', 'ncumul_deceased', 'source', 'current_isolated',
+        'current_quarantined', 'ncumul_confirmed_non_resident', 'current_hosp_non_resident',
+        'current_quarantined_riskareatravel', 'current_quarantined_total',
+        'current_hosp_resident', 'ndiff_conf', 'ndiff_released', 'ndiff_deceased', 'ndiff_confirmed_non_resident', 'test_date']]
 
-# print(f'Removing test_date column for the moment...')
-# df_merged = df_merged.drop(columns=['test_date'])
+print(f'Removing test_date column for the moment...')
+df_merged = df_merged.drop(columns=['test_date'])
 
-print(f'Keeping only latest record...')
+print(f'Keeping only top row...')
 df_latest = df_merged.head(1)
 latest_date = df_latest['date'][0]
-export_filename = os.path.join(credentials.path, credentials.filename).replace('.csv', '_' + latest_date + '.csv')
+
+export_filename = os.path.join(credentials.path, credentials.filename).replace('.csv', f'_{latest_date}.csv')
 print(f'Exporting csv to {export_filename}')
 df_latest.to_csv(export_filename, index=False)
 
