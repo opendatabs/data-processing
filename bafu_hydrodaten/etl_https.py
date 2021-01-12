53311645fa4e360cddba3b58b2bd08937079ef8c
jb3-2 <jonas.bieri@gmail.com>
2021-01-12 12:07:54 +0100

    Add latest data to ODS via push API

--- bafu_hydrodaten/etl_https.py
+++ bafu_hydrodaten/etl_https.py
@@ -40,8 +40,26 @@ merged_df['timestamp'] = pd.to_datetime(merged_df.datum + ' ' + merged_df.zeit, 
 # Adding a minute to entries with time 23:59 then replacing 23:59 with 24:00 again
 merged_df.timestamp = np.where(merged_df.zeit != '23:59', merged_df.timestamp, merged_df.timestamp + pd.Timedelta(minutes=1))
 merged_df.zeit = np.where(merged_df.zeit == '23:59', '24:00', merged_df.zeit)
+
 merged_filename = os.path.join(local_path, f'2289_pegel_abfluss_{datetime.today().strftime("%Y-%m-%d")}.csv')
 merged_df.to_csv(merged_filename, index=False)
 
 common.upload_ftp(merged_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, credentials.ftp_remote_dir)
+
+print(f'Retrieving latest record from ODS...')
+r = requests.get('https://data.bs.ch/api/records/1.0/search/?dataset=100089&q=&rows=1&sort=timestamp')
+r.raise_for_status()
+latest_ods_value = r.json()['records'][0]['fields']['timestamp']
+
+print(f'Filtering data after {latest_ods_value} for submission to ODS via realtime API...')
+realtime_df = merged_df[merged_df['timestamp'] > latest_ods_value]
+
+print(f'Pushing {realtime_df.timestamp.count()} rows to ODS realtime API...')
+for index, row in realtime_df.iterrows():
+    timestamp_text = row.timestamp.strftime('%Y-%m-%dT%H:%M:%S%z')
+    payload = {'timestamp': timestamp_text, 'pegel': row.pegel, 'abfluss': row.abfluss}
+    print(f'Pushing row {index} with with the following data to ODS: {payload}')
+    r = requests.post(credentials.ods_live_push_api_url, json=payload)
+    r.raise_for_status()
+
 print('Job successful!')
