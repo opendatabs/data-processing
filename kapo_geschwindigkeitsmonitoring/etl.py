77d2129f57aa76fbabbe49bb5084c84b7122d588
jb3-2 <jonas.bieri@gmail.com>
2021-01-14 17:24:53 +0100

    Add module kapo_geschwindigkeitsmonitoring

--- kapo_geschwindigkeitsmonitoring/etl.py
+++ kapo_geschwindigkeitsmonitoring/etl.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import glob
 import pandas.io.sql as psql
 import os
 from ftplib import FTP
@@ -17,7 +18,22 @@ filename = os.path.join(credentials.path, credentials.filename)
 print(f'Exporting data to {filename}...')
 df.to_csv(filename, index=False)
 
-print(f'{df}')
+metadata_file_path = os.path.join(
+    credentials.detail_data_q_drive,
+    df['Verzeichnis'].iloc[0].replace('Q:\\', '').replace('Ka', 'KA'))
+print(f'{metadata_file_path}')
+
+data_search_string = os.path.join(metadata_file_path, "**/*.txt")
+raw_files = glob.glob(data_search_string)
+print(f'*.txt Contents of directory {data_search_string}: {raw_files}')
+dfs = []
+for file in raw_files:
+    file = file.replace('\\', '/')
+    raw_df = pd.read_table(file, skiprows=6, header=0, names=['Geschwindigkeit', 'Zeit', 'Datum', 'Richtung ID', 'Fahrzeuglänge'])
+    dfs.append(raw_df)
+
+# print(f'{df}')
 con.close()
+
 
 print('Job successful!')
