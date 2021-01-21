bf80e5e8950a4871a1a0f00fcaee5b4085e16037
jb3-2 <jonas.bieri@gmail.com>
2021-01-21 17:01:08 +0100

    Upload tourismus data, change source path, add @common.retry decorator

--- stata_daily_upload/etl.py
+++ stata_daily_upload/etl.py
@@ -2,10 +2,19 @@ import common
 import os
 from stata_daily_upload import credentials
 
-uploads = [{'file': 'sterbefaelle.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'geburten_nach_datum.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung'}]
+uploads = [{'file': 'Bevoelkerung/sterbefaelle.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/geburten_nach_datum.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Tourismus/tourismus-daily.csv', 'dest_dir': 'tourismus'},
+           {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus'}]
+
+
+@common.retry(BrokenPipeError, tries=10, delay=10, backoff=1)
+def upload_ftp(file, dest_dir):
+    common.upload_ftp(os.path.join(credentials.path_work, file), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, dest_dir)
+
+
 for upload in uploads:
-    common.upload_ftp(os.path.join(credentials.path_work, upload['file']), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
+    upload_ftp(upload['file'], upload['dest_dir'])
 
 print('Job successful!')
