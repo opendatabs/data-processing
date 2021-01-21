e5c68cbe3ff4ee9b80500470e2b57f92c329a191
jb3-2 <jonas.bieri@gmail.com>
2021-01-21 11:52:40 +0100

    Add @retry decorator, remove tenacity dependency

--- common/__init__.py
+++ common/__init__.py
@@ -2,13 +2,58 @@ from ftplib import FTP
 import requests
 import os
 import logging
-# from tenacity import *
+import time
+from functools import wraps
 
-logger = logging.getLogger(__name__)
+
+# Source: https://github.com/saltycrane/retry-decorator/blob/master/retry_decorator.py
+# BSD license: https://github.com/saltycrane/retry-decorator/blob/master/LICENSE
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck as e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
 
 # Upload file to FTP Server
-# @retry(retry=retry_if_exception_type(BrokenPipeError), wait=wait_fixed(15), stop=stop_after_attempt(3),
-#        before_sleep=before_sleep_log(logger, logging.DEBUG))
+# Retry 4 times with 5s delay in between if BrokenPipeError is raised
+@retry(BrokenPipeError, tries=4, delay=5)
 def upload_ftp(filename, server, user, password, remote_path):
     print("Uploading " + filename + " to FTP server directory " + remote_path + '...')
     # change to desired directory first
