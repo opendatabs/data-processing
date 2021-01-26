c364c6497038656e21a65f2070e9cc9b7c1fe7ba
jb3-2 <jonas.bieri@gmail.com>
2021-01-26 12:05:53 +0100

    Add @retry to handle http ConnectionResetError

--- common/__init__.py
+++ common/__init__.py
@@ -92,9 +92,16 @@ def download_ftp(files, server, user, password, remote_path, local_path):
     return
 
 
+def http_errors_to_handle():
+    return ConnectionResetError
+
+
 # Tell Opendatasoft to (re-)publish datasets
 # How to get the dataset_uid from ODS:
 # curl --proxy https://USER:PASSWORD@PROXYSERVER:PORT -i https://data.bs.ch/api/management/v2/datasets/?where=datasetid='100001' -u username@bs.ch:password123
+
+# Retry with some delay in between if any explicitly defined error is raised
+@retry(http_errors_to_handle(), tries=6, delay=10, backoff=1)
 def publish_ods_dataset(dataset_uid, creds):
     print("Telling OpenDataSoft to reload dataset " + dataset_uid + '...')
     response = requests.put('https://data.bs.ch/api/management/v2/datasets/' + dataset_uid + '/publish', params={'apikey': creds.api_key}, proxies={'https': creds.proxy})
