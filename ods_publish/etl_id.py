0d4a9024de8855b6bc666ff0ed9d4c233083d3b9
jb3-2 <jonas.bieri@gmail.com>
2021-01-26 15:42:56 +0100

    Publish ods dataset by id instead of uid

---
+++ ods_publish/etl_id.py
@@ -0,0 +1,22 @@
+import common
+from ods_publish import credentials
+import time
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+ods_dataset_ids = sys.argv[1].split(',')
+count = len(ods_dataset_ids)
+print('Publishing ODS datasets...')
+for i, dataset_id in enumerate(ods_dataset_ids):
+    print(f'Retrieving dataset uid for id {dataset_id} from ODS...')
+    dataset_uid = common.get_ods_uid_by_id(dataset_id, credentials)
+    print(f'Received dataset uid "{dataset_uid}", sending publish API call...')
+    common.publish_ods_dataset(dataset_uid, credentials)
+    if i < (count-1):
+        print('Waiting 5 seconds before sending next publish request to ODS...')
+        time.sleep(5)
+
+print('Job successful!')
