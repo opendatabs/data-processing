8ee17a8fe20ae963c9d1cd2b0e2e122092592b9f
jb3-2 <jonas.bieri@gmail.com>
2021-01-26 15:50:29 +0100

    Fix log statement

--- ods_publish/etl_id.py
+++ ods_publish/etl_id.py
@@ -13,7 +13,7 @@ print('Publishing ODS datasets...')
 for i, dataset_id in enumerate(ods_dataset_ids):
     print(f'Retrieving dataset uid for id {dataset_id} from ODS...')
     dataset_uid = common.get_ods_uid_by_id(dataset_id, credentials)
-    print(f'Received dataset uid "{dataset_uid}", sending publish API call...')
+    print(f'Received dataset uid "{dataset_uid}"...')
     common.publish_ods_dataset(dataset_uid, credentials)
     if i < (count-1):
         print('Waiting 5 seconds before sending next publish request to ODS...')
