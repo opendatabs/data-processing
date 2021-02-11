c28b3b11983676e9aef923501c24544a0f019a44
jb3-2 <jonas.bieri@gmail.com>
2021-02-11 12:04:25 +0100

    Run etl_covid_gestorbene.py

--- md_covid19cases/etl.sh
+++ md_covid19cases/etl.sh
@@ -1,5 +1,5 @@
 cd /code/data-processing || exit
 # pip freeze > /code/data-processing/md_covid19cases/requirements-in-docker.txt
-python3 -m md_covid19cases.etl_covid_faelle_detail && python3 -m md_covid19cases.etl_covid_faelle
+python3 -m md_covid19cases.etl_covid_faelle_detail && python3 -m md_covid19cases.etl_covid_faelle && python3 -m md_covid19cases.etl_covid_gestorbene
 
 
