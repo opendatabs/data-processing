0f127978a8ea662d8f4475b940991dd80a887349
jb3-2 <jonas.bieri@gmail.com>
2021-02-11 14:29:52 +0100

    Rename columns, add more columns to match existing dataset more closely

--- md_covid19cases/etl_covid_gestorbene.py
+++ md_covid19cases/etl_covid_gestorbene.py
@@ -1,8 +1,24 @@
 from md_covid19cases import credentials
+import pandas as pd
 import common
 import os
 
 filename = os.path.join(credentials.path, credentials.filename_gestorbene)
-print(f'Uploading data to FTP server...')
-common.upload_ftp(filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'md/covid19_cases')
+print(f'Reading data from {filename}, renaming columns and performing some calculations...')
+df = pd.read_csv(filename, sep=';')
+df = df.rename(columns={'sterbe_datum': 'date_of_death',
+                        'melde_datum': 'date_of_publication',
+                        'alter': 'AgeYear',
+                        'geschlecht': 'Gender',
+                        'vorerkrankung': 'PreExistingCond',
+                        'verstorbene_kumuliert': 'ncumul_deceased'})
+df['source'] = 'https://www.gesundheit.bs.ch'
+df['Area'] = 'Canton_BS'
+df['NewDeaths'] = 1
+df['CaseNumberPerDay'] = df.groupby(['date_of_death']).cumcount() + 1
+
+export_filename = os.path.join(credentials.export_path, credentials.export_filename_gestorbene)
+print(f'Exporting data to {export_filename}...')
+df.to_csv(export_filename, index=False)
+common.upload_ftp(export_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'md/covid19_cases')
 print('Job successful!')
