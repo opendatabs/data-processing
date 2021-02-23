0cd0da194d0ba449ef30de2de6151bd25187ad8d
jb3-2 <jonas.bieri@gmail.com>
2021-02-23 11:38:17 +0100

    Add module lufthygiene_nmbs_pm25

---
+++ lufthygiene_nmbs_pm25/etl.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import common
+import urllib3
+from lufthygiene_nmbs_pm25 import credentials
+
+
+@common.retry(common.http_errors_to_handle, tries=6, delay=10, backoff=1)
+def read_data_from_url(uri):
+    return pd.read_csv(uri, sep=';', encoding='cp1252', skiprows=range(1, 2))
+
+
+url = 'https://data-bs.ch/lufthygiene/nmbs_pm25/airmet_bs_museum_pm25_aktuell.csv'
+print(f'Downloading data from {url}...')
+df = read_data_from_url(url)
+print(f'Calculating ISO8601 time string...')
+df['timestamp'] = pd.to_datetime(df.Anfangszeit, format='%d.%m.%Y %H:%M:%S').dt.tz_localize('Europe/Zurich', ambiguous='infer')
+
+print(f'Reading latest timestamp from ODS dataset...')
+urllib3.disable_warnings()
+
+r = common.requests_get('https://data.bs.ch/api/records/1.0/search/?dataset=100100&q=&rows=1&sort=anfangszeit', verify=False)
+r.raise_for_status()
+latest_ods_timestamp = r.json()['records'][0]['fields']['anfangszeit']
+print(f'Latest timestamp is {latest_ods_timestamp}.')
+print(f'Filtering data after {latest_ods_timestamp} for submission to ODS via realtime API...')
+realtime_df = df[df['timestamp'] > latest_ods_timestamp]
+print(f'Pushing {realtime_df.timestamp.count()} rows to ODS realtime API...')
+
+for index, row in realtime_df.iterrows():
+    timestamp_text = row.timestamp.strftime('%Y-%m-%dT%H:%M:%S%z')
+    # Realtime API bootstrap data:
+    # {
+    #     "anfangszeit": "23.02.2021 10:30:00",
+    #     "pm25": 13.3
+    # }
+    payload = {"anfangszeit": row['Anfangszeit'],  "pm25": row['PM25_Sensirion']}
+    print(f'Pushing row {index} with with the following data to ODS: {payload}')
+    r = common.requests_post(url=credentials.ods_live_push_api_url, json=payload, verify=False)
+    r.raise_for_status()
+
+print('Job successful!')
---
+++ lufthygiene_nmbs_pm25/etl.sh
@@ -0,0 +1,4 @@
+cd /code/data-processing || exit
+# pip freeze > /code/data-processing/lufthygiene_pm25/requirements-in-docker.txt
+python3 -m lufthygiene_nmbs_pm25.etl
+
