b4f9cbec463784bdeb4567a30cee7ca9d0430be1
jb3-2 <jonas.bieri@gmail.com>
2021-02-25 08:11:59 +0100

    Push all latest data without checking for latest timestamp in ODS to circumvent ignoring values delivered late

--- lufthygiene_pm25/etl.py
+++ lufthygiene_pm25/etl.py
@@ -6,20 +6,25 @@ from lufthygiene_pm25 import credentials
 
 url = 'https://data-bs.ch/lufthygiene/regionales-mikroklima/airmet_bs_sensirion_pm25_aktuell.csv'
 print(f'Downloading data from {url}...')
+urllib3.disable_warnings()
 df = common.pandas_read_csv(url, sep=';', encoding='cp1252', skiprows=range(1, 6))
 print(f'Calculating ISO8601 time string...')
 df['timestamp'] = pd.to_datetime(df.Zeit, format='%d.%m.%Y %H:%M:%S').dt.tz_localize('Europe/Zurich', ambiguous=True, nonexistent='shift_forward')
 
-print(f'Reading latest timestamp from ODS dataset...')
-urllib3.disable_warnings()
-r = common.requests_get('https://data.bs.ch/api/records/1.0/search/?dataset=100081&q=&rows=1&sort=zeitstempel', verify=False)
-r.raise_for_status()
-latest_ods_timestamp = r.json()['records'][0]['fields']['zeitstempel']
-print(f'Latest timestamp retrieved from ODS is {latest_ods_timestamp}.')
-print(f'Latest timestamp retrieved new data is {df.timestamp.max().strftime("%Y.%m.%dT%H:%M:%S%z")}')
-print(f'Filtering data after {latest_ods_timestamp} for submission to ODS via realtime API...')
-realtime_df = df[df['timestamp'] > latest_ods_timestamp]
+# we simplify the code and re-push all current data all the time instead of checking for the latest timestamp in ODS.
+# Otherwise we'd need to check for the latest timestamp of each single sensro, instead of the latest overall.
 
+# print(f'Reading latest timestamp from ODS dataset...')
+# urllib3.disable_warnings()
+# r = common.requests_get('https://data.bs.ch/api/records/1.0/search/?dataset=100081&q=&rows=1&sort=zeitstempel', verify=False)
+# r.raise_for_status()
+# latest_ods_timestamp = r.json()['records'][0]['fields']['zeitstempel']
+# print(f'Latest timestamp retrieved from ODS is {latest_ods_timestamp}.')
+# print(f'Latest timestamp retrieved new data is {df.timestamp.max().strftime("%Y.%m.%dT%H:%M:%S%z")}')
+# print(f'Filtering data after {latest_ods_timestamp} for submission to ODS via realtime API...')
+# realtime_df = df[df['timestamp'] > latest_ods_timestamp]
+
+realtime_df = df
 if len(realtime_df) == 0:
     print(f'No rows to push to ODS... ')
 else:
@@ -38,10 +43,9 @@ else:
     #     "pm_2_5": 8.8
     # }
 
-    # Push all records in one HTTP post
     ldf.timestamp = ldf.timestamp.dt.strftime('%Y-%m-%d %H:%M:%S%z')
     payload = ldf.to_json(orient="records")
-    print(f'Pushing the following data to ODS: {json.dumps(json.loads(payload), indent=4)}')
+    # print(f'Pushing the following data to ODS: {json.dumps(json.loads(payload), indent=4)}')
     # use data=payload here because payload is a string. If it was an object, we'd have to use json=payload.
     r = common.requests_post(url=credentials.ods_live_push_api_url, data=payload, verify=False)
     r.raise_for_status()
