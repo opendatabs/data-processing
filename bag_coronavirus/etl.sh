8267d07fa4f7e63bd7516bc364e917273270685e
jb3-2 <jonas.bieri@gmail.com>
2021-03-03 12:11:40 +0100

    Also run etl_test_pcr_antigen as part of the pipeline

--- bag_coronavirus/etl.sh
+++ bag_coronavirus/etl.sh
@@ -1,3 +1,3 @@
 cd /code/data-processing || exit
-python3 -m bag_coronavirus.etl_test && python3 -m bag_coronavirus.etl_hosp_capacity
+python3 -m bag_coronavirus.etl_test && python3 -m bag_coronavirus.etl_hosp_capacity && python3 -m bag_coronavirus.etl_test_pcr_antigen
 
