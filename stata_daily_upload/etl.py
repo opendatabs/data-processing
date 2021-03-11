2af5a4219285a703b56e36f6e0e58f00fbf3c101
jb3-2 <jonas.bieri@gmail.com>
2021-03-11 15:56:22 +0100

    Remove module stata_veranstaltungen - work is now done in stata_daily_upload

--- stata_daily_upload/etl.py
+++ stata_daily_upload/etl.py
@@ -6,7 +6,8 @@ uploads = [{'file': 'Bevoelkerung/sterbefaelle.csv', 'dest_dir': 'bevoelkerung'}
            {'file': 'Bevoelkerung/geburten_nach_datum.csv', 'dest_dir': 'bevoelkerung'},
            {'file': 'Bevoelkerung/geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung'},
            {'file': 'Tourismus/tourismus-daily.csv', 'dest_dir': 'tourismus'},
-           {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus'}]
+           {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus'},
+           {'file': 'Veranstaltung/veranstaltungen.csv', 'dest_dir': 'veranstaltungen'},]
 
 for upload in uploads:
     common.upload_ftp(os.path.join(credentials.path_work, upload['file']), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
--- stata_veranstaltungen/etl.sh
+++
@@ -1,3 +0,0 @@
-cd /code/data-processing || exit
-python3 -m stata_veranstaltungen.etl
-
