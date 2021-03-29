ee82a046bf10183d61dbc451c77dcdb61d7e453b
jb3-2 <jonas.bieri@gmail.com>
2021-03-29 13:16:31 +0200

    Add 5 new datasets

--- stata_daily_upload/etl.py
+++ stata_daily_upload/etl.py
@@ -7,7 +7,13 @@ uploads = [{'file': 'Bevoelkerung/sterbefaelle.csv', 'dest_dir': 'bevoelkerung'}
            {'file': 'Bevoelkerung/geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung'},
            {'file': 'Tourismus/tourismus-daily.csv', 'dest_dir': 'tourismus'},
            {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus'},
-           {'file': 'Veranstaltung/veranstaltungen.csv', 'dest_dir': 'veranstaltungen'},]
+           {'file': 'Veranstaltung/veranstaltungen.csv', 'dest_dir': 'veranstaltungen'},
+           {'file': 'Bevoelkerung/01bevoelkerung_monat_nach_bezirk.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/02bevoelkerung_jahr_nach_CH_A_geschlecht.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/03bevoelkerung_jahr_nach_heimat_geschlecht.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/04bevoelkerung_jahr_nach_vorname.csv', 'dest_dir': 'bevoelkerung'},
+           {'file': 'Bevoelkerung/05bevoelkerung_jahr_nach_nachname.csv', 'dest_dir': 'bevoelkerung'},
+           ]
 
 for upload in uploads:
     common.upload_ftp(os.path.join(credentials.path_work, upload['file']), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
