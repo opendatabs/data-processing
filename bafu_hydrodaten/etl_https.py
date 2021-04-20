da7f6b61768c9f25c95be62ec76062cb99a28426
jb3_2 <jonas.bieri@gmail.com>
2021-04-20 16:50:40 +0200

    Create csv for multiple sensors

--- bafu_hydrodaten/etl_https.py
+++ bafu_hydrodaten/etl_https.py
@@ -4,24 +4,28 @@ import os
 import pandas as pd
 import common
 from requests.auth import HTTPBasicAuth
+from functools import reduce
 from bafu_hydrodaten import credentials
 
 print('Loading data into data frames...')
 dfs = []
-for file in [credentials.abfluss_file, credentials.pegel_file]:
+for file in credentials.files:
     response = common.requests_get(f'{credentials.https_url}/{file}', auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass), stream=True)
     df = pd.read_csv(response.raw, parse_dates=True, infer_datetime_format=True)
     dfs.append(df)
 
 print(f'Merging data frames...')
-merged_df = dfs[0].merge(dfs[1], on=['Time'], how='outer')
+all_df = reduce(lambda left, right: pd.merge(left, right, on=['Time'], how='outer'), dfs)
+all_df.to_csv(f"{os.path.join(credentials.path, 'bafu_hydrodaten/data/')}hydrodata_{datetime.today().strftime('%Y-%m-%d')}.csv", index=False)
 
 print('Processing data...')
+merged_df = all_df.copy(deep=True)
 merged_df['timestamp'] = pd.to_datetime(merged_df.Time, infer_datetime_format=True)
 merged_df['datum'] = merged_df.timestamp.dt.strftime('%d.%m.%Y')
 merged_df['zeit'] = merged_df.timestamp.dt.strftime('%H:%M')
 merged_df['intervall'] = 5
-merged_df = merged_df.rename(columns={'BAFU_2289_AbflussRadar': 'abfluss', 'BAFU_2289_PegelRadar': 'pegel'})
+merged_df['abfluss'] = merged_df['BAFU_2289_AbflussRadar']
+merged_df['pegel'] = merged_df['BAFU_2289_PegelRadar']
 merged_df = merged_df[['datum', 'zeit', 'abfluss', 'intervall', 'pegel', 'timestamp']]
 
 local_path = os.path.join(credentials.path, 'bafu_hydrodaten/data')
