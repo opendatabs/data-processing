eb0915f2203fd327c199e9090753b3f941396568
jb3_2 <jonas.bieri@gmail.com>
2021-04-21 09:30:39 +0200

    Add new variables

--- bafu_hydrodaten/dummy-credentials.py
+++ bafu_hydrodaten/dummy-credentials.py
@@ -2,15 +2,21 @@ ftp_server = 'FTP-GATEWAY'
 ftp_user = 'USERNAME@data-bs.ch@data-bs.ch'
 ftp_pass = 'DUMMYPASSWORD'
 ftp_remote_dir = 'some/folder'
+ftp_dir_all = 'some/folder'
 
 path = '/code/data-processing/aue_schall/data/'
 
 proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
 
-ftp_read_server = 'FTP-GATEWAY'
-ftp_read_user = 'USERNAME@data-bs.ch@data-bs.ch'
-ftp_read_pass = 'DUMMYPASSWORD'
-ftp_read_remote_path = 'some/folder'
+https_url = 'https://some.url'
+https_live_url = 'https://some.url'
+https_user = 'username'
+https_pass = 'password'
 
-abfluss_file = 'HBCHa2289_10_10_02.txt'
-pegel_file = 'HBCHa2289_02_00_02.txt'
+abfluss_2289 = 'id/file.csv'
+pegel_2289 = 'id/file.csv'
+
+files = ['id/file.csv', 'id/file.csv', 'id/file.csv']
+
+ods_test_push_api_url = 'https://data.bs.ch/api/push/1.0/ID/realtime/push/?pushkey=PUSHKEY'
+ods_live_push_api_url = 'https://data.bs.ch/api/push/1.0/ID/realtime/push/?pushkey=PUSHKEY'
