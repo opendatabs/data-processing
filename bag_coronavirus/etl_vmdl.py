9ee410da51eb249aa924deda23e60646c7b18dac
jb3_2 <jonas.bieri@gmail.com>
2021-04-30 22:01:44 +0200

    Replace nan with 0, increase legibility of SQL code

--- bag_coronavirus/etl_vmdl.py
+++ bag_coronavirus/etl_vmdl.py
@@ -2,8 +2,6 @@ import pandas as pd
 import requests
 import os
 import common
-import pandas as pd
-from io import StringIO
 from pandasql import sqldf
 from bag_coronavirus import credentials
 
@@ -31,16 +29,31 @@ with open(file_path, "w") as f:
 
 print(f'Reading data into dataframe...')
 df = pd.read_csv(file_path, sep=';')
-df['vacc_date_dt'] = pd.to_datetime(df.vacc_date, format='%Y-%m-%dT%H:%M:%S.%f%z')
+# df['vacc_date_dt'] = pd.to_datetime(df.vacc_date, format='%Y-%m-%dT%H:%M:%S.%f%z')
 df['vacc_day'] = df.vacc_date.str.slice(stop=10)
 
 print(f'Executing calculations...')
 pysqldf = lambda q: sqldf(q, globals())
 # sum type 1 and 99, filter by BS, count distinct persons
-df_bs = sqldf('select * from df where reporting_unit_location_ctn = "BS"')
-df_bs_by = sqldf('select vacc_day, vacc_count, case reporting_unit_location_type when 1 then "vacc_centre" when 99 then "vacc_centre" when 6 then "hosp" else reporting_unit_location_type end as location_type, count(distinct person_anonymised_id) as count from df_bs group by vacc_day, vacc_count, location_type order by vacc_day asc;')
+df_bs = sqldf('''
+    select * 
+    from df 
+    where reporting_unit_location_ctn = "BS"''')
 
-df_pivot = df_bs_by.pivot_table(values='count', index=['vacc_day'], columns=['location_type', 'vacc_count'])
+df_bs_by = sqldf('''
+    select vacc_day, vacc_count, 
+    case reporting_unit_location_type 
+        when 1  then "vacc_centre" 
+        when 99 then "vacc_centre" 
+        when 6  then "hosp" 
+        else reporting_unit_location_type 
+        end as location_type, 
+    count(distinct person_anonymised_id) as count 
+    from df_bs 
+    group by vacc_day, vacc_count, location_type
+    order by vacc_day asc;''')
+
+df_pivot = df_bs_by.pivot_table(values='count', index=['vacc_day'], columns=['location_type', 'vacc_count'], fill_value=0)
 # Replace the 2-level column names with a string that concatenates both strings
 df_pivot.columns = ["_".join(str(c) for c in col) for col in df_pivot.columns.values]
 df_pivot = df_pivot.reset_index()
@@ -54,7 +67,11 @@ df_pivot['cum_1'] = df_pivot.vacc_count_1.cumsum()
 df_pivot['cum_2'] = df_pivot.vacc_count_2.cumsum()
 df_pivot['only_1'] = df_pivot.cum_1 - df_pivot.cum_2
 
-df_total = sqldf('select vacc_day, count(distinct person_anonymised_id) as total from df_bs group by vacc_day order by vacc_day asc;')
+df_total = sqldf('''
+    select vacc_day, count(distinct person_anonymised_id) as total 
+    from df_bs 
+    group by vacc_day 
+    order by vacc_day asc;''')
 df_total['total_cum'] = df_total.total.cumsum()
 
 df_merged = df_pivot.merge(right=df_total, how='outer', on='vacc_day')
