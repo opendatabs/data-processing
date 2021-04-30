560e282f41b2325a58af87daebe694c2eee49c92
jb3_2 <jonas.bieri@gmail.com>
2021-04-30 10:23:42 +0200

    Load vmdl data

--- bag_coronavirus/etl_vmdl.py
+++ bag_coronavirus/etl_vmdl.py
@@ -7,7 +7,7 @@ headers_token = {'Content-Type': 'application/x-www-form-urlencoded'}
 print(f'Getting OAUTH2 access token...')
 resp_token = requests.request("POST", credentials.vmdl_url_token, headers=headers_token, data=payload_token)
 resp_token.raise_for_status()
-#token_type = resp_token.json()['token_type']
+# token_type = resp_token.json()['token_type']
 auth_string = f'Bearer {resp_token.json()["access_token"]}'
 
 payload_download={}
@@ -15,8 +15,9 @@ headers_download = {'Authorization': auth_string}
 print(f'Downloading data...')
 resp_download = requests.request("GET", credentials.vmdl_url_download, headers=headers_download, data=payload_download)
 resp_download.raise_for_status()
-print(f'Writing data to file {credentials.vmdl_file}...')
-with open(os.path.join(credentials.path, credentials.vmdl_file), "w") as f:
+file_path = os.path.join(credentials.vmdl_path, credentials.vmdl_file)
+print(f'Writing data to file {file_path}...')
+with open(file_path, "w") as f:
     f.write(resp_download.text)
 
 print(f'Job successful!')
