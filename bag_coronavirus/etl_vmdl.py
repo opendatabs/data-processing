260ddaff644f9b7319ade376d0809636185528ab
jb3-2 <jonas.bieri@gmail.com>
2021-04-30 11:47:32 +0200

    Calculate counts

--- bag_coronavirus/etl_vmdl.py
+++ bag_coronavirus/etl_vmdl.py
@@ -1,5 +1,9 @@
+import pandas as pd
 import requests
 import os
+import common
+import pandas as pd
+from io import StringIO
 from bag_coronavirus import credentials
 
 payload_token = f'client_id={credentials.vmdl_client_id}&scope={credentials.vmdl_scope}&username={credentials.vmdl_user}&password={credentials.vmdl_password}&grant_type=password'
@@ -13,11 +17,24 @@ auth_string = f'Bearer {resp_token.json()["access_token"]}'
 payload_download={}
 headers_download = {'Authorization': auth_string}
 print(f'Downloading data...')
-resp_download = requests.request("GET", credentials.vmdl_url_download, headers=headers_download, data=payload_download)
+resp_download = common.requests_get(credentials.vmdl_url_download, headers=headers_download, data=payload_download)
+# resp_download = requests.request("GET", credentials.vmdl_url_download, headers=headers_download, data=payload_download)
 resp_download.raise_for_status()
 file_path = os.path.join(credentials.vmdl_path, credentials.vmdl_file)
 print(f'Writing data to file {file_path}...')
 with open(file_path, "w") as f:
     f.write(resp_download.text)
+
+print(f'Reading data into StringIO...')
+vmdl_text = StringIO(resp_download.text)
+print(f'Reading data into dataframe...')
+df = pd.read_csv(vmdl_text, sep=';')
+print(f'Calculating...')
+df_bs = df[df['reporting_unit_location_ctn']=='BS']
+# see https://medium.com/jbennetcodes/how-to-rewrite-your-sql-queries-in-pandas-and-more-149d341fc53e
+df_by = df.groupby(['vacc_date', 'vacc_count', 'reporting_unit_location_type']).size().to_frame('count').reset_index()
+by_file = os.path.join(credentials.vmdl_path, f'vmdl_by.csv')
+print(f'Exporting resulting data to {by_file}...')
+df_by.to_csv(by_file, index=False)
 
 print(f'Job successful!')
