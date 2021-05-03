ceec3b4b3ade42e25f88085b29625faaea0c5e29
jb3_2 <jonas.bieri@gmail.com>
2021-05-03 11:06:30 +0200

    Add days without vaccinations, clean up

--- bag_coronavirus/etl_vmdl.py
+++ bag_coronavirus/etl_vmdl.py
@@ -17,10 +17,7 @@ payload_download={}
 headers_download = {'Authorization': auth_string}
 print(f'Downloading data...')
 resp_download = common.requests_get(credentials.vmdl_url_download, headers=headers_download, data=payload_download)
-# resp_download = requests.request("GET", credentials.vmdl_url_download, headers=headers_download, data=payload_download)
 resp_download.raise_for_status()
-# print(f'Reading data into StringIO...')
-# vmdl_text = StringIO(resp_download.content)
 file_path = os.path.join(credentials.vmdl_path, credentials.vmdl_file)
 print(f'Writing data to file {file_path}...')
 resp_download.encoding = 'utf-8'
@@ -53,31 +50,32 @@ df_bs_by = sqldf('''
     group by vacc_day, vacc_count, location_type
     order by vacc_day asc;''')
 
-df_pivot = df_bs_by.pivot_table(values='count', index=['vacc_day'], columns=['location_type', 'vacc_count'], fill_value=0)
+# Create empty table of all combinations
+df_all_days = pd.DataFrame(data=pd.date_range(start=df_bs.vacc_day.min(), end=df_bs.vacc_day.max()).astype(str), columns=['vacc_day'])
+df_all_vacc_count = sqldf('select distinct vacc_count from df;')
+df_all_location_type = sqldf('select distinct location_type from df_bs_by')
+df_all_comb = sqldf('select *from df_all_days cross join df_all_vacc_count cross join df_all_location_type;')
+
+# Add days without vaccinations
+df_bs_by_all = df_all_comb.merge(df_bs_by, on=['vacc_day', 'vacc_count', 'location_type'], how='outer').fillna(0)
+
+df_pivot_table = df_bs_by_all.pivot_table(values='count', index=['vacc_day'], columns=['location_type', 'vacc_count'], fill_value=0)
 # Replace the 2-level column names with a string that concatenates both strings
-df_pivot.columns = ["_".join(str(c) for c in col) for col in df_pivot.columns.values]
-df_pivot = df_pivot.reset_index()
+df_pivot_table.columns = ["_".join(str(c) for c in col) for col in df_pivot_table.columns.values]
+df_pivot = df_pivot_table.reset_index()
 
 df_pivot['hosp'] = df_pivot.hosp_1 + df_pivot.hosp_2
 df_pivot['vacc_centre'] = df_pivot.vacc_centre_1 + df_pivot.vacc_centre_2
 df_pivot['vacc_count_1'] = df_pivot.hosp_1 + df_pivot.vacc_centre_1
 df_pivot['vacc_count_2'] = df_pivot.hosp_2 + df_pivot.vacc_centre_2
-
 df_pivot['cum_1'] = df_pivot.vacc_count_1.cumsum()
 df_pivot['cum_2'] = df_pivot.vacc_count_2.cumsum()
 df_pivot['only_1'] = df_pivot.cum_1 - df_pivot.cum_2
+df_pivot['total'] = df_pivot.hosp + df_pivot.vacc_centre
+df_pivot['total_cum'] = df_pivot.total.cumsum()
 
-df_total = sqldf('''
-    select vacc_day, count(person_anonymised_id) as total 
-    from df_bs 
-    group by vacc_day 
-    order by vacc_day asc;''')
-df_total['total_cum'] = df_total.total.cumsum()
-
-df_merged = df_pivot.merge(right=df_total, how='outer', on='vacc_day')
-
-bs_by_file = os.path.join(credentials.vmdl_path, f'vmdl_bs_by.csv')
+bs_by_file = os.path.join(credentials.vmdl_path, f'vaccination_report_bs.csv')
 print(f'Exporting resulting data to {bs_by_file}...')
-df_merged.to_csv(bs_by_file, index=False)
+df_pivot.to_csv(bs_by_file, index=False)
 
 print(f'Job successful!')
