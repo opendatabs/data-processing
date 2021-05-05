cf231accd659daa66783e455b90aaa259e5b08b4
jb3_2 <jonas.bieri@gmail.com>
2021-05-05 12:57:31 +0200

    Add etl_vmdl command and explanation

--- bag_coronavirus/etl.sh
+++ bag_coronavirus/etl.sh
@@ -1,3 +1,4 @@
 cd /code/data-processing || exit
 python3 -m bag_coronavirus.copy_bag_datasets && python3 -m bag_coronavirus.etl_test
-
+# command below is executed through separate AirFlow Task:
+# python3 -m bag_coronavirus.etl_vmdl
