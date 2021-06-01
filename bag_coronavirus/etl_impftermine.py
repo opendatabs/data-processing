b104c3621766c54c924beee9ef3f8c8564b21c85
jb3_2 <jonas.bieri@gmail.com>
2021-06-01 12:59:06 +0200

    Read all daily files on every run

--- bag_coronavirus/etl_impftermine.py
+++ bag_coronavirus/etl_impftermine.py
@@ -2,36 +2,50 @@ import common
 import datetime
 import numpy
 import os
+import glob
 import openpyxl
 from bag_coronavirus import credentials
+from bag_coronavirus import etl_vmdl_altersgruppen as vmdl
 import pandas as pd
 
 
 def main():
-    impftermine_file = os.path.join(credentials.impftermine_path, credentials.impftermine_file)
-    print(f'Reading data from {impftermine_file}...')
-    df = pd.read_excel(impftermine_file)
+
+    files = sorted(glob.glob(os.path.join(credentials.impftermine_path, "*.xlsx")))
+    df = pd.DataFrame()
+    for f in files:
+        print(f'Reading data from {f}...')
+        df_single = pd.read_excel(f)
+        print(f'Get file modification datetime...')
+        # file_date = datetime.datetime.fromtimestamp(os.path.getmtime(f)).strftime('%Y-%m-%d')
+        # See https://stackoverflow.com/a/7983848
+        file_date = f[-15:][:-5]
+        print(f'Adding file date {file_date} as a new column...')
+        df_single['date'] = file_date
+        print(f'Appending dataset...')
+        df = df.append(df_single)
+
 
     print(f'Calculating age, age group...')
     df['birthday'] = pd.to_datetime(df.Birthdate, format='%d.%m.%Y')
     df['age'] = (pd.to_datetime('now') - df.birthday).astype('<m8[Y]')
-    bins =      [numpy.NINF, 15,     49,         64,         74,         numpy.inf]
-    labels =    ['Unbekannt',       '16-49',    '50-64',    '65-74',    '> 74']
-    df['age_group'] = pd.cut(df.age, bins=bins, labels=labels, include_lowest=True)
+    df['age_group'] = pd.cut(df.age, bins=vmdl.get_age_groups()['bins'], labels=vmdl.get_age_groups()['labels'], include_lowest=True)
     df = df.rename(columns={'Has appointments': 'has_appointments'})
 
     print(f'Aggregating data...')
-    df_agg = df.groupby(['age_group', 'has_appointments']).agg(len).reset_index().rename(columns={'age': 'count'})[['age_group', 'has_appointments', 'count']]
+    df_agg = (df.groupby(['date', 'age_group', 'has_appointments'])
+        .agg(len)
+        .reset_index()
+        .rename(columns={'age': 'count'})[['date', 'age_group', 'has_appointments', 'count']]
+    )
 
     print(f'Filtering "Unbekannt"...')
     df_agg = df_agg[df_agg.age_group != 'Unbekannt']
 
-    print(f'Get file modification datetime...')
-    file_date = datetime.datetime.fromtimestamp(os.path.getmtime(impftermine_file)).strftime('%Y-%m-%d')
-    df_agg['date'] = file_date
-    df_agg = df_agg[['age_group', 'has_appointments', 'count', 'date']]
+    print(f'Making sure only certain columns are exported...')
+    df_agg = df_agg[['date', 'age_group', 'has_appointments', 'count']]
 
-    export_file_name = os.path.join(credentials.impftermine_path, 'export', f'impftermine_agg_{file_date}.csv')
+    export_file_name = os.path.join(credentials.impftermine_path, 'export', f'impftermine_agg.csv')
     print(f'Exporting resulting data to {export_file_name}...')
     df_agg.to_csv(export_file_name, index=False)
     common.upload_ftp(export_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'md/covid19_vacc')
