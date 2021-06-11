51eab42cb509bb40182970b3a6ec5df247128961
jb3_2 <jonas.bieri@gmail.com>
2021-06-11 15:22:52 +0200

    Add test_regression_details()

--- staka_abstimmungen/tests/test_regression.py
+++ staka_abstimmungen/tests/test_regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pytest
 from staka_abstimmungen.src import etl_kennzahlen
+from staka_abstimmungen.src import etl_details
 from staka_abstimmungen import credentials
 import os
 import filecmp
@@ -8,11 +9,22 @@ import filecmp
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_regression():
+def test_regression_kennzahlen(tmp_path):
     ref_file = os.path.join(CURR_DIR, 'fixtures', 'Abstimmungen_2021-06-13.csv')
     source_files = [os.path.join(CURR_DIR, 'fixtures', 'Resultate_EID.xlsx'), os.path.join(CURR_DIR, 'fixtures', 'Resultate_KAN.xlsx')]
+
     abst_date, df_report = etl_kennzahlen.calculate_kennzahlen(source_files)
-    file_to_test = os.path.join(CURR_DIR, 'fixtures', f'kennzahlen_generated_by_test_{abst_date}.csv')
+    file_to_test = os.path.join(CURR_DIR, 'fixtures', f'Abstimmungen_{abst_date}_generated_by_test.csv')
+    df_report.to_csv(file_to_test, index=False)
+    assert filecmp.cmp(ref_file, file_to_test)
+
+
+def test_regression_details(tmp_path):
+    ref_file = os.path.join(CURR_DIR, 'fixtures', 'Abstimmungen_Details_2021-06-13.csv')
+    source_files = [os.path.join(CURR_DIR, 'fixtures', 'Resultate_EID.xlsx'), os.path.join(CURR_DIR, 'fixtures', 'Resultate_KAN.xlsx')]
+
+    abst_date, df_report = etl_details.calculate_details(source_files)
+    file_to_test = os.path.join(CURR_DIR, 'fixtures', f'Abstimmungen_Details_{abst_date}_generated_by_test.csv')
     df_report.to_csv(file_to_test, index=False)
     assert filecmp.cmp(ref_file, file_to_test)
 
