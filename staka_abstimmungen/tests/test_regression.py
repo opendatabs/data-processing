c42d6a468f4577a6c27fe82a869c9a13371a4eae
jb3_2 <jonas.bieri@gmail.com>
2021-06-11 14:58:19 +0200

    Add date to filename

--- staka_abstimmungen/tests/test_regression.py
+++ staka_abstimmungen/tests/test_regression.py
@@ -12,7 +12,7 @@ def test_regression():
     ref_file = os.path.join(CURR_DIR, 'fixtures', 'Abstimmungen_2021-06-13.csv')
     source_files = [os.path.join(CURR_DIR, 'fixtures', 'Resultate_EID.xlsx'), os.path.join(CURR_DIR, 'fixtures', 'Resultate_KAN.xlsx')]
     abst_date, df_report = etl_kennzahlen.calculate_kennzahlen(source_files)
-    file_to_test = os.path.join(CURR_DIR, 'fixtures', 'kennzahlen_generated_by_test.csv')
+    file_to_test = os.path.join(CURR_DIR, 'fixtures', f'kennzahlen_generated_by_test_{abst_date}.csv')
     df_report.to_csv(file_to_test, index=False)
     assert filecmp.cmp(ref_file, file_to_test)
 
