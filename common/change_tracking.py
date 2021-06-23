c16bfdb3710854ae9fe1568bf149b742857f446f
jb3_2 <jonas.bieri@gmail.com>
2021-06-23 21:17:25 +0200

    Add module common.change_tracking

---
+++ common/change_tracking.py
@@ -0,0 +1,49 @@
+import logging
+import pathlib
+from hashlib import blake2b
+from filehash import FileHash
+import os
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def write_hash_file(file_name, sfv_file_name) -> str:
+    crc32_hasher = FileHash(hash_algorithm='crc32')
+    logging.info(f'Ensuring that path exists...')
+    pathlib.Path(os.path.dirname(sfv_file_name)).mkdir(parents=True, exist_ok=True)
+    with open(sfv_file_name, 'w') as f:
+        logging.info(f'Calculating hash of file {file_name} and writing to sfv file {sfv_file_name}...')
+        file_hash = crc32_hasher.hash_file(file_name)
+        f.write(f'{file_name} {file_hash}')
+    return file_hash
+
+
+def get_hash_file(filename, dir='') -> str:
+    if not dir:
+        dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'change_tracking')
+    f = blake2b(filename.encode('utf-8')).hexdigest()
+    sfv_filename = os.path.join(dir, f'{f}.sfv')
+    return sfv_filename
+
+
+def has_changed(filename: str, hash_file_dir='') -> bool:
+    sfv_filename = get_hash_file(filename, hash_file_dir)
+    crc32_hasher = FileHash(hash_algorithm='crc32')
+    if not os.path.exists(filename):
+        logging.info(f'File does not exist: {filename}')
+        return True
+    if not os.path.exists(sfv_filename):
+        logging.info(f'SFV file does not exist, calculating has for file: {sfv_filename}...')
+        hash_value = write_hash_file(filename, sfv_filename)
+        logging.info(f'Hash value {hash_value} written into SFV file.')
+        return True
+    logging.info(f'SFV file exists, checking for changes: {sfv_filename}...')
+    hashes_differ = not crc32_hasher.verify_sfv(sfv_filename=sfv_filename)[0].hashes_match
+    if hashes_differ:
+        logging.info(f'Hashes do not match, recalculating hash...')
+        hash_value = write_hash_file(filename, sfv_filename)
+        logging.info(f'New hash: {hash_value}')
+        return True
+    else:
+        logging.info(f'Hashes match...')
+        return False
---
+++ common/tests/test_change_tracking.py
@@ -0,0 +1,56 @@
+import os
+import pathlib
+import shutil
+from datetime import datetime
+from random import random
+import common.change_tracking as ct
+import pytest
+import common
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+CHANGE_TRACKING_DIR = os.path.join(CURR_DIR, 'fixtures', 'change_tracking')
+
+
+@pytest.fixture
+def text_file(tmp_path):
+    file_path = os.path.join(tmp_path, f'test-{random()}.txt')
+    with open(file_path, 'w') as f:
+        f.write(f'{datetime.now()}: Hello World!')
+    yield file_path
+    os.remove(file_path)
+
+
+def test_no_file(tmp_path):
+    file_path = os.path.join(tmp_path, f'test-{random()}.txt')
+    assert ct.has_changed(file_path, CHANGE_TRACKING_DIR)
+
+
+def test_new_file(text_file):
+    assert ct.has_changed(text_file)
+
+
+def test_unchanged_file(text_file):
+    # Access file first to generate, then a 2nd time it should not have changed.
+    assert ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+
+
+def test_changed_file(text_file):
+    # Access file first to generate, then a 2nd time it should not have changed.
+    assert ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    with open(text_file, 'w') as f:
+        f.write(f'{datetime.now()}: Hello World!')
+    assert ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    with open(text_file, 'a') as f:
+        f.write(f'{datetime.now()}_Hello World! \n')
+    assert ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+    assert not ct.has_changed(text_file, CHANGE_TRACKING_DIR)
+
+
+def test_teardown():
+    """Delete all sfv files used for the tests."""
+    shutil.rmtree(CHANGE_TRACKING_DIR)
