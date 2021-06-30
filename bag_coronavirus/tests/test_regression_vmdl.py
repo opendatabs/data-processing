04bbdc83882d5e59a2079f305ef4f6bfabc0233f
jb3_2 <jonas.bieri@gmail.com>
2021-06-30 18:51:19 +0200

    Add assert comment

--- bag_coronavirus/tests/test_regression_vmdl.py
+++ bag_coronavirus/tests/test_regression_vmdl.py
@@ -19,4 +19,4 @@ def test_first_age_group_period_regression(vmdl_raw_df):
     bin_def = etl_vmdl_altersgruppen.get_age_groups_over_time()[0]
     raw_df = etl_vmdl_altersgruppen.get_partial_raw_df(conftest.VMDL_CSV_FILE, bin_def)
     until_date = bin_def['until_date']
-    assert vmdl_raw_df.query('vacc_day <= @until_date').equals(raw_df)
+    assert vmdl_raw_df.query('vacc_day <= @until_date').equals(raw_df), 'First age_group period should match with older data where only 1 period was present.'
