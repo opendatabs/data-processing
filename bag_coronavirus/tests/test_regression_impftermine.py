f4b917f111301f4fbfe6de62109a399adf1ddd06
jb3_2 <jonas.bieri@gmail.com>
2021-07-01 18:10:06 +0200

    Add regression test for firts age group period

--- bag_coronavirus/tests/test_regression_impftermine.py
+++ bag_coronavirus/tests/test_regression_impftermine.py
@@ -1,13 +1,20 @@
 import pytest
 import pandas as pd
 import os
-from bag_coronavirus.src import etl_impftermine
+from bag_coronavirus.src import etl_impftermine, etl_vmdl_altersgruppen
 from bag_coronavirus.tests import conftest
 
 
 def test_regression_impftermine(impft_df, impft_df_agg):
     df_to_test, df_agg_to_test = etl_impftermine.transform(impft_df)
     assert impft_df_agg.equals(df_agg_to_test)
+
+
+def test_first_age_group_period_regression(impft_df, impft_df_agg):
+    df_to_test, df_agg_to_test = etl_impftermine.transform(impft_df)
+    bin_def = etl_vmdl_altersgruppen.get_age_group_periods()[0]
+    until_date = bin_def['until_date']
+    assert df_agg_to_test.query('date <= @until_date').equals(impft_df_agg.query('date <= @until_date')), 'First age_group period should match with older data where only 1 period was present.'
 
 
 def main():
