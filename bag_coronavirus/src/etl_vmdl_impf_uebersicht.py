1cde1b5e89146644039af3813b7fe1dd70c52b58
jb3_2 <jonas.bieri@gmail.com>
2021-07-05 15:37:00 +0200

    Prepare for testing, move to src folder

--- bag_coronavirus/src/etl_vmdl_impf_uebersicht.py
+++ bag_coronavirus/src/etl_vmdl_impf_uebersicht.py
@@ -7,23 +7,28 @@ from bag_coronavirus.src import vmdl
 
 
 def main():
-    # file_path = vmdl_extract.retrieve_vmdl_data()
-    file_path = vmdl.file_path()
+    df = extract_data(file_path=vmdl.file_path())
+    df_export = transform_data(df)
+    load_data(df_export)
+    print(f'Job successful!')
 
-    print(f'Reading data into dataframe...')
+
+def extract_data(file_path):
+    print(f'Reading data from into dataframe {file_path}...')
     df = pd.read_csv(file_path, sep=';')
+    return df
+
+
+def transform_data(df):
+    print(f'Executing calculations...')
     # df['vacc_date_dt'] = pd.to_datetime(df.vacc_date, format='%Y-%m-%dT%H:%M:%S.%f%z')
     df['vacc_day'] = df.vacc_date.str.slice(stop=10)
-
-    print(f'Executing calculations...')
     pysqldf = lambda q: sqldf(q, globals())
-
     print(f'Filter by BS and vacc_date, sum type 1 and 99, create "other" type, count persons...')
     df_bs = sqldf(f'''
         select * 
         from df 
         where reporting_unit_location_ctn = "BS" and vacc_day < "{vmdl.today_string()}"''')
-
     df_bs_by = sqldf('''
         select vacc_day, vacc_count, 
         case reporting_unit_location_type 
@@ -36,22 +41,18 @@ def main():
         from df_bs 
         group by vacc_day, vacc_count, location_type
         order by vacc_day asc;''')
-
     print(f'Create empty table of all combinations...')
     df_all_days = pd.DataFrame(data=pd.date_range(start=df_bs.vacc_day.min(), end=vmdl.yesterday_string()).astype(str), columns=['vacc_day'])
     df_all_vacc_count = sqldf('select distinct vacc_count from df;')
     df_all_location_type = sqldf('select distinct location_type from df_bs_by')
     df_all_comb = sqldf('select * from df_all_days cross join df_all_vacc_count cross join df_all_location_type;')
-
     print(f'Adding days without vaccinations...')
     df_bs_by_all = df_all_comb.merge(df_bs_by, on=['vacc_day', 'vacc_count', 'location_type'], how='outer').fillna(0)
-
     print(f'Pivoting...')
     df_pivot_table = df_bs_by_all.pivot_table(values='count', index=['vacc_day'], columns=['location_type', 'vacc_count'], fill_value=0)
     # Replace the 2-level column names with a string that concatenates both strings
     df_pivot_table.columns = ["_".join(str(c) for c in col) for col in df_pivot_table.columns.values]
     df_pivot = df_pivot_table.reset_index()
-
     print(f'Ensure columns exist...')
     for column_name in [
         'other_1',
@@ -62,7 +63,6 @@ def main():
     ]:
         if column_name not in df_pivot.columns:
             df_pivot[column_name] = 0
-
     print(f'Calculating columns...')
     df_pivot['hosp'] = df_pivot.hosp_1 + df_pivot.hosp_2
     df_pivot['vacc_centre'] = df_pivot.vacc_centre_1 + df_pivot.vacc_centre_2
@@ -74,28 +74,26 @@ def main():
     df_pivot['only_1'] = df_pivot.cum_1 - df_pivot.cum_2
     df_pivot['total'] = df_pivot.hosp + df_pivot.vacc_centre + df_pivot.other
     df_pivot['total_cum'] = df_pivot.total.cumsum()
-
     print(f'Renaming and restricting columns for export...')
     df_export = df_pivot.rename(columns={
-        'vacc_day':         'datum',
-        'hosp_1':           'im_spital_mit_erster_dosis_geimpfte_personen_pro_tag',
-        'hosp_2':           'im_spital_mit_zweiter_dosis_geimpfte_personen_pro_tag',
-        'vacc_centre_1':    'im_impfzentrum_mit_erster_dosis_geimpfte_personen_pro_tag',
-        'vacc_centre_2':    'im_impfzentrum_mit_zweiter_dosis_geimpfte_personen_pro_tag',
-        'other_1':          'anderswo_mit_erster_dosis_geimpfte_personen_pro_tag',
-        'other_2':          'anderswo_mit_zweiter_dosis_geimpfte_personen_pro_tag',
-        'hosp':             'im_spital_verabreichte_impfungen_pro_tag',
-        'vacc_centre':      'im_impfzentrum_verabreichte_impfungen_pro_tag',
-        'other':            'anderswo_verabreichte_impfungen_pro_tag',
-        'vacc_count_1':     'total_mit_erster_dosis_geimpfte_personen_pro_tag',
-        'vacc_count_2':     'total_mit_zweiter_dosis_geimpfte_personen_pro_tag',
-        'cum_1':            'total_personen_mit_erster_dosis',
-        'cum_2':            'total_personen_mit_zweiter_dosis',
-        'only_1':           'total_personen_mit_ausschliesslich_erster_dosis',
-        'total':            'total_verabreichte_impfungen_pro_tag',
-        'total_cum':        'total_verabreichte_impfungen',
+        'vacc_day': 'datum',
+        'hosp_1': 'im_spital_mit_erster_dosis_geimpfte_personen_pro_tag',
+        'hosp_2': 'im_spital_mit_zweiter_dosis_geimpfte_personen_pro_tag',
+        'vacc_centre_1': 'im_impfzentrum_mit_erster_dosis_geimpfte_personen_pro_tag',
+        'vacc_centre_2': 'im_impfzentrum_mit_zweiter_dosis_geimpfte_personen_pro_tag',
+        'other_1': 'anderswo_mit_erster_dosis_geimpfte_personen_pro_tag',
+        'other_2': 'anderswo_mit_zweiter_dosis_geimpfte_personen_pro_tag',
+        'hosp': 'im_spital_verabreichte_impfungen_pro_tag',
+        'vacc_centre': 'im_impfzentrum_verabreichte_impfungen_pro_tag',
+        'other': 'anderswo_verabreichte_impfungen_pro_tag',
+        'vacc_count_1': 'total_mit_erster_dosis_geimpfte_personen_pro_tag',
+        'vacc_count_2': 'total_mit_zweiter_dosis_geimpfte_personen_pro_tag',
+        'cum_1': 'total_personen_mit_erster_dosis',
+        'cum_2': 'total_personen_mit_zweiter_dosis',
+        'only_1': 'total_personen_mit_ausschliesslich_erster_dosis',
+        'total': 'total_verabreichte_impfungen_pro_tag',
+        'total_cum': 'total_verabreichte_impfungen',
     })
-
     df_export = df_export[[
         'datum',
         'total_verabreichte_impfungen',
@@ -116,12 +114,14 @@ def main():
         'anderswo_mit_zweiter_dosis_geimpfte_personen_pro_tag',
         'total_verabreichte_impfungen_pro_tag',
     ]]
+    return df_export
 
+
+def load_data(df_export):
     export_file_name = os.path.join(credentials.vmdl_path, f'vaccination_report_bs.csv')
     print(f'Exporting resulting data to {export_file_name}...')
     df_export.to_csv(export_file_name, index=False)
     common.upload_ftp(export_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'bag/vmdl')
-    print(f'Job successful!')
 
 
 if __name__ == "__main__":
