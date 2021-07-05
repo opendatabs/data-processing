8a5e8dc6e0dd6f52b4c96c66d2a99ac3f90ba9cd
jb3_2 <jonas.bieri@gmail.com>
2021-07-05 15:37:22 +0200

    Add regression test for etl_vmdl_imp_uebersicht

--- bag_coronavirus/tests/conftest.py
+++ bag_coronavirus/tests/conftest.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import pytest
-from bag_coronavirus.src import etl_impftermine, etl_vmdl_altersgruppen, vmdl
+from bag_coronavirus.src import etl_impftermine, etl_vmdl_altersgruppen, vmdl, etl_vmdl_impf_uebersicht
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 IMPFT_DF_FILE = os.path.join(CURR_DIR, 'fixtures', 'impftermine_df.pickle')
@@ -9,6 +9,7 @@ IMPFT_DF_AGG_FILE = os.path.join(CURR_DIR, 'fixtures', 'impftermine_df_agg.pickl
 VMDL_CSV_FILE = os.path.join(CURR_DIR, 'fixtures', 'vmdl.csv')
 VMDL_RAW_DF = os.path.join(CURR_DIR, 'fixtures', 'vmdl_df_bs_long_all.pickle')
 VMDL_REPORTING_DF = os.path.join(CURR_DIR, 'fixtures', 'vmdl_reporting_df.pickle')
+VMDL_IMPF_OVERVIEW_DF = os.path.join(CURR_DIR, 'fixtures', 'vmdl_overview_df.pickle')
 
 
 @pytest.fixture
@@ -16,24 +17,14 @@ def impft_df():
     return pd.read_pickle(IMPFT_DF_FILE)
 
 
-@pytest.fixture
-def impft_df_agg():
-    return pd.read_pickle(IMPFT_DF_AGG_FILE)
-
-
-@pytest.fixture
-def vmdl_raw_df():
-    return pd.read_pickle(VMDL_RAW_DF)
-
-
-@pytest.fixture
-def vmdl_reporting_df():
-    return pd.read_pickle(VMDL_REPORTING_DF)
-
-
 def persist_impft_df():
     df = etl_impftermine.load_data()
     df.to_pickle(IMPFT_DF_FILE)
+
+
+@pytest.fixture
+def impft_df_agg():
+    return pd.read_pickle(IMPFT_DF_AGG_FILE)
 
 
 def persist_impft_df_agg():
@@ -42,13 +33,19 @@ def persist_impft_df_agg():
     df_agg.to_pickle(IMPFT_DF_AGG_FILE)
 
 
-def persists_vmdl_csv():
-    vmdl.retrieve_vmdl_data(VMDL_CSV_FILE)
+@pytest.fixture
+def vmdl_raw_df():
+    return pd.read_pickle(VMDL_RAW_DF)
 
 
 def persist_vmdl_raw_df():
     df_bs_long_all = etl_vmdl_altersgruppen.get_raw_df(file_path=VMDL_CSV_FILE)
     df_bs_long_all.to_pickle(VMDL_RAW_DF)
+
+
+@pytest.fixture
+def vmdl_reporting_df():
+    return pd.read_pickle(VMDL_REPORTING_DF)
 
 
 def persist_vmdl_reporting_df():
@@ -57,10 +54,31 @@ def persist_vmdl_reporting_df():
     df_bs_perc.to_pickle(VMDL_REPORTING_DF)
 
 
+@pytest.fixture()
+def vmdl_impf_overview_df():
+    return pd.read_pickle(VMDL_IMPF_OVERVIEW_DF)
+
+
+def persist_vmdl_impf_overview_df():
+    df = etl_vmdl_impf_uebersicht.extract_data(VMDL_CSV_FILE)
+    export_df = etl_vmdl_impf_uebersicht.transform_data(df)
+    export_df.to_pickle(VMDL_IMPF_OVERVIEW_DF)
+
+
+def persists_vmdl_csv():
+    vmdl.retrieve_vmdl_data(VMDL_CSV_FILE)
+
+
+
+
+
+
+
 def main():
     # persists_vmdl_csv()
     # persist_vmdl_raw_df()
     # persist_vmdl_reporting_df()
+    persist_vmdl_impf_overview_df()
     pass
 
 
---
+++ bag_coronavirus/tests/test_regression_impf_uebersicht.py
@@ -0,0 +1,11 @@
+import pytest
+from bag_coronavirus.src import vmdl, etl_vmdl_impf_uebersicht
+import pandas as pd
+import conftest
+
+
+
+def test_regression_vmdl_raw(vmdl_impf_overview_df):
+    df = etl_vmdl_impf_uebersicht.extract_data(conftest.VMDL_CSV_FILE)
+    df_export = etl_vmdl_impf_uebersicht.transform_data(df)
+    assert vmdl_impf_overview_df.equals(df_export)
