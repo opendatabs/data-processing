a86ae4b96c541886d1f8b04dbb677e4e8127f79e
jb3_2 <jonas.bieri@gmail.com>
2021-07-06 15:19:11 +0200

    Only upload and publish to ods if transformed data has changed

--- bag_coronavirus/src/etl_vmdl_impf_uebersicht.py
+++ bag_coronavirus/src/etl_vmdl_impf_uebersicht.py
@@ -14,13 +14,17 @@ def main():
     vmdl_copy_path = vmdl.file_path().replace('vmdl.csv', 'vmdl_impf_uebersicht.csv')
     logging.info(f'Copying vmdl csv for this specific job to {vmdl_copy_path}...')
     shutil.copy(vmdl.file_path(), vmdl_copy_path)
-    if ct.has_changed(vmdl_copy_path):
+    if not ct.has_changed(vmdl_copy_path):
+        logging.info(f'Data have not changed, doing nothing for this dataset: {vmdl_copy_path}')
+    else:
         df = extract_data(vmdl_copy_path)
         df_export = transform_data(df)
-        load_data(df_export)
-        odsp.publish_ods_dataset_by_id('100111')
-    else:
-        logging.info(f'Data have not changed, doing nothing for this dataset: {vmdl_copy_path}')
+        export_file_name = load_data(df_export)
+        if not ct.has_changed(export_file_name):
+            logging.info(f'Data have not changed, doing nothing for this dataset: {export_file_name}')
+        else:
+            common.upload_ftp(export_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,'bag/vmdl')
+            odsp.publish_ods_dataset_by_id('100111')
     logging.info(f'Job successful!')
 
 
@@ -128,11 +132,11 @@ def transform_data(df):
     return df_export
 
 
-def load_data(df_export):
+def load_data(df_export: pd.DataFrame) -> str:
     export_file_name = os.path.join(credentials.vmdl_path, f'vaccination_report_bs.csv')
     logging.info(f'Exporting resulting data to {export_file_name}...')
     df_export.to_csv(export_file_name, index=False)
-    common.upload_ftp(export_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'bag/vmdl')
+    return export_file_name
 
 
 if __name__ == "__main__":
