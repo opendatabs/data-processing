67d42ba4a1b1ae43bf758d4391fc018d6a3e7988
jb3_2 <jonas.bieri@gmail.com>
2021-07-06 16:39:05 +0200

    Only upload and publish when data has changed

--- stata_daily_upload/etl.py
+++ stata_daily_upload/etl.py
@@ -1,21 +1,35 @@
+import logging
 import common
 import os
 from stata_daily_upload import credentials
+import common.change_tracking as ct
+import ods_publish.etl_id as odsp
 
-uploads = [{'file': 'Bevoelkerung/sterbefaelle.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/geburten_nach_datum.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Tourismus/tourismus-daily.csv', 'dest_dir': 'tourismus'},
-           {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus'},
-           {'file': 'Veranstaltung/veranstaltungen.csv', 'dest_dir': 'veranstaltungen'},
-           {'file': 'Bevoelkerung/01bevoelkerung_monat_nach_bezirk.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/02bevoelkerung_jahr_nach_CH_A_geschlecht.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/03bevoelkerung_jahr_nach_heimat_geschlecht.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/04bevoelkerung_jahr_nach_vorname.csv', 'dest_dir': 'bevoelkerung'},
-           {'file': 'Bevoelkerung/05bevoelkerung_jahr_nach_nachname.csv', 'dest_dir': 'bevoelkerung'},
-           ]
 
-for upload in uploads:
-    common.upload_ftp(os.path.join(credentials.path_work, upload['file']), credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
+def main():
+    uploads = [{'file': 'Bevoelkerung/sterbefaelle.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100079'},
+               {'file': 'Bevoelkerung/geburten_nach_datum.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100092'},
+               {'file': 'Bevoelkerung/geburten_nach_monat.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100099'},
+               {'file': 'Tourismus/tourismus-daily.csv', 'dest_dir': 'tourismus', 'ods_id': '100106'},
+               {'file': 'Tourismus/tourismus-monthly.csv', 'dest_dir': 'tourismus', 'ods_id': '100107'},
+               {'file': 'Veranstaltung/veranstaltungen.csv', 'dest_dir': 'veranstaltungen', 'ods_id': '100074'},
+               {'file': 'Bevoelkerung/01bevoelkerung_monat_nach_bezirk.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100125'},
+               {'file': 'Bevoelkerung/02bevoelkerung_jahr_nach_CH_A_geschlecht.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100128'},
+               {'file': 'Bevoelkerung/03bevoelkerung_jahr_nach_heimat_geschlecht.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100126'},
+               {'file': 'Bevoelkerung/04bevoelkerung_jahr_nach_vorname.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100129'},
+               {'file': 'Bevoelkerung/05bevoelkerung_jahr_nach_nachname.csv', 'dest_dir': 'bevoelkerung', 'ods_id': '100127'},
+               ]
+    for upload in uploads:
+        file_path = os.path.join(credentials.path_work, upload['file'])
+        if ct.has_changed(file_path):
+            common.upload_ftp(file_path, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
+            odsp.publish_ods_dataset_by_id(upload['ods_id'])
+        else:
+            logging.info(f'No changes detected, doing nothing for this dataset: {file_path}')
+    print('Job successful!')
 
-print('Job successful!')
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
