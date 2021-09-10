6a9c8500dd7984b4ff02c92aef8e669fc15c3c79
jb3_2 <jonas.bieri@gmail.com>
2021-09-10 15:15:25 +0200

    Auto-upload csv and publish ods dataset 100152

--- covid19bs/etl.sh
+++ covid19bs/etl.sh
@@ -1,4 +1,3 @@
 cd /code/data-processing || exit
-# pip freeze > /code/data-processing/covid19bs/requirements-in-docker.txt
-python3 -m covid19bs.etl_bs
+python3 -m covid19bs.etl_bs && python3 -m covid19bs.src.etl_copy_files
 
---
+++ covid19bs/src/etl_copy_files.py
@@ -0,0 +1,29 @@
+import logging
+import common
+import os
+from covid19bs import credentials
+import common.change_tracking as ct
+import ods_publish.etl_id as odsp
+
+
+def main():
+    uploads = [{'src_dir': credentials.path_orig, 'file': 'faelle_minderjaehrige_3j_klassen.csv', 'dest_dir': 'stata/covid19bs', 'ods_id': '100152'}
+               ]
+    upload_publish_if_changed(uploads)
+    print('Job successful!')
+
+
+def upload_publish_if_changed(uploads):
+    for upload in uploads:
+        file_path = os.path.join(upload['src_dir'], upload['file'])
+        if ct.has_changed(file_path):
+            common.upload_ftp(file_path, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, upload['dest_dir'])
+            odsp.publish_ods_dataset_by_id(upload['ods_id'])
+        else:
+            logging.info(f'No changes detected, doing nothing for this dataset: {file_path}')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
