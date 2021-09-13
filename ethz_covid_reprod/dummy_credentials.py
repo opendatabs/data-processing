6b8e28978eaf08986d455e79d8cab40a434637f2
Hester Pieters <42835455+HesterFrederiek@users.noreply.github.com>
2021-09-13 14:46:47 +0200

    Add files via upload

---
+++ ethz_covid_reprod/dummy_credentials.py
@@ -0,0 +1,10 @@
+proxies = {
+    "http": 'https://USER:PASSWORD@PROXYSERVER:PORT/',
+    "https": 'https://USER:PASSWORD@PROXYSERVER:PORT/'
+}
+
+path = '/Covid19_growth'
+
+file_name = "CHE-estimates.csv"
+
+file_name_new = 'CHE-estimates_with_growth.csv'
