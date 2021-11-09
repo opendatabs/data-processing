3f5128e6123e3c74c8f16f3ce81b7f843f539dde
jb3_2 <jonas.bieri@gmail.com>
2021-11-09 12:10:47 +0100

    Filter out empty tag, add code explanation

--- gva_geodatenshop/etl.py
+++ gva_geodatenshop/etl.py
@@ -232,7 +232,8 @@ for index, row in joined_data.iterrows():
                     'modified': modified,
                     'language': 'de',
                     'publizierende-organisation': row['publizierende_organisation'],
-                    'tags': ';'.join(list(set(row['tags'].split(';') + ['opendata.swiss', 'geoportal']))),
+                    # Concat tags from csv with list of fixed tags, remove duplicates by converting to set, remove empty string via filter()
+                    'tags': filter(None, ';'.join(list(set(row['tags'].split(';') + ['opendata.swiss', 'geoportal'])))),
                     'geodaten-modellbeschreibung': row['modellbeschreibung'],
                     'source_dataset': 'https://data-bs.ch/opendatasoft/harvesters/GVA/' + zipfilepath_relative,
                     'schema_file': schema_file
