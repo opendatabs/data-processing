c57d807af4d614750986904440d6d31aa10f145d
jb3_2 <jonas.bieri@gmail.com>
2021-12-07 10:55:43 +0100

    Rename test

--- bag_coronavirus/tests/test_regression_impf_uebersicht.py
+++ bag_coronavirus/tests/test_regression_impf_uebersicht.py
@@ -15,4 +15,3 @@ def test_new_cols_regression_vmdl_raw(vmdl_impf_overview_df):
     df_export = etl_vmdl_impf_uebersicht.transform_data(df)
     df_export_2 = df_export[vmdl_impf_overview_df.columns]
     assert vmdl_impf_overview_df.equals(df_export_2)
-
