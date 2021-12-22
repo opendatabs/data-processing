7a006fc09d2c9587be470f055c0fd37795e5d409
jb3_2 <jonas.bieri@gmail.com>
2021-12-22 09:33:36 +0100

    Re-introduce Clara

--- gsv_covid19_hosp/hospitalzahlen.py
+++ gsv_covid19_hosp/hospitalzahlen.py
@@ -156,7 +156,6 @@ if __name__ == "__main__":
     now_in_switzerland = datetime.now(timezone.utc).astimezone(ZoneInfo('Europe/Zurich'))
     date = now_in_switzerland.date()
     time_for_email = datetime(year=date.year, month=date.month, day=date.day, hour=9, minute=15)
-    # list_hospitals = ['USB', 'Clara', 'UKBB']
-    list_hospitals = ['USB', 'UKBB']
+    list_hospitals = ['USB', 'Clara', 'UKBB']
     all_together(date=date, list_hospitals=list_hospitals)
 
