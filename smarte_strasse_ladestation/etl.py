8cb5a4813e75356e4b39180bb890343a4088581f
jb3_2 <jonas.bieri@gmail.com>
2022-01-11 18:02:57 +0100

    Use common.requests_get and requests_post

--- smarte_strasse_ladestation/etl.py
+++ smarte_strasse_ladestation/etl.py
@@ -2,13 +2,14 @@ import logging
 import requests
 from smarte_strasse_ladestation import credentials
 import pandas as pd
+import common
 
 
 def main():
     token = authenticate()
     headers = {'authorization': f'Bearer {token}', 'x-api-key': credentials.api_key}
     # todo: define from url parameter so that we never get more than 1000 charges, see https://virta-partner-admin.api-docs.io/4.1/statistics-and-data-analysis
-    r = requests.request('GET', url=f'{credentials.charges_url}?from=2022-01-01T00%3A00%3A00%2B02%3A00', headers=headers)
+    r = common.requests_get(url=f'{credentials.charges_url}?from=2022-01-01T00%3A00%3A00%2B02%3A00', headers=headers)
     df = pd.json_normalize(r.json())
     pass
 
@@ -16,7 +17,7 @@ def main():
 def authenticate():
     payload = {'username': credentials.api_username, 'password': credentials.api_password}
     headers = {'x-api-key': credentials.api_key, 'content-type': 'application/json'}
-    r = requests.request('POST', credentials.auth_url, json=payload, headers=headers)
+    r = common.requests_post(url=credentials.auth_url, json=payload, headers=headers)
     return r.json()['token']
 
 
