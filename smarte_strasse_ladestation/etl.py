9ca9b39b9739288db55987e1c3ada284aed86c08
jb3_2 <jonas.bieri@gmail.com>
2022-01-11 17:57:50 +0100

    Retrieve charges

---
+++ smarte_strasse_ladestation/etl.py
@@ -0,0 +1,26 @@
+import logging
+import requests
+from smarte_strasse_ladestation import credentials
+import pandas as pd
+
+
+def main():
+    token = authenticate()
+    headers = {'authorization': f'Bearer {token}', 'x-api-key': credentials.api_key}
+    # todo: define from url parameter so that we never get more than 1000 charges, see https://virta-partner-admin.api-docs.io/4.1/statistics-and-data-analysis
+    r = requests.request('GET', url=f'{credentials.charges_url}?from=2022-01-01T00%3A00%3A00%2B02%3A00', headers=headers)
+    df = pd.json_normalize(r.json())
+    pass
+
+
+def authenticate():
+    payload = {'username': credentials.api_username, 'password': credentials.api_password}
+    headers = {'x-api-key': credentials.api_key, 'content-type': 'application/json'}
+    r = requests.request('POST', credentials.auth_url, json=payload, headers=headers)
+    return r.json()['token']
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
