dd44524196a8406574c11a9ce3c03698a527b2ca
jb3_2 <jonas.bieri@gmail.com>
2022-01-18 17:56:18 +0100

    Add API call to vehicle detection API

--- smarte_strasse_schall/etl.py
+++ smarte_strasse_schall/etl.py
@@ -1,5 +1,7 @@
 import logging
+import datetime
 import pandas as pd
+import common
 from smarte_strasse_schall import credentials
 import requests
 from requests.auth import HTTPBasicAuth
@@ -7,10 +9,21 @@ from requests.auth import HTTPBasicAuth
 
 def main():
     url = f'{credentials.url}api/devices/{credentials.device_id}'
-    r = requests.get(url=url, auth=HTTPBasicAuth(credentials.username, credentials.password), timeout=credentials.request_timeout)
+    auth = HTTPBasicAuth(credentials.username, credentials.password)
+    r = requests.get(url=url, auth=auth, timeout=credentials.request_timeout)
     r.raise_for_status()
     json = r.json()
     df = pd.json_normalize(r.json())
+
+    # todo: use local time zone
+    now = datetime.datetime.now()
+    end = now.isoformat()
+    start = (now - datetime.timedelta(hours=6)).isoformat()
+    r2 = common.requests_get(url=credentials.url + 'api/vehicle-detections', params={'start_time': start, 'size': '10000'}, auth=auth)
+    r2.raise_for_status()
+    json2 = r2.json()
+    df2 = pd.json_normalize(r2.json(), record_path='results')
+
     pass
 
 
