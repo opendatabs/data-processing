97cad14c67aea038b3fc86ac888efe09d2b97c60
jb3_2 <jonas.bieri@gmail.com>
2022-01-20 11:42:18 +0100

    Push vehicles to ODS

--- smarte_strasse_schall/etl.py
+++ smarte_strasse_schall/etl.py
@@ -1,29 +1,56 @@
 import logging
 import datetime
+from datetime import timezone
+from zoneinfo import ZoneInfo
+import numpy
 import pandas as pd
 import common
 from smarte_strasse_schall import credentials
-import requests
 from requests.auth import HTTPBasicAuth
 
 
 def main():
-    url = f'{credentials.url}api/devices/{credentials.device_id}'
     auth = HTTPBasicAuth(credentials.username, credentials.password)
-    r = requests.get(url=url, auth=auth, timeout=credentials.request_timeout)
-    r.raise_for_status()
-    json = r.json()
-    df = pd.json_normalize(r.json())
+    df_vehicles = push_vehicles(auth)
+    # df_sound_levels = push_sound_levels(auth)
+    pass
 
-    # todo: use local time zone
-    now = datetime.datetime.now()
+
+def push_vehicles(auth):
+    now = datetime.datetime.now(timezone.utc).astimezone(ZoneInfo('Europe/Zurich'))
     end = now.isoformat()
     start = (now - datetime.timedelta(hours=6)).isoformat()
-    r2 = common.requests_get(url=credentials.url + 'api/vehicle-detections', params={'start_time': start, 'size': '10000'}, auth=auth)
-    r2.raise_for_status()
-    json2 = r2.json()
-    df2 = pd.json_normalize(r2.json(), record_path='results')
+    r = common.requests_get(url=credentials.url + 'api/vehicle-detections', params={'start_time': start, 'size': '10000'}, auth=auth)
+    r.raise_for_status()
+    json = r.json()
+    df = pd.json_normalize(json, record_path='results')
+    df_vehicles = df[['localDateTime', 'classification']].copy(deep=True)
+    # todo: Retrieve real values for speed and sound level as soon as API provides them
+    df_vehicles['speed'] = numpy.NAN
+    df_vehicles['level'] = numpy.NAN
+    df_vehicles['timestamp_text'] = df_vehicles.localDateTime
+    common.ods_realtime_push_df(df_vehicles, credentials.ods_dataset_url, credentials.ods_push_key, credentials.ods_api_key)
+    # {
+    #     "localDateTime": "2022-01-19T08:17:13.896+01:00",
+    #     "classification": "UNKNOWN",
+    #     "timestamp_text": "2022-01-19T08:17:13.896+01:00",
+    #     "speed": 49.9,
+    #     "level": 50.1
+    # }
+    return df_vehicles
 
+
+# todo: Implement as soon as API is ready.
+def push_sound_levels(auth):
+    now = datetime.datetime.now(timezone.utc).astimezone(ZoneInfo('Europe/Zurich'))
+    end = now.isoformat()
+    start = (now - datetime.timedelta(hours=6)).isoformat()
+    r = common.requests_get(url=credentials.url + 'api/sound-levels', auth=auth, params={'start_time': start, 'size': '10000'})
+    # r = common.requests_get(url=credentials.url + 'api/sound-levels/aggs/avg', auth=auth, params={'start_time': start, 'size': '10000'})
+    r.raise_for_status()
+    json = r.json()
+    df = pd.json_normalize(json, record_path='results')
+    df_sound_levels = ''
     pass
 
 
