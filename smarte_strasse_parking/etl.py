ae338b78130ba60912ec17c060552e9735fe57dc
jb3_2 <jonas.bieri@gmail.com>
2022-02-03 17:24:45 +0100

    Adapt to new schema

--- smarte_strasse_parking/etl.py
+++ smarte_strasse_parking/etl.py
@@ -8,58 +8,71 @@ from smarte_strasse_parking import credentials
 
 def main():
     df1 = get_current_state_date()
-    common.ods_realtime_push_df(df=df1, url=credentials.ods_realtime_push_url_curr, push_key=credentials.ods_realtime_push_key_curr)
-    push_timeseries_data(df=df1, min_time_delta_minutes=60, url=credentials.ods_realtime_push_url_hist, push_key=credentials.ods_realtime_push_key_hist, api_key=credentials.ods_api_key)
+    common.ods_realtime_push_df(df1, url=credentials.ods_realtime_push_url_curr, push_key=credentials.ods_realtime_push_key_curr)
+    # common.ods_realtime_push_df(df=df1, url=credentials.ods_realtime_push_url_curr, push_key=credentials.ods_realtime_push_key_curr)
+    # push_timeseries_data(df=df1, min_time_delta_minutes=60, url=credentials.ods_realtime_push_url_hist, push_key=credentials.ods_realtime_push_key_hist, api_key=credentials.ods_api_key)
     logging.info(f'Job successful!')
 
 
 def get_current_state_date():
     logging.info(f'Retrieving current state data from API...')
-    r = common.requests_get(credentials.api1_url, auth=HTTPBasicAuth(credentials.api1_user, credentials.api1_pw))
+    df_spots = pd.DataFrame.from_dict(credentials.spots)
+    headers = {'Authorization': f'Bearer {credentials.api3_token}'}
+    r = common.requests_get(credentials.api3_url, headers=headers)
     json = r.json()
-
-    # showcase data
-    df = pd.json_normalize(r.json(), record_path='attributes', meta=['id', 'type'], sep='_')
+    df = pd.json_normalize(json, record_path='spots', meta='latest_timestamp')
     df['id'] = df.id.astype(int)
-    df_curr = df[['id', 'value_updated', 'value_label']]
+    df_curr = df[['latest_timestamp', 'id', 'status']]
+    df_merged = df_curr.merge(df_spots, on='id', how='left')
+    df_min = df_merged[['latest_timestamp', 'status', 'type']].rename(columns={'latest_timestamp': 'timestamp'})
+    df_count = df_min.groupby(['timestamp', 'status', 'type']).size().reset_index(name='count')
+    df_pivot = pd.pivot_table(df_count, values=['count'], index=['timestamp'], columns=['type', 'status'], aggfunc='sum')
+    df_wide = common.collapse_multilevel_column_names(df_pivot['count']).reset_index()
+    return df_wide
 
-    # 1 row per spot
-    # headers = {'Authorization': f'Bearer {credentials.api3_token}'}
-    # r = common.requests_get(credentials.api3_url, headers=headers)
-    # json = r.json()
-    # df = pd.json_normalize(json, record_path='spots', meta='latest_timestamp')
-    # df['id'] = df.id.astype(int)
-    # df_curr = df[['id', 'occupied', 'status']]
+    # {
+    #     "timestamp":"2022-02-03T16:21:10+00:00",
+    #     "Blue_available":1,
+    #     "Blue_occupied":3,
+    #     "Yellow_available":2,
+    #     "Yellow_occupied": 0
+    # }
+
 
     # 1 row in total
     # df = pd.json_normalize(json)
     # df_curr = df[['latest_timestamp', 'statistics.total.total_spots', 'statistics.total.occupied_spots', 'statistics.total.available_spots']]
+    # df_curr = df_curr.rename(columns={
+    #     'latest_timestamp': 'timestamp',
+    #     'statistics.total.total_spots': 'total',
+    #     'statistics.total.occupied_spots': 'occupied',
+    #     'statistics.total.available_spots': 'available'
+    # })
+    # return df_curr
 
-    return df_curr
 
-
-def push_timeseries_data(df, min_time_delta_minutes, url, push_key, api_key):
-    logging.info(f'Checking timestamp of latest entry in time series...')
-    latest_data_url = f'https://data.bs.ch/api/records/1.0/search/?dataset=100171&q=&rows=1&sort=-value_updated&apikey={api_key}'
-    r = common.requests_get(url=latest_data_url)
-    json = r.json()
-    results = len(json['records'])
-    delta_minutes = -1
-    if results > 0:
-        latest_ods_record = datetime.datetime.fromisoformat(json['records'][0]['fields']['value_updated'])
-        current_df_time = datetime.datetime.strptime(df.value_updated.max(), '%Y-%m-%dT%H:%M:%S.%f%z')
-        delta_minutes = (current_df_time - latest_ods_record).seconds / 60
-    if results == 0 or delta_minutes >= min_time_delta_minutes:
-        logging.info(f'Pushing data time series dataset (minutes since last entry: {delta_minutes}).')
-        # Realtime API bootstrap data:
-        # {
-        #     "id": 0,
-        #     "value_updated": "2022-01-14T08:44:56.000Z",
-        #     "value_label": "1"
-        # }
-        common.ods_realtime_push_df(df, url, push_key)
-    else:
-        logging.info(f"It's not time yet to push into time series dataset (minutes since last entry: {delta_minutes}).")
+# def push_timeseries_data(df, min_time_delta_minutes, url, push_key, api_key):
+#     logging.info(f'Checking timestamp of latest entry in time series...')
+#     latest_data_url = f'https://data.bs.ch/api/records/1.0/search/?dataset=100171&q=&rows=1&sort=-value_updated&apikey={api_key}'
+#     r = common.requests_get(url=latest_data_url)
+#     json = r.json()
+#     results = len(json['records'])
+#     delta_minutes = -1
+#     if results > 0:
+#         latest_ods_record = datetime.datetime.fromisoformat(json['records'][0]['fields']['value_updated'])
+#         current_df_time = datetime.datetime.strptime(df.value_updated.max(), '%Y-%m-%dT%H:%M:%S.%f%z')
+#         delta_minutes = (current_df_time - latest_ods_record).seconds / 60
+#     if results == 0 or delta_minutes >= min_time_delta_minutes:
+#         logging.info(f'Pushing data time series dataset (minutes since last entry: {delta_minutes}).')
+#         # Realtime API bootstrap data:
+#         # {
+#         #     "id": 0,
+#         #     "value_updated": "2022-01-14T08:44:56.000Z",
+#         #     "value_label": "1"
+#         # }
+#         common.ods_realtime_push_df(df, url, push_key)
+#     else:
+#         logging.info(f"It's not time yet to push into time series dataset (minutes since last entry: {delta_minutes}).")
 
 
 # def push_detailed_historical_data():
