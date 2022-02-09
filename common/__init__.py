4d846e0bcddb01990708730331e6f715852caee3
jb3_2 <jonas.bieri@gmail.com>
2022-02-09 11:36:36 +0100

    Add email_message()

--- common/__init__.py
+++ common/__init__.py
@@ -8,6 +8,10 @@ from functools import wraps
 import pandas as pd
 import fnmatch
 import logging
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 from datetime import datetime, timedelta, timezone
 # see https://pypi.org/project/backports.zoneinfo/
 try:
@@ -240,3 +244,42 @@ def collapse_multilevel_column_names(df: pd.DataFrame, sep='_'):
     # Replace the 2-level column names with a string that concatenates both strings
     df.columns = [sep.join(str(c) for c in col) for col in df.columns.values]
     return df
+
+
+# copied from: https://towardsdatascience.com/automate-email-with-python-1e755d9c6276
+def email_message(subject="Python Notification", text="", img=None, attachment=None):
+    # build message contents
+    msg = MIMEMultipart()
+    msg['Subject'] = subject  # add in the subject
+    #msg.attach(MIMEText(text))  # add text contents
+    msg.attach(MIMEText(text, 'plain', 'utf-8'))  # add plain text contents
+
+    # check if we have anything given in the img parameter
+    if img is not None:
+        # if we do, we want to iterate through the images, so let's check that
+        # what we have is actually a list
+        if type(img) is not list:
+            img = [img]  # if it isn't a list, make it one
+        # now iterate through our list
+        for one_img in img:
+            img_data = open(one_img, 'rb').read()  # read the image binary data
+            # attach the image data to MIMEMultipart using MIMEImage, we add
+            # the given filename use os.basename
+            msg.attach(MIMEImage(img_data, name=os.path.basename(one_img)))
+
+    # we do the same for attachments as we did for images
+    if attachment is not None:
+        if type(attachment) is not list:
+            attachment = [attachment]  # if it isn't a list, make it one
+
+        for one_attachment in attachment:
+            with open(one_attachment, 'rb') as f:
+                # read in the attachment using MIMEApplication
+                file = MIMEApplication(
+                    f.read(),
+                    name=os.path.basename(one_attachment)
+                )
+            # here we edit the attached file metadata
+            file['Content-Disposition'] = f'attachment; filename="{os.path.basename(one_attachment)}"'
+            msg.attach(file)  # finally, add the attachment to our message object
+    return msg
