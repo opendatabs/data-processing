e406323dcadef8489889dba014c1da6ccc737936
HesterFrederiek <hesterpieters@gmail.com>
2022-02-17 16:27:14 +0100

    define columns dataframe

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -5,3 +5,8 @@ import logging
 from common import change_tracking as ct
 
 
+columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Gewässercode', 'Fischart', 'Gewicht',
+           'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
+           'Abfluss_Rhein_über_1800m3', 'Bemerkungen']
+
+df = pd.DataFrame(columns=columns)
