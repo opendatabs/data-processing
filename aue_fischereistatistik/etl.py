c7198c4aa29067bd28240cff62886132871d62ec
hesterfrederiek <hesterpieters@gmail.com>
2022-02-18 12:32:00 +0100

    import all data

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -4,14 +4,23 @@ import common
 import logging
 from common import change_tracking as ct
 
+pd.set_option('display.max_columns', None)
 
-columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Year','Gewässercode', 'Fischart', 'Gewicht',
+columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Jahr','Gewässercode', 'Fischart', 'Gewicht',
            'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
            'Abfluss_Rhein_über_1800m3', 'Bemerkungen']
 
 df = pd.DataFrame(columns=columns)
 
-path = credentials.path_2020
-df_2020 = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
-pd.set_option('display.max_columns', None)
-print(df_2020)
+
+for year in range(2010,2021):
+    year = str(year)
+    path = f'{credentials.path_csv}/fangstatistik_{year}.csv'
+    df_year = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
+    df_year['Jahr'] = year
+    # Month as a zero-padded decimal number
+    # probably remove column Bemerkungen
+    # remove column Datum? Else need to put all in same format (day?)
+    # replace 0 by empty string
+    df = pd.concat([df, df_year])
+
