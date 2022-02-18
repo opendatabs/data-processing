9419b4d15a6671e43dd0671db660608a8f7c68d6
hesterfrederiek <hesterpieters@gmail.com>
2022-02-18 11:08:11 +0100

    Add year to columns

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -5,8 +5,13 @@ import logging
 from common import change_tracking as ct
 
 
-columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Gewässercode', 'Fischart', 'Gewicht',
+columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Year','Gewässercode', 'Fischart', 'Gewicht',
            'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
            'Abfluss_Rhein_über_1800m3', 'Bemerkungen']
 
 df = pd.DataFrame(columns=columns)
+
+path = credentials.path_2020
+df_2020 = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
+pd.set_option('display.max_columns', None)
+print(df_2020)
