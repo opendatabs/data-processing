01fa2a50fb578dbc4fba6afc82f13de54136133f
hesterfrederiek <hesterpieters@gmail.com>
2022-02-21 10:33:20 +0100

    Add to do

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -6,6 +6,7 @@ from common import change_tracking as ct
 
 pd.set_option('display.max_columns', None)
 
+
 columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Jahr','Gewässercode', 'Fischart', 'Gewicht',
            'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
            'Abfluss_Rhein_über_1800m3', 'Bemerkungen']
@@ -18,9 +19,9 @@ for year in range(2010,2021):
     path = f'{credentials.path_csv}/fangstatistik_{year}.csv'
     df_year = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
     df_year['Jahr'] = year
-    # Month as a zero-padded decimal number
-    # probably remove column Bemerkungen
+    # Month as a zero-padded decimal number or written out
+    # need to correct in month column: 'juli' 'juö' 'ap' '3' 'mai' '0' ''
+    # probably remove column Bemerkungen (or remove all personal info)
     # remove column Datum? Else need to put all in same format (day?)
     # replace 0 by empty string in some columns
     df = pd.concat([df, df_year])
-
