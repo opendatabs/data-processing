de558d636d5661b64d8bcb8b49685327f76d4d65
hesterfrederiek <hesterpieters@gmail.com>
2022-02-21 13:13:35 +0100

    Correct typos month column, convert to zero-padded decimal numbers

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -4,6 +4,7 @@ import common
 import logging
 from common import change_tracking as ct
 import locale
+from datetime import datetime
 
 pd.set_option('display.max_columns', None)
 # datetime in German
@@ -21,14 +22,25 @@ for year in range(2010,2021):
     path = f'{credentials.path_csv}/fangstatistik_{year}.csv'
     df_year = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
     df_year['Jahr'] = year
-
-    # Complete month column all in same format
-    # need to correct in month column: 'juli' 'juö' 'ap' '3' 'mai' '0' ''
-    # Month as a zero-padded decimal number
-
-
-
-    # probably remove column Bemerkungen (or remove all personal info)
-    # remove column Datum? Else need to put all in same format (day?)
-    # replace 0 by empty string in some columns
     df = pd.concat([df, df_year])
+
+
+
+# probably remove column Bemerkungen (or remove all personal info)
+# remove column Datum? Else need to put all in same format (day?)
+# replace 0 by empty string in some columns
+
+
+
+# Complete month column all in same format
+# need to correct in month column: 'juli' 'juö' 'ap' '3' 'mai' '0' ''
+df['Monat'].replace('0','', inplace=True)
+df['Monat'].replace('juli', 'Juli', inplace=True)
+df['Monat'].replace('ap', 'April', inplace=True)
+df['Monat'].replace('mai', 'Mai', inplace=True)
+df['Monat'].replace('3', 'März', inplace=True)
+df['Monat'].replace('juö', 'Juli', inplace=True)
+
+# change month names to zero-padded decimal numbers
+df['Monat'] = df['Monat'].apply(lambda x: datetime.strptime(x, '%B') if type(x) == str and x != '' else pd.NaT)
+df['Monat'] = pd.to_datetime(df['Monat']).dt.strftime('%m')
