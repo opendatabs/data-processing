072ef08e8f4a0a546105cc1313ce5861f0df2d66
hesterfrederiek <hesterpieters@gmail.com>
2022-02-25 11:50:33 +0100

    Remove unused import statements

--- gsv_covid19_hosp_auto/update_coreport.py
+++ gsv_covid19_hosp_auto/update_coreport.py
@@ -1,11 +1,9 @@
 
 from datetime import timezone, datetime
 import logging
-import pandas as pd
 import common
 from gsv_covid19_hosp_auto import credentials
 from gsv_covid19_hosp_auto import calculation
-from gsv_covid19_hosp_auto import hospitalzahlen
 from gsv_covid19_hosp_auto import send_email2
 from zoneinfo import ZoneInfo
 
