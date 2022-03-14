f8ce79db47ca244f45aadf0dbf30eb92cdf193a8
hesterfrederiek <hesterpieters@gmail.com>
2022-03-14 15:03:29 +0100

    change function/variable names to lower case

--- gsv_covid19_hosp_auto/calculation.py
+++ gsv_covid19_hosp_auto/calculation.py
@@ -2,68 +2,71 @@
 import pandas as pd
 
 
-def add_discrepancy_in_ICU_beds_without_vent(ies_numbers):
+def add_discrepancy_in_icu_beds_without_vent(ies_numbers):
     df = ies_numbers
-    Total_IS_Pat = df['TotalIcuPats']
-    Beatmete_IS_Pat = df['VentIcuPats']
-    Betriebene_IS_Betten = df['OperIcuBeds']
-    Beatmete_IS_Betten = df['VentIcuBeds']
-    Betriebene_IS_Betten_ohne_Beatmung = Betriebene_IS_Betten - Beatmete_IS_Betten
-    df['C'] = Betriebene_IS_Betten_ohne_Beatmung - Total_IS_Pat + Beatmete_IS_Pat
+    total_is_pat = df['TotalIcuPats']
+    beatmete_is_pat = df['VentIcuPats']
+    betriebene_is_betten = df['OperIcuBeds']
+    beatmete_is_betten = df['VentIcuBeds']
+    betriebene_is_betten_ohne_beatmung = betriebene_is_betten - beatmete_is_betten
+    df['C'] = betriebene_is_betten_ohne_beatmung - total_is_pat + beatmete_is_pat
     return df
 
 
-def Betten_frei_IPS_ohne_Beatmung(df):
+def betten_frei_ips_ohne_beatmung(df):
     if df['C'] < 0:
         return 0
     else:
         return df['C']
 
 
-def Betten_frei_IPS_mit_Beatmung(df):
-    Beatmete_IS_Pat = df['VentIcuPats']
-    Beatmete_IS_Betten = df['VentIcuBeds']
+def betten_frei_ips_mit_beatmung(df):
+    beatmete_is_pat = df['VentIcuPats']
+    beatmete_is_betten = df['VentIcuBeds']
     if df['C'] < 0:
-        return Beatmete_IS_Betten - Beatmete_IS_Pat + df['C']
+        return beatmete_is_betten - beatmete_is_pat + df['C']
     else:
-        return Beatmete_IS_Betten - Beatmete_IS_Pat
+        return beatmete_is_betten - beatmete_is_pat
 
 
 def calculate_numbers(ies_numbers):
-    df = add_discrepancy_in_ICU_beds_without_vent(ies_numbers)
+    df = add_discrepancy_in_icu_beds_without_vent(ies_numbers)
     df_coreport = pd.DataFrame()
     df_coreport[["Hospital", "NoauResid", "CapacDate", "CapacTime"]] = \
         df[["Hospital", "NoauResid", "CapacDate", "CapacTime"]]
 
-    Total_Betten = df['TotalAllBeds']
-    Total_Betten_COVID = df['TotalAllBedsC19']
-    Betriebene_IS_Betten = df['OperIcuBeds']
-    Betriebene_IS_Betten_COVID = df['OperIcuBedsC19']
-    Beatmete_IS_Betten = df['VentIcuBeds']
-    Betriebene_IMCU_Betten = df['OperImcBeds']
-    Betriebene_IMCU_Betten_COVID = df['OperImcBedsC19']
-    Total_Pat = df['TotalAllPats']
-    Total_Pat_COVID = df['TotalAllPatsC19']
-    Total_IS_Pat = df['TotalIcuPats']
-    Total_IS_Pat_COVID = df['TotalIcuPatsC19']
-    Beatmete_IS_Pat = df['VentIcuPats']
-    Total_IMCU_Pat = df['TotalImcPats']
-    Total_IMCU_Pat_COVID = df['TotalImcPatsC19']
-    Betriebene_ECMO_Betten = 5  # not available (for USB) at the moment, we take ECMO = 5...
-    Total_ECMO_Pat = df['EcmoPats']
-
+    total_betten = df['TotalAllBeds']
+    total_betten_covid = df['TotalAllBedsC19']
+    betriebene_is_betten = df['OperIcuBeds']
+    betriebene_is_betten_covid = df['OperIcuBedsC19']
+    # Beatmete_IS_Betten = df['VentIcuBeds']
+    betriebene_imcu_betten = df['OperImcBeds']
+    betriebene_imcu_betten_covid = df['OperImcBedsC19']
+    total_pat = df['TotalAllPats']
+    total_pat_covid = df['TotalAllPatsC19']
+    total_is_pat = df['TotalIcuPats']
+    total_is_pat_covid = df['TotalIcuPatsC19']
+    beatmete_is_pat = df['VentIcuPats']
+    total_imcu_pat = df['TotalImcPats']
+    total_imcu_pat_covid = df['TotalImcPatsC19']
+    betriebene_ecmo_betten = 5  # not available (for USB) at the moment, we take ECMO = 5...
+    total_ecmo_pat = df['EcmoPats']
 
     # numbers to return
-    df_coreport['Bettenanzahl frei "Normalstation" COVID'] = Total_Betten_COVID - Betriebene_IS_Betten_COVID - Betriebene_IMCU_Betten_COVID - Total_Pat_COVID + Total_IS_Pat_COVID + Total_IMCU_Pat_COVID
-    df_coreport['Bettenanzahl frei "Normalstation"'] = Total_Betten - Betriebene_IS_Betten - Betriebene_IMCU_Betten - Total_Pat + Total_IS_Pat + Total_IMCU_Pat - df_coreport['Bettenanzahl frei "Normalstation" COVID']
-    df_coreport['Bettenanzahl frei "IMCU"'] = Betriebene_IMCU_Betten - Total_IMCU_Pat
-    df_coreport['Bettenanzahl frei "IPS ohne Beatmung"'] = df.apply(Betten_frei_IPS_ohne_Beatmung, axis=1)
-    df_coreport['Bettenanzahl frei "IPS mit Beatmung"'] = df.apply(Betten_frei_IPS_mit_Beatmung, axis=1)
-    df_coreport['Bettenanzahl frei " IPS ECMO"'] = Betriebene_ECMO_Betten - Total_ECMO_Pat
-    df_coreport['Bettenanzahl belegt "Normalstation"'] = Total_Pat - Total_IS_Pat - Total_IMCU_Pat
-    df_coreport['Bettenanzahl belegt "IMCU"'] = Total_IMCU_Pat
-    df_coreport['Bettenanzahl belegt "IPS ohne Beatmung"'] = Total_IS_Pat - Beatmete_IS_Pat
-    df_coreport['Bettenanzahl belegt "IPS mit Beatmung"'] = Beatmete_IS_Pat
-    df_coreport['Bettenanzahl belegt "IPS ECMO"'] = Total_ECMO_Pat
+    df_coreport['Bettenanzahl frei "Normalstation" COVID'] = \
+        total_betten_covid - betriebene_is_betten_covid - betriebene_imcu_betten_covid \
+        - total_pat_covid + total_is_pat_covid + total_imcu_pat_covid
+    df_coreport['Bettenanzahl frei "Normalstation"'] = \
+        total_betten - betriebene_is_betten - betriebene_imcu_betten \
+        - total_pat + total_is_pat + total_imcu_pat - df_coreport['Bettenanzahl frei "Normalstation" COVID']
+    df_coreport['Bettenanzahl frei "IMCU"'] = betriebene_imcu_betten - total_imcu_pat
+    df_coreport['Bettenanzahl frei "IPS ohne Beatmung"'] = df.apply(betten_frei_ips_ohne_beatmung, axis=1)
+    df_coreport['Bettenanzahl frei "IPS mit Beatmung"'] = df.apply(betten_frei_ips_mit_beatmung, axis=1)
+    df_coreport['Bettenanzahl frei " IPS ECMO"'] = betriebene_ecmo_betten - total_ecmo_pat
+    df_coreport['Bettenanzahl belegt "Normalstation"'] = total_pat - total_is_pat - total_imcu_pat
+    df_coreport['Bettenanzahl belegt "IMCU"'] = total_imcu_pat
+    df_coreport['Bettenanzahl belegt "IPS ohne Beatmung"'] = total_is_pat - beatmete_is_pat
+    df_coreport['Bettenanzahl belegt "IPS mit Beatmung"'] = beatmete_is_pat
+    df_coreport['Bettenanzahl belegt "IPS ECMO"'] = total_ecmo_pat
 
     return df_coreport
