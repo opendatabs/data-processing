4d0c63fb8cef539451d7fe8e82989dc1da2f4797
hesterfrederiek <hesterpieters@gmail.com>
2022-03-15 14:33:30 +0100

    Remove print filter

--- gsv_covid19_hosp_BS/get_data.py
+++ gsv_covid19_hosp_BS/get_data.py
@@ -63,10 +63,3 @@ def get_dataframe(hospital, date):
                  'TotalImcPatsC19', 'EcmoPats']]
         df["Hospital"] = hospital
     return df
-
-if __name__ == "__main__":
-    from zoneinfo import ZoneInfo
-    from datetime import timezone, datetime, timedelta
-    now_in_switzerland = datetime.now(timezone.utc).astimezone(ZoneInfo('Europe/Zurich'))
-    date = now_in_switzerland.date()
-    print(get_filter('Clara', date))
