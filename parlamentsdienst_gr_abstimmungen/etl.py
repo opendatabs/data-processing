c0eb8327ba3efa83339e9232c6d477db5e190df3
jb3_2 <jonas.bieri@gmail.com>
2022-03-23 13:01:07 +0100

    Process data

--- parlamentsdienst_gr_abstimmungen/etl.py
+++ parlamentsdienst_gr_abstimmungen/etl.py
@@ -1,8 +1,14 @@
 import logging
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import pandas as pd
 import xml
+import common
 from xml.sax.handler import ContentHandler
 from parlamentsdienst_gr_abstimmungen import credentials
+import common.change_tracking as ct
+import ods_publish.etl_id as odsp
 
 
 # see https://stackoverflow.com/a/33504236
@@ -35,13 +41,35 @@ class ExcelHandler(ContentHandler):
 
 
 def main():
-    excel_handler = ExcelHandler()
-    xml.sax.parse(credentials.file_name, excel_handler)
-    polls = pd.DataFrame(excel_handler.tables[1][1:], columns=excel_handler.tables[1][0])
-    details = pd.DataFrame(excel_handler.tables[0][1:101], columns=excel_handler.tables[0][0])
-    sums_per_decision = pd.DataFrame(excel_handler.tables[0][101:107], columns=excel_handler.tables[0][0])
-    timestamp = excel_handler.tables[0][108][1]
-    pass
+    listing = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, '', credentials.local_data_path, '*.xml')
+    for file in listing:
+        local_file = file['local_file']
+        if ct.has_changed(local_file, do_update_hash_file=False):
+            session_date = os.path.basename(local_file).split('_')[0]
+            excel_handler = ExcelHandler()
+            xml.sax.parse(local_file, excel_handler)
+            polls = pd.DataFrame(excel_handler.tables[1][1:], columns=excel_handler.tables[1][0])
+            details = pd.DataFrame(excel_handler.tables[0][1:101], columns=excel_handler.tables[0][0])
+            sums_per_decision = pd.DataFrame(excel_handler.tables[0][101:107], columns=excel_handler.tables[0][0])
+            data_timestamp = datetime.strptime(excel_handler.tables[0][108][1], '%Y-%m-%dT%H:%M:%S').astimezone(ZoneInfo('Europe/Zurich'))
+
+            polls['Zeitstempel_text'] = polls.Zeit
+            polls['Zeitstempel'] = pd.to_datetime(polls.Zeit, format='%Y-%m-%dT%H:%M:%S.%f').dt.tz_localize('Europe/Zurich')
+            polls[['Datum', 'Zeit']] = polls.Datum.str.split('T', expand=True)
+            polls = polls.rename(columns={'Nr': 'Abst_Nr'})
+
+            details['Datum'] = session_date[:4] + '-' + session_date[4:6] + '-' + session_date[6:8]
+            details.columns.values[0] = 'Sitz_Nr'
+            details.columns.values[1] = 'Mitglied_Name_Fraktion'
+            details['Fraktion'] = details.Mitglied_Name_Fraktion.str.extract(r"\(([^)]+)\)", expand=False)
+            details['Mitglied_Name'] = details.Mitglied_Name_Fraktion.str.split('(', expand=True)[[0]]
+            details['Datenstand'] = pd.to_datetime(data_timestamp.isoformat())
+            details_long = details.melt(id_vars=['Sitz_Nr', 'Mitglied_Name', 'Fraktion', 'Mitglied_Name_Fraktion', 'Datum', 'Datenstand'], var_name='Abst_Nr', value_name='Entscheid')
+
+            all_df = polls.merge(details_long, how='left', left_on=['Datum', 'Abst_Nr'], right_on=['Datum', 'Abst_Nr'])
+
+            pass
+            # ct.update_hash_file(local_file)
 
 
 if __name__ == "__main__":
