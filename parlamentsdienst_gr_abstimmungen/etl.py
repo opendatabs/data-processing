724e84d3c52349398b06788e9e6990e1e391cec8
jb3_2 <jonas.bieri@gmail.com>
2022-03-25 11:41:34 +0100

    Handle missing ics file, check changes in ftp ls file first

--- parlamentsdienst_gr_abstimmungen/etl.py
+++ parlamentsdienst_gr_abstimmungen/etl.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import xml
@@ -7,8 +8,6 @@ from zoneinfo import ZoneInfo
 # import ods_publish.etl_id as odsp
 import icalendar
 import pandas as pd
-import requests
-
 import common
 import common.change_tracking as ct
 from parlamentsdienst_gr_abstimmungen import credentials
@@ -55,13 +54,14 @@ def is_file_older_than(file, delta):
     return True if mtime < cutoff else False
 
 
-def is_session_now():
+def is_session_now(cutoff=timedelta(hours=12)):
+    # todo: Handle case where session takes longer than defined in calendar event
     logging.info(f'Checking if we should reload the ical file from the web...')
     ical_file_path = credentials.ics_file
-    if is_file_older_than(ical_file_path, timedelta(hours=12)):
-        logging.info(f'Opening Google Calendar from web...')
+    if not os.path.exists(ical_file_path) or is_file_older_than(ical_file_path, cutoff):
+        logging.info(f'Ical file does not exist or is older than required - opening Google Calendar from web...')
         url = 'https://calendar.google.com/calendar/ical/vfb9bndssqs2v9uiun9uk7hkl8%40group.calendar.google.com/public/basic.ics'
-        r = requests.get(url=url, allow_redirects=True)
+        r = common.requests_get(url=url, allow_redirects=True)
         with open(ical_file_path, 'wb') as f:
             f.write(r.content)
     # see https://stackoverflow.com/a/26329138
@@ -78,36 +78,43 @@ def is_session_now():
 
 
 def handle_polls():
-    listing = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, '', credentials.local_data_path, '*.xml')
-    # todo: check if any file has changed on FT Server, e.g. by saving listing into text file and checking for changes in content (hash)
-    for file in listing:
-        local_file = file['local_file']
-        if ct.has_changed(local_file, do_update_hash_file=False):
-            session_date = os.path.basename(local_file).split('_')[0]
-            excel_handler = ExcelHandler()
-            xml.sax.parse(local_file, excel_handler)
-            polls = pd.DataFrame(excel_handler.tables[1][1:], columns=excel_handler.tables[1][0])
-            details = pd.DataFrame(excel_handler.tables[0][1:101], columns=excel_handler.tables[0][0])
-            sums_per_decision = pd.DataFrame(excel_handler.tables[0][101:107], columns=excel_handler.tables[0][0])
-            data_timestamp = datetime.strptime(excel_handler.tables[0][108][1], '%Y-%m-%dT%H:%M:%S').astimezone(ZoneInfo('Europe/Zurich'))
+    ftp_ls = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, '', credentials.local_data_path, '*.xml', list_only=True)
+    logging.info(f'Saving ftp ls file to {credentials.ftp_ls_file}...')
+    json.dump(ftp_ls, open(credentials.ftp_ls_file, 'w'), indent=1)
+    if ct.has_changed(credentials.ftp_ls_file, do_update_hash_file=False):
+        xml_files = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, '', credentials.local_data_path, '*.xml')
+        for file in xml_files:
+            local_file = file['local_file']
+            if ct.has_changed(local_file, do_update_hash_file=False):
+                session_date = os.path.basename(local_file).split('_')[0]
+                excel_handler = ExcelHandler()
+                xml.sax.parse(local_file, excel_handler)
+                polls = pd.DataFrame(excel_handler.tables[1][1:], columns=excel_handler.tables[1][0])
+                details = pd.DataFrame(excel_handler.tables[0][1:101], columns=excel_handler.tables[0][0])
+                sums_per_decision = pd.DataFrame(excel_handler.tables[0][101:107], columns=excel_handler.tables[0][0])
+                data_timestamp = datetime.strptime(excel_handler.tables[0][108][1], '%Y-%m-%dT%H:%M:%S').astimezone(ZoneInfo('Europe/Zurich'))
 
-            polls['Zeitstempel_text'] = polls.Zeit
-            polls['Zeitstempel'] = pd.to_datetime(polls.Zeit, format='%Y-%m-%dT%H:%M:%S.%f').dt.tz_localize('Europe/Zurich')
-            polls[['Datum', 'Zeit']] = polls.Datum.str.split('T', expand=True)
-            polls = polls.rename(columns={'Nr': 'Abst_Nr', 'J': 'Anz_J', 'N': 'Anz_N', 'E': 'Anz_E', 'A': 'Anz_A', 'P': 'Anz_P'})
+                polls['Zeitstempel_text'] = polls.Zeit
+                polls['Zeitstempel'] = pd.to_datetime(polls.Zeit, format='%Y-%m-%dT%H:%M:%S.%f').dt.tz_localize('Europe/Zurich')
+                polls[['Datum', 'Zeit']] = polls.Datum.str.split('T', expand=True)
+                polls = polls.rename(columns={'Nr': 'Abst_Nr', 'J': 'Anz_J', 'N': 'Anz_N', 'E': 'Anz_E', 'A': 'Anz_A', 'P': 'Anz_P'})
 
-            details['Datum'] = session_date[:4] + '-' + session_date[4:6] + '-' + session_date[6:8]
-            details.columns.values[0] = 'Sitz_Nr'
-            details.columns.values[1] = 'Mitglied_Name_Fraktion'
-            details['Fraktion'] = details.Mitglied_Name_Fraktion.str.extract(r"\(([^)]+)\)", expand=False)
-            details['Mitglied_Name'] = details.Mitglied_Name_Fraktion.str.split('(', expand=True)[[0]]
-            details['Datenstand'] = pd.to_datetime(data_timestamp.isoformat())
-            details_long = details.melt(id_vars=['Sitz_Nr', 'Mitglied_Name', 'Fraktion', 'Mitglied_Name_Fraktion', 'Datum', 'Datenstand'], var_name='Abst_Nr', value_name='Entscheid_Mitglied')
+                details['Datum'] = session_date[:4] + '-' + session_date[4:6] + '-' + session_date[6:8]
+                details.columns.values[0] = 'Sitz_Nr'
+                details.columns.values[1] = 'Mitglied_Name_Fraktion'
+                details['Fraktion'] = details.Mitglied_Name_Fraktion.str.extract(r"\(([^)]+)\)", expand=False)
+                details['Mitglied_Name'] = details.Mitglied_Name_Fraktion.str.split('(', expand=True)[[0]]
+                details['Datenstand'] = pd.to_datetime(data_timestamp.isoformat())
+                details_long = details.melt(id_vars=['Sitz_Nr', 'Mitglied_Name', 'Fraktion', 'Mitglied_Name_Fraktion', 'Datum', 'Datenstand'], var_name='Abst_Nr', value_name='Entscheid_Mitglied')
 
-            all_df = polls.merge(details_long, how='left', left_on=['Datum', 'Abst_Nr'], right_on=['Datum', 'Abst_Nr'])
+                all_df = polls.merge(details_long, how='left', left_on=['Datum', 'Abst_Nr'], right_on=['Datum', 'Abst_Nr'])
+                print(all_df)
+                pass
 
-            pass
-            # ct.update_hash_file(local_file)
+                # todo: Push data to ODS dataset
+
+                # ct.update_hash_file(local_file)
+        # ct.update_hash_file(credentials.ftp_ls_file)
 
 
 if __name__ == "__main__":
