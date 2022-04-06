9e676fd8ea877a8857ac40c5c3b84f11d4a5de2c
jb3_2 <jonas.bieri@gmail.com>
2022-04-06 15:48:33 +0200

    Remove unused etl.sh files

--- aue_umweltlabor/etl.sh
+++
@@ -1,5 +0,0 @@
-cd /code/data-processing || exit
-# pip freeze > /code/data-processing/aue_umweltlabor/requirements-in-docker.txt
-# python3 -m aue_umweltlabor.etl no_file_copy
-python3 -m aue_umweltlabor.etl
-
--- bafu_hydrodaten/etl.sh
+++
@@ -1,4 +0,0 @@
-cd /code/data-processing || exit
-# pip freeze > /code/data-processing/aue_schall/requirements-in-docker.txt
-python3 -m bafu_hydrodaten.etl_https
-
