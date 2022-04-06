916edce05c5cf10b717c24a9506cdd8662b5a01d
jb3_2 <jonas.bieri@gmail.com>
2022-04-06 17:54:54 +0200

    Use common.requests_post

--- bag_coronavirus/src/vmdl.py
+++ bag_coronavirus/src/vmdl.py
@@ -25,7 +25,7 @@ def retrieve_vmdl_data(csv_filename: str = '') -> str:
     payload_token = f'client_id={credentials.vmdl_client_id}&scope={credentials.vmdl_scope}&username={credentials.vmdl_user}&password={credentials.vmdl_password}&grant_type=password'
     headers_token = {'Content-Type': 'application/x-www-form-urlencoded'}
     print(f'Getting OAUTH2 access token...')
-    resp_token = requests.request("POST", credentials.vmdl_url_token, headers=headers_token, data=payload_token)
+    resp_token = common.requests_post(credentials.vmdl_url_token, headers=headers_token, data=payload_token)
     resp_token.raise_for_status()
     # token_type = resp_token.json()['token_type']
     auth_string = f'Bearer {resp_token.json()["access_token"]}'
