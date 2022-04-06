c70afb1dc681c7130dd679fa5cf29c23654a20b1
jb3_2 <jonas.bieri@gmail.com>
2022-04-06 17:44:27 +0200

    Use get_text_from_url()

--- bag_coronavirus/src/copy_bag_datasets.py
+++ bag_coronavirus/src/copy_bag_datasets.py
@@ -42,7 +42,7 @@ def get_dataset_metadata():
 
 def extract(url):
     logging.info(f'Reading current csv from {url} into data frame...')
-    df_raw = common.pandas_read_csv(url)
+    df_raw = pd.read_csv(common.get_text_from_url(url))
     return df_raw
 
 
