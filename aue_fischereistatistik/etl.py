b7d1d4c399b50d39943a83469898ba0fd0e0b8c2
hesterfrederiek <hesterpieters@gmail.com>
2022-04-11 13:40:58 +0200

    Remove empty rows

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -11,14 +11,14 @@ pd.set_option( 'display.max_rows', None)
 # datetime in German
 locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
 
-columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Jahr','Gewässercode', 'Fischart', 'Gewicht',
+columns = ['Fischereikarte', 'Fangbüchlein_retourniert', 'Datum', 'Monat', 'Jahr', 'Gewässercode', 'Fischart', 'Gewicht',
            'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
            'Abfluss_Rhein_über_1800m3', 'Bemerkungen']
 
 df = pd.DataFrame(columns=columns)
 
 
-for year in range(2010,2021):
+for year in range(2010, 2021):
     year = str(year)
     path = f'{credentials.path_csv}/fangstatistik_{year}.csv'
     df_year = pd.read_csv(path, encoding='utf-8', keep_default_na=False)
@@ -58,13 +58,14 @@ for year in range(2010,2021):
             df_year['Tag'] = df_year['Datum']
     df = pd.concat([df, df_year])
 
-# to do: filter empty rows: 2010,,,unbekannt,,,,,,,,
-
+# to do: filter empty rows: remove all rows that have no entry for date ánd fischart or grundel..
+df.to_csv(f'{credentials.base_path_local}/fangstatistik.csv')
+df = pd.read_csv(f'{credentials.base_path_local}/fangstatistik.csv')
+condition = ~(df['Fischart'].isna() & df['Monat'].isna())
+df = df[condition]
 
 # filter columns for export
 df = df[['Jahr', 'Monat', 'Tag', 'Gewässercode', 'Fischart', 'Gewicht',
            'Länge', 'Nasenfänge', 'Kesslergrundel', 'Schwarzmundgrundel', 'Nackthalsgrundel',
            'Abfluss_Rhein_über_1800m3']]
 df.to_csv(f'{credentials.base_path_local}/fangstatistik.csv')
-
-
