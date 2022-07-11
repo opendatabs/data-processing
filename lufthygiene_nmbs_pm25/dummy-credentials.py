364ca6a9fdfdcd222c89b48f0012d184e9490686
jb3_2 <jonas.bieri@gmail.com>
2022-07-11 09:51:59 +0200

    Remove module lufthygiene_nmbs_pm25

--- lufthygiene_nmbs_pm25/dummy-credentials.py
+++
@@ -1,2 +0,0 @@
-ods_live_push_api_url = 'https://data.bs.ch/api/push/1.0/100100/realtime/push/?pushkey=APIKEY'
-ods_test_push_api_url = 'https://data.bs.ch/api/push/1.0/99999/realtime/push/?pushkey=APIKEY'
