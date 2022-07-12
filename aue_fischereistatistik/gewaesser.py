41b0061145e160ea583ea0950344424489ac73b6
hesterfrederiek <hesterpieters@gmail.com>
2022-07-12 13:53:49 +0200

    Add function split_line_at_point, remove/add comments

--- aue_fischereistatistik/gewaesser.py
+++ aue_fischereistatistik/gewaesser.py
@@ -1,18 +1,11 @@
 import pandas as pd
 import geopandas as gpd
-from shapely.ops import split
-from shapely.geometry import Point, LineString
+from shapely.ops import split, linemerge, nearest_points
+from shapely.geometry import Point, LineString, MultiLineString
 
-# dict_gew =  {   '0' : '-',
-#                 '1' : 'Rhein - Basel',
-#                 '2' : 'Rhein - Basel',
-#                 '3' : 'Wiese - Pachtstrecke Stadt Basel',
-#                 '4' : 'Birs - Pachtstrecke Stadt Basel',
-#                 '5' : 'Riehenteich - Pachtstrecke Riehen',
-#                 '6' : 'Wiese - Pachtstrecke Riehen',
-#                 '7' : 'Wiese - Pachstrecke Riehen'
-# }
+pd.options.display.max_colwidth = 1000
 
+# Add df_gewaesser
 df_gewaesser = pd.DataFrame()
 df_gewaesser['Gewässer'] = ['Rhein - Basel',
                              'Wiese - Pachtstrecke Stadt Basel',
@@ -21,12 +14,21 @@ df_gewaesser['Gewässer'] = ['Rhein - Basel',
                              'Riehenteich - Pachtstrecke Riehen',
                             ]
 
-
-pd.options.display.max_colwidth = 1000
-
-
+# Read in relevant coordinates from Gewässernetz Basel
 gdf = gpd.read_file('data/gewaesser/gewaesser.geojson')
 print(gdf)
+
+# function to split line at point closest to a given point
+def split_line_at_point(line,point):
+    nearest_on_line = nearest_points(line, point)[0]
+    coordinates_line = [coord for coord in line.coords]
+    coordinates_point = [coord for coord in nearest_on_line.coords]
+    list_coordinates_line_with_point = coordinates_line + coordinates_point
+    line_with_point = LineString(sorted(list_coordinates_line_with_point))
+    splitted_line = split(line_with_point, nearest_on_line)
+    line1 = splitted_line.geoms[0]
+    line2 = splitted_line.geoms[1]
+    return line1, line2
 
 
 # split up Wiese in Basel and Riehen part, breaking point: 47.57906, 7.62498
@@ -36,11 +38,6 @@ line = gdf[gdf['gew_name']== 'Wiese']['geometry'].iloc[0]
 point = Point(7.623379999359302, 47.578078114071396)
 
 split_Wiese = split(line, point)
-
-
-
-
-
 
 
 # Construct gdf_gewaesser
