3f03248e8423fc8b3cee38a6bedf47d0642c951f
hesterfrederiek <hesterpieters@gmail.com>
2022-07-12 15:54:41 +0200

    Add all pieces of Wiese-Stadt Basel

--- aue_fischereistatistik/gewaesser.py
+++ aue_fischereistatistik/gewaesser.py
@@ -30,27 +30,35 @@ def split_line_at_point(line,point):
     line2 = splitted_line.geoms[1]
     return line1, line2
 
-
-# split up Wiese in Basel and Riehen part, breaking point: 47.57906, 7.62498
-# Still add small pieces to Basel Wiese?
-line = gdf[gdf['gew_name']== 'Wiese']['geometry'].iloc[0]
-point = Point(7.62498, 47.57906)
-split_Wiese = split_line_at_point(line, point)
-
 # 'Riehenteich - Pachtstrecke Riehen': Add Neuer Teich and Mühleteich, cut off at border (7.65363, 47.59551)
+# cut off piece of Neuer Teich that belongs to Wiese-Basel (7.62643, 47.57782)
 line1 = gdf[gdf['gew_name']== 'Neuer Teich']['geometry'].iloc[0]
 line2 = gdf[gdf['gew_name']== 'Mühleteich']['geometry'].iloc[0]
 multi_line = MultiLineString([line1, line2])
 merged_line = linemerge(multi_line)
-point = Point(7.65363, 47.59551)
-line_riehenteich, _ = split_line_at_point(merged_line, point)
+point_border = Point(7.65363, 47.59551)
+line_from_border, _ = split_line_at_point(merged_line, point_border)
+cutpoint = Point(7.62643, 47.57782)
+part_Wiese_Basel, line_riehenteich = split_line_at_point(line_from_border, cutpoint)
 
+# split up Wiese in Basel and Riehen part, breaking point: 47.57906, 7.62498
+line = gdf[gdf['gew_name']== 'Wiese']['geometry'].iloc[0]
+point = Point(7.62498, 47.57906)
+Wiese_Basel, Wiese_Riehen = split_line_at_point(line, point)
+# cut of piece of Riehenteich for Wiese_Basel (7.62393, 47.57679)
+riehenteich =  gdf[gdf['gew_name']== 'Riehenteich']['geometry'].iloc[0]
+cutpoint = Point(7.62393, 47.57679)
+_, riehenteich_to_Wiese_Basel = split_line_at_point(riehenteich, cutpoint)
+# add all pieces to Wiese_Basel
+Wildschutzkanal = gdf[gdf['gew_name']== 'Wildschutzkanal']['geometry'].iloc[0]
+multi_line = MultiLineString([Wiese_Basel, riehenteich_to_Wiese_Basel, part_Wiese_Basel, Wildschutzkanal])
+Wiese_Basel = linemerge(multi_line)
 
 
 # Construct gdf_gewaesser
 geo_gewaesser = gpd.GeoSeries([gdf[gdf['gew_name']== 'Rhein']['geometry'].iloc[0],
-                                split_Wiese[1],
-                                split_Wiese[0],
+                                Wiese_Basel,
+                                Wiese_Riehen,
                                 gdf[gdf['gew_name']== 'Birs']['geometry'].iloc[0],
                                 line_riehenteich
                                 ])
