8909701608d3d0c102dccc53e2841dc2105db3dd
jb3_2 <jonas.bieri@gmail.com>
2022-09-27 16:32:48 +0200

    Rename data file from *od_Exp* to *od_Proc* instead of moving to different folder, handle new name of archive file

--- aue_grundwasser/etl.py
+++ aue_grundwasser/etl.py
@@ -14,14 +14,14 @@ from io import StringIO
 def list_files():
     file_list = []
     for remote_path in credentials.ftp_remote_paths:
-        listing = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, remote_path, credentials.data_orig_path, '*.csv', list_only=True)
+        listing = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, remote_path, credentials.data_orig_path, 'BS_Grundwasser_odExp_*.csv', list_only=True)
         file_list.extend(listing)
     return file_list
 
 
 def process(file, x_coords_1416):
     export_stats = True
-    if 'historische_Daten_inaktive_Messstellen.csv' in file:
+    if 'BS_Grundwasser_odExp_historische_inaktive_Messstellen.csv' in file:
         logging.info(f'Processing archive file {file}...')
         dfa = pd.read_csv(file, sep=';', names=['StationNr', 'SensorNr', 'Date_text', 'Time', 'Value'], low_memory=False)
         logging.info(f'Pre-processing archive dataframe...')
@@ -86,7 +86,7 @@ def process(file, x_coords_1416):
 
 
 def archive(file):
-    to_name = os.path.join('..', credentials.ftp_archive_path, os.path.basename(file))
+    to_name = os.path.basename(file).replace('BS_Grundwasser_odExp_', 'BS_Grundwasser_odProc_')
     logging.info(f'Renaming file on FTP server from {file} to {to_name}...')
     common.rename_ftp(file, to_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass)
 
