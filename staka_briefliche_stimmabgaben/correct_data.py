248970d4defe00c63f5a44e7645717b76fc288da
hesterfrederiek <hesterpieters@gmail.com>
2022-10-07 16:05:16 +0200

    add staka_briefliche_stimmabgaben/correct_data.py

---
+++ staka_briefliche_stimmabgaben/correct_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import common
+from staka_briefliche_stimmabgaben import credentials
+
+
+# remove data that has been imported with the wrong Abstimmungsdatum
+
+datum = "2022-09-24"
+
+# obtain all entries with the wrong date in 100223
+req = common.requests_get(f'https://data.bs.ch/api/v2/catalog/datasets/100223/exports/json?refine=abstimmungsdatum:{datum}&limit=-1&offset=0&timezone=UTC', auth=(credentials.username, credentials.password))
+file = req.json()
+df = pd.DataFrame.from_dict(file)
+
+
+# remove the records with the wrong date in 100223
+payload = df.to_json(orient="records")
+delete_url = credentials.ods_live_realtime_delete_url_publ
+push_key = credentials.ods_live_realtime_push_key_publ
+r = common.requests_post(url=delete_url, data=payload, params={'pushkey': push_key})
+r.raise_for_status()
+
+
+# obtain all entries with the wrong date in 100224
+req = common.requests_get(f'https://data.bs.ch/api/v2/catalog/datasets/100224/exports/json?refine=abstimmungsdatum:{datum}&limit=-1&offset=0&timezone=UTC', auth=(credentials.username, credentials.password))
+file = req.json()
+df = pd.DataFrame.from_dict(file)
+
+
+# remove the records with the wrong date in 100224
+payload = df.to_json(orient="records")
+delete_url = credentials.ods_live_realtime_delete_url_viz
+push_key = credentials.ods_live_realtime_push_key_viz
+r = common.requests_post(url=delete_url, data=payload, params={'pushkey': push_key})
+r.raise_for_status()
+
+
