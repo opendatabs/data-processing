e9afb01a72f1e358994632abb4c15ccb46235305
jb3_2 <jonas.bieri@gmail.com>
2022-10-27 14:00:48 +0200

    Add decoded columns, upload to ftp server, re-publish ODS datasets

--- stata_gwr/etl.py
+++ stata_gwr/etl.py
@@ -1,13 +1,28 @@
 import logging
 import os
 import zipfile
+import pandas as pd
+import common
 import common.change_tracking as ct
 import ods_publish.etl_id as odsp
-import common
 from stata_gwr import credentials
 
 
 def main():
+    GEB = {'filename': 'gebaeude_batiment_edificio{}.csv',
+           'to_decode': ['GKSCE', 'GSTAT', 'GKAT', 'GKLAS', 'GBAUP', 'GVOLNORM', 'GVOLSCE', 'GSCHUTZR', 'GWAERZH1', 'GENH1',
+                         'GWAERSCEH1', 'GWAERZH2', 'GENH2', 'GWAERSCEH2', 'GWAERZW1', 'GENW1', 'GWAERSCEW1', 'GWAERZW2',
+                         'GENW2', 'GWAERSCEW2']}
+    DOM = {'filename': 'eingang_entree_entrata{}.csv',
+           'to_decode': ['STRSP', 'STROFFIZIEL', 'DOFFADR']}
+    WHG = {'filename': 'wohnung_logement_abitazione{}.csv',
+           'to_decode': ['WSTWK', 'WMEHRG', 'WSTAT', 'WKCHE']}
+
+    all_entities = [GEB, DOM, WHG]
+
+    DECODED_COL_APPENDIX = '_DECODED'
+    DECODED_FILE_APPENDIX = '_bs'  # with empty string, this overwrites original files. Use 'decoded' or similar to create new files
+
     r = common.requests_get('https://public.madd.bfs.admin.ch/bs.zip')
     r.raise_for_status()
     data_orig_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data_orig')
@@ -18,13 +33,26 @@ def main():
     if ct.has_changed(zip_file_path):
         with zipfile.ZipFile(zip_file_path) as z:
             z.extractall(os.path.join(data_orig_path, zip_folder))
-        for filename in os.listdir(os.path.join(data_orig_path, zip_folder)):
-            file_path = os.path.join(data_orig_path, zip_folder, filename)
-            if os.path.isfile(file_path):
-                common.upload_ftp(file_path, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'gwr/{zip_folder}')
-        # todo: Publish ods datasets
-        # filenames = ['eingang_entree_entrata.csv', 'gebaeude_batiment_edificio.csv', 'kodes_codes_codici.csv', 'wohnung_logement_abitazione.csv']
-        # odsp.publish_ods_dataset_by_id('')
+        codes = pd.read_table(os.path.join(data_orig_path, zip_folder, 'kodes_codes_codici.csv'), dtype=str)
+        code_map = pd.Series(codes.CODTXTLD.values, index=codes.CECODID).to_dict()
+        logging.info(f'Adding decoded columns to datasets...')
+        data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+        exported_files = []
+        for entity in all_entities:
+            ent = pd.read_table(os.path.join(data_orig_path, zip_folder, entity['filename'].format('')), dtype=str)
+            for var in entity['to_decode']:
+                ent.insert(ent.columns.get_loc(var) + 1, var + DECODED_COL_APPENDIX, '')
+                ent[var + DECODED_COL_APPENDIX] = ent[var].map(code_map)
+            export_file_path = os.path.join(data_path, entity['filename'].format(DECODED_FILE_APPENDIX))
+            ent.to_csv(export_file_path, index=False, sep='\t')
+            exported_files.append(export_file_path)
+        for filename in os.listdir(data_path):
+            file_path = os.path.join(data_path, filename)
+            if os.path.isfile(file_path) and '.csv' in file_path and ct.has_changed(file_path):
+                common.upload_ftp(file_path, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'gwr/opendata_export')
+        ods_ids = ['100230', '100231', '100232']
+        for ods_id in ods_ids:
+            odsp.publish_ods_dataset_by_id(ods_id)
         ct.update_hash_file(zip_file_path)
     pass
 
