739d0eeb9c1741b8e347068be2cf5742c8e703da
hesterfrederiek <hesterpieters@gmail.com>
2022-12-16 15:35:46 +0100

    process all files

--- staka_abstimmungen/src/etl_all.py
+++ staka_abstimmungen/src/etl_all.py
@@ -6,9 +6,47 @@ import pathlib
 
 path_files = os.path.join(pathlib.Path(__file__).parents[1], 'data/data-processing-output')
 
-def get_dates():
+def main():
+    df_all = process_files()
+    df_all = construct_dataset(df_all)
+    return df_all
+
+def process_files():
+    df = pd.DataFrame()
+    files = get_files_kennzahlen() + get_files_details()
+    for file in files:
+        df_file = pd.read_csv(file)
+        df = pd.concat([df, df_file])
+    return df
+
+
+def construct_dataset(df):
+    df['auf_ebene'] = ['Gemeinde' if x else 'Wahllokal' for x in pd.isna(df['Wahllok_name'])]
+    # add BS-ID
+    df['year'] = (df['Abst_Datum'].astype('datetime64[ns]')).dt.year
+    dict_laufnr = make_dict_date_laufnr()
+    df['lauf_nr_termin'] = [dict_laufnr[datum] for datum in df['Abst_Datum']]
+    df['bs_id'] = df['year'].astype(str) + '0' + df['lauf_nr_termin'] + '0' + df['Abst_ID'].astype(str)
+    #df['bs_id'] = [bs_id[i] if x == "kantonal" else '' for i, x in df['Abst_Art'].items()]
+    path_export = os.path.join(pathlib.Path(__file__).parents[1], 'data/export', 'abstimmungen.csv')
+    df.to_csv(path_export, index=False)
+    return df
+
+
+def get_files_kennzahlen():
     pattern_kennzahlen = 'Abstimmungen_??????????.csv'
     file_list = glob.glob(os.path.join(path_files, pattern_kennzahlen))
+    return file_list
+
+
+def get_files_details():
+    pattern_details = 'Abstimmungen_Details_??????????.csv'
+    file_list = glob.glob(os.path.join(path_files, pattern_details))
+    return file_list
+
+
+def get_dates():
+    file_list = get_files_kennzahlen()
     list_dates = []
     for file in file_list:
         date_str = os.path.basename(file).split("_", 1)[1][:10]
@@ -24,31 +62,11 @@ def make_dict_date_laufnr():
     lauf_nr_termin = 2
     for date in dates:
         if date[0:4] == year:
-            dict[date] = lauf_nr_termin
+            dict[date] = str(lauf_nr_termin)
         else:
             year = date[0:4]
             lauf_nr_termin = 1
-            dict[date] = lauf_nr_termin
+            dict[date] = str(lauf_nr_termin)
         lauf_nr_termin += 1
     return dict
 
-
-path_kennzahlen = os.path.join(pathlib.Path(__file__).parents[1], 'data/data-processing-output', 'Abstimmungen_2022-09-25.csv')
-path_details = os.path.join(pathlib.Path(__file__).parents[1], 'data/data-processing-output', 'Abstimmungen_Details_2022-09-25.csv')
-
-df_kennzahlen = pd.read_csv(path_kennzahlen)
-df_details = pd.read_csv(path_details)
-
-
-df_all = pd.concat([df_details, df_kennzahlen], ignore_index=True)
-
-df_all['auf_ebene'] = ['Gemeinde' if x else 'Wahllokal' for x in pd.isna(df_all['Wahllok_name'])]
-
-
-# add BS-ID
-df_all['year'] = (df_all['Abst_Datum'].astype('datetime64[ns]')).dt.year
-lauf_nr_termin = '03'
-bs_id = df_all['year'].astype(str) + lauf_nr_termin + '0' + df_all['Abst_ID'].astype(str)
-df_all['bs_id'] = [bs_id[i] if x == "kantonal" else '' for i, x in df_all['Abst_Art'].items()]
-path_export = os.path.join(pathlib.Path(__file__).parents[1], 'data/export', 'abstimmungen.csv')
-df_all.to_csv(path_export, index=False)
