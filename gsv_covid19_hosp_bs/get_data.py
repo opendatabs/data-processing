8b91cba0738db6c2cd6c2372649649e6fb0dcbad
jb3_2 <jonas.bieri@gmail.com>
2023-01-10 13:00:59 +0100

    Add proxies

--- gsv_covid19_hosp_bs/get_data.py
+++ gsv_covid19_hosp_bs/get_data.py
@@ -64,12 +64,12 @@ def get_data(hospital, date):
     payload = {}
     headers = {
         'Authorization': credentials.authorization_live}
-    requests.request("GET", url, headers=headers, data=payload)
+    requests.request("GET", url, headers=headers, data=payload, proxies=credentials.proxies)
     if hospital == 'UKBB':
         url2 = credentials.url_hosp_children + get_filter(hospital, date)
     else:
         url2 = credentials.url_hosp_adults + get_filter(hospital, date)
-    response = requests.request("GET", url2, headers=headers, data=payload)
+    response = requests.request("GET", url2, headers=headers, data=payload, proxies=credentials.proxies)
     response.raise_for_status()
     results = response.json()["d"]["results"]
     return results
