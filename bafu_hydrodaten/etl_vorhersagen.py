20364396e7a20658c594eaeb98e8f98733078028
hesterfrederiek <hesterpieters@gmail.com>
2023-01-26 16:41:34 +0100

    write out methode + extract publication time

--- bafu_hydrodaten/etl_vorhersagen.py
+++ bafu_hydrodaten/etl_vorhersagen.py
@@ -2,10 +2,19 @@ import pandas as pd
 import common
 from requests.auth import HTTPBasicAuth
 from bafu_hydrodaten import credentials
+import re
+from datetime import datetime
+
+
+date_patterns = [r'\d{2}.\d{1}.\d{4}, \d{2].\d{2}']
 
 url = credentials.url_det_C1E_Rhein
 req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
 lines = req.content.splitlines()
+info_C1E = lines[6:9]
+ausgabe_info = str(lines[6])
+match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
+ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
 
 with open('det_C1E_Rhein_table.txt', mode='wb') as file:
     for line in lines[14::]:
@@ -13,13 +22,17 @@ with open('det_C1E_Rhein_table.txt', mode='wb') as file:
         file.write(b'\n')
 
 df1 = pd.read_table('det_C1E_Rhein_table.txt', delim_whitespace=True)
-df1['methode'] = 'C1E'
-df1['ausgebeben_an'] = ''
+df1['methode'] = 'COSMO-1E ctrl'
+df1['ausgegeben_an'] = ausgabe
 df1['gemessen'] = ''
 
 url = credentials.url_det_C2E_Rhein
 req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
 lines = req.content.splitlines()
+info_C2E = lines[6:9]
+ausgabe_info = str(lines[6])
+match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
+ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
 
 with open('det_C2E_Rhein_table.txt', mode='wb') as file:
     for line in lines[14::]:
@@ -27,13 +40,17 @@ with open('det_C2E_Rhein_table.txt', mode='wb') as file:
         file.write(b'\n')
 
 df2 = pd.read_table('det_C2E_Rhein_table.txt', delim_whitespace=True)
-df2['methode'] = 'C2E'
-df2['ausgebeben_an'] = ''
+df2['methode'] = 'COSMO-2E ctrl'
+df2['ausgegeben_an'] = ausgabe
 df2['gemessen'] = ''
 
 url = credentials.url_det_IFS_Rhein
 req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
 lines = req.content.splitlines()
+info_IFS = lines[6:9]
+ausgabe_info = str(lines[6])
+match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
+ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
 
 with open('det_IFS_Rhein_table.txt', mode='wb') as file:
     for line in lines[14::]:
@@ -42,9 +59,8 @@ with open('det_IFS_Rhein_table.txt', mode='wb') as file:
 
 df3 = pd.read_table('det_IFS_Rhein_table.txt', delim_whitespace=True)
 df3['methode'] = 'IFS'
-df3['ausgebeben_an'] = ''
+df3['ausgegeben_an'] = ausgabe
 df3['gemessen'] = ''
-
 
 
 df = pd.concat([df1, df2, df3])
