ba3f22f804f4b715dbd1e092db46d722659348fc
hesterfrederiek <hesterpieters@gmail.com>
2023-01-27 13:32:41 +0100

    add extract_data, get_date_time

--- bafu_hydrodaten/etl_vorhersagen.py
+++ bafu_hydrodaten/etl_vorhersagen.py
@@ -6,62 +6,44 @@ import re
 from datetime import datetime
 
 
-date_patterns = [r'\d{2}.\d{1}.\d{4}, \d{2].\d{2}']
-
-url = credentials.url_det_C1E_Rhein
-req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
-lines = req.content.splitlines()
-info_C1E = lines[6:9]
-ausgabe_info = str(lines[6])
-match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
-ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
-
-with open('det_C1E_Rhein_table.txt', mode='wb') as file:
-    for line in lines[14::]:
-        file.write(line)
-        file.write(b'\n')
-
-df1 = pd.read_table('det_C1E_Rhein_table.txt', delim_whitespace=True)
-df1['methode'] = 'COSMO-1E ctrl'
-df1['ausgegeben_an'] = ausgabe
-
-url = credentials.url_det_C2E_Rhein
-req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
-lines = req.content.splitlines()
-info_C2E = lines[6:9]
-ausgabe_info = str(lines[6])
-match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
-ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
-
-with open('det_C2E_Rhein_table.txt', mode='wb') as file:
-    for line in lines[14::]:
-        file.write(line)
-        file.write(b'\n')
-
-df2 = pd.read_table('det_C2E_Rhein_table.txt', delim_whitespace=True)
-df2['methode'] = 'COSMO-2E ctrl'
-df2['ausgegeben_an'] = ausgabe
+rivers = ['Rhein', 'Birs']
+methods = ['COSMO-1E ctrl', 'COSMO-2E ctrl', 'IFS']
 
 
-url = credentials.url_det_IFS_Rhein
-req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
-lines = req.content.splitlines()
-info_IFS = lines[6:9]
-ausgabe_info = str(lines[6])
-match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', ausgabe_info)
-ausgabe = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
-
-with open('det_IFS_Rhein_table.txt', mode='wb') as file:
-    for line in lines[14::]:
-        file.write(line)
-        file.write(b'\n')
-
-df3 = pd.read_table('det_IFS_Rhein_table.txt', delim_whitespace=True)
-df3['methode'] = 'IFS'
-df3['ausgegeben_an'] = ausgabe
+def get_date_time(line):
+    match = re.search(r'\d{1,2}.\d{1,2}.\d{4}, \d{2}.\d{2}', line)
+    date_time = datetime.strptime(match.group(), '%d.%m.%Y, %H.%M')
+    return date_time
 
 
-df = pd.concat([df1, df2, df3])
+def extract_data(river, method):
+    url = credentials.dict_url[river][method]
+    req = common.requests_get(url, auth=HTTPBasicAuth(credentials.https_user, credentials.https_pass))
+    lines = req.content.splitlines()
+    ausgabe_info = str(lines[6])
+    ausgabe = get_date_time(ausgabe_info)
+    meteolauf_info = str(lines[7])
+    meteolauf = get_date_time(meteolauf_info)
+    gemessen_info = str(lines[8])
+    gemessen = get_date_time(gemessen_info)
+    with open(f'det_{method}_{river}_table.txt', mode='wb') as file:
+        for line in lines[14::]:
+            file.write(line)
+            file.write(b'\n')
+    df = pd.read_table(f'det_{method}_{river}_table.txt', delim_whitespace=True)
+    df['methode'] = method
+    df['ausgegeben_an'] = ausgabe
+    df['meteolauf'] = meteolauf
+    df['gemessene_werten_bis'] = gemessen
+    return df
+
+
+river = 'Rhein'
+df = pd.DataFrame()
+for method in methods:
+    df_method = extract_data(river, method)
+    df = pd.concat([df, df_method])
+
 for column in ['hh', 'dd', 'mm']:
     df[column] = [x if len(x) == 2 else ("0" + x) for x in df[column].astype(str)]
 df['timestamp'] = df['mm'].astype(str) + '-' + df['dd'].astype(str) + '-' + df['yyyy'].astype(str) + ' ' + df['hh'].astype(str)
