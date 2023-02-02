e9cf34d87c1293cd5746a183b810301760d3ee7a
hesterfrederiek <hesterpieters@gmail.com>
2023-02-02 09:59:45 +0100

    remove code for "visualization dataset"

--- staka_briefliche_stimmabgaben/correct_data.py
+++ staka_briefliche_stimmabgaben/correct_data.py
@@ -21,17 +21,6 @@ r = common.requests_post(url=delete_url, data=payload, params={'pushkey': push_k
 r.raise_for_status()
 
 
-# obtain all entries with the wrong date in 100224
-req = common.requests_get(f'https://data.bs.ch/api/v2/catalog/datasets/100224/exports/json?refine=abstimmungsdatum:{datum}&limit=-1&offset=0&timezone=UTC', auth=(credentials.username, credentials.password))
-file = req.json()
-df = pd.DataFrame.from_dict(file)
 
-
-# remove the records with the wrong date in 100224
-payload = df.to_json(orient="records")
-delete_url = credentials.ods_live_realtime_delete_url_viz
-push_key = credentials.ods_live_realtime_push_key_viz
-r = common.requests_post(url=delete_url, data=payload, params={'pushkey': push_key})
-r.raise_for_status()
 
 
