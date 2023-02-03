be117f2d99bf0d78f43a1a969ac1339c64392c40
hesterfrederiek <hesterpieters@gmail.com>
2023-02-03 10:39:55 +0100

    add main()

--- lufthygiene_rosental/etl.py
+++ lufthygiene_rosental/etl.py
@@ -5,37 +5,34 @@ import logging
 from lufthygiene_rosental import credentials
 
 
+def main():
+    url = 'https://data-bs.ch/lufthygiene/Rosental-Mitte/online/airmet_bs_rosental_pm25_aktuell'
+    logging.info(f'Downloading data from {url}...')
+    r = common.requests_get(url)
+    r.raise_for_status()
+    s = r.text
+    df = pd.read_csv(io.StringIO(s), sep=';')
+    df = df.dropna(subset=['Anfangszeit'])
+    df['timestamp'] = pd.to_datetime(df.Anfangszeit, format='%d.%m.%Y %H:%M:%S').dt.tz_localize('Europe/Zurich', ambiguous=True, nonexistent='shift_forward')
 
-url = 'https://data-bs.ch/lufthygiene/Rosental-Mitte/online/airmet_bs_rosental_pm25_aktuell'
-logging.info(f'Downloading data from {url}...')
-r = common.requests_get(url)
-r.raise_for_status()
-s = r.text
-df = pd.read_csv(io.StringIO(s), sep=';')
-df.to_csv('rosental_pm25.csv', index=False)
-df = df.dropna(subset=['Anfangszeit'])
-df['timestamp'] = pd.to_datetime(df.Anfangszeit, format='%d.%m.%Y %H:%M:%S').dt.tz_localize('Europe/Zurich', ambiguous=True, nonexistent='shift_forward')
 
-
-if len(df) == 0:
-    logging.info('No rows to push to ODS... ')
-else:
-    logging.info(f'Melting dataframe...')
-    ldf = df.melt(id_vars=['Anfangszeit', 'timestamp'], var_name='station', value_name='pm_2_5')
-    logging.info(f'Dropping rows with empty pm25 value...')
-    ldf['pm_2_5'] = pd.to_numeric(ldf['pm_2_5'], errors='coerce')
-    ldf = ldf.dropna(subset=['pm_2_5'])
-    row_count = ldf.timestamp.count()
-    if row_count == 0:
-        logging.info(f'No rows to push to ODS... ')
+    if len(df) == 0:
+        logging.info('No rows to push to ODS... ')
     else:
-        logging.info(f'Pushing {row_count} rows to ODS realtime API...')
-        ldf.timestamp = ldf.timestamp.dt.strftime('%Y-%m-%d %H:%M:%S%z')
-        payload = ldf.to_json(orient="records")
-        r = common.requests_post(url=credentials.ods_live_push_api_url, data=payload, verify=False)
-        r.raise_for_status()
-
-    logging.info('Job successful!')
+        logging.info(f'Melting dataframe...')
+        ldf = df.melt(id_vars=['Anfangszeit', 'timestamp'], var_name='station', value_name='pm_2_5')
+        logging.info(f'Dropping rows with empty pm25 value...')
+        ldf['pm_2_5'] = pd.to_numeric(ldf['pm_2_5'], errors='coerce')
+        ldf = ldf.dropna(subset=['pm_2_5'])
+        row_count = ldf.timestamp.count()
+        if row_count == 0:
+            logging.info(f'No rows to push to ODS... ')
+        else:
+            logging.info(f'Pushing {row_count} rows to ODS realtime API...')
+            ldf.timestamp = ldf.timestamp.dt.strftime('%Y-%m-%d %H:%M:%S%z')
+            payload = ldf.to_json(orient="records")
+            r = common.requests_post(url=credentials.ods_live_push_api_url, data=payload, verify=False)
+            r.raise_for_status()
 
 
  # Realtime API bootstrap data:
@@ -50,3 +47,8 @@ else:
 #     print(f'Executing {__file__}...')
 #     main()
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
+    print('Job successful!')
