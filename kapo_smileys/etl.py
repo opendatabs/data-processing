caaac034801f7ec4ac50699475030fd4379c237d
Jonas Bieri <jonas.bieri@gmail.com>
2023-03-01 16:54:16 +0100

    Create new dataset from stat_df

--- kapo_smileys/etl.py
+++ kapo_smileys/etl.py
@@ -25,10 +25,12 @@ def parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze):
     any_changes = False
     messdaten_folders = glob.glob(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'Datenablage', '*'))
     messdaten_dfs = []
+    stat_dfs = []
     for folder in messdaten_folders:
-        messdaten_dfs_pro_standort = parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsaetze)
-        df_all_pro_standort = pd.concat(messdaten_dfs_pro_standort)
+        df_all_pro_standort, df_stat_pro_standort = parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsaetze)
         messdaten_dfs.append(df_all_pro_standort)
+        stat_dfs.append(df_stat_pro_standort)
+
     all_df = pd.concat(messdaten_dfs)
     export_file_all = os.path.join(curr_dir, 'data', 'all_data.csv')
     all_df.to_csv(export_file_all, index=False)
@@ -36,7 +38,16 @@ def parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze):
         common.upload_ftp(export_file_all, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'kapo/smileys/all_data')
         any_changes = True
         ct.update_hash_file(export_file_all)
-    return all_df, any_changes
+
+    stat_df = pd.concat(stat_dfs)
+    export_file_stat = os.path.join(curr_dir, 'data', 'all_stat.csv')
+    stat_df.to_csv(export_file_stat, index=False)
+    if True:  # ct.has_changed(export_file_stat):
+        common.upload_ftp(export_file_stat, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'kapo/smileys/all_data')
+        any_changes = True
+        ct.update_hash_file(export_file_stat)
+
+    return all_df, stat_df, any_changes
 
 
 def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze):
@@ -59,22 +70,15 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
         df['V_Delta'] = df.V_Ausfahrt - df.V_Einfahrt
         # Determining Zyklus and Smiley_Nr of measurement
         df_m1 = pd.merge(df_einsatz_days, df, how='right', on=['id_standort', 'day_str']).drop(columns=['datum_aktiv', 'day_str'])
-        df_m = pd.merge(df_m1, df_einsatze, how='left', left_on=['id_standort', 'Zyklus', 'Smiley_Nr'], right_on=['id_Standorte', 'Zyklus', 'Smiley-Nr.'])
-        df_m = df_m.drop(columns=['id_Standorte', 'Smiley-Nr.'])
+        df_m = pd.merge(df_m1, df_einsatze, how='left', left_on=['id_standort', 'Zyklus'], right_on=['id_Standorte', 'Zyklus'])
+        df_m = df_m.drop(columns=['id_Standorte'])
         df_m['Phase'] = np.where(df_m.Messung_Timestamp < df_m.Start_Vormessung, 'Vor Vormessung',
                                  np.where(df_m.Messung_Timestamp < df_m.Start_Betrieb, 'Vormessung',
                                           np.where(df_m.Messung_Timestamp < df_m.Start_Nachmessung, 'Betrieb',
                                                    np.where(df_m.Messung_Timestamp < df_m.Ende, 'Nachmessung', 'Nach Ende')))
                                  )
+        logging.info(f'Removing measurements with phase "Vor Vormessung"...')
         df_m = df_m[df_m.Phase != 'Vor Vormessung']
-        # Calculate statistics for this single data file i.e. single day
-        df_all_v = pd.DataFrame(pd.concat([df_m.V_Einfahrt, df_m.V_Ausfahrt], ignore_index=True), columns=['V'])
-        df_m['V_max'] = max(df_all_v.V)
-        df_m['V_min'] = min(df_all_v.V)
-        df_m['V_50'] = np.mean(df_all_v.V)
-        df_m['V_85'] = np.percentile(df_all_v.V, 85)
-        df_m['V_Einfahrt_pct_ueber_limite'] = (df_m.V_Einfahrt > df_m.Geschwindigkeit).mean() * 100
-        df_m['V_Ausfahrt_pct_ueber_limite'] = (df_m.V_Ausfahrt > df_m.Geschwindigkeit).mean() * 100
 
         messdaten_dfs_pro_standort.append(df_m)
         export_file_single = os.path.join(curr_dir, 'data', f'{day_str}_{id_standort}.csv')
@@ -82,7 +86,28 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
         if True:  # ct.has_changed(export_file_single):
             common.upload_ftp(export_file_single, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'kapo/smileys/data')
             ct.update_hash_file(export_file_single)
-    return messdaten_dfs_pro_standort
+    df_all_pro_standort = pd.concat(messdaten_dfs_pro_standort)
+
+    if len(df_all_pro_standort.id_standort.unique()) > 1:
+        raise RuntimeError(f'More than 1 ({df_all_pro_standort.id_standort.unique()}) idstandort found in 1 data folder ({folder}!)')
+    if len(df_all_pro_standort.Zyklus.unique()) > 1:
+        raise RuntimeError(f'More than 1 ({df_all_pro_standort.Zyklus.unique()}) Zyklus found in 1 data folder ({folder}!)')
+
+    # Calculate statistics for this data folder
+    df_all_v = pd.DataFrame(pd.concat([df_all_pro_standort.V_Einfahrt, df_all_pro_standort.V_Ausfahrt], ignore_index=True), columns=['V'])
+    stat_pro_standort = {
+        'idstandort': int(df_all_pro_standort.id_standort.iloc[0]),
+        'Zyklus': int(df_all_pro_standort.Zyklus.iloc[0]),
+        'V_max': max(df_all_v.V),
+        'V_min': min(df_all_v.V),
+        'V_50': np.mean(df_all_v.V),
+        'V_85': np.percentile(df_all_v.V, 85),
+        'V_Einfahrt_pct_ueber_limite': (df_all_pro_standort.V_Einfahrt > df_all_pro_standort.Geschwindigkeit).mean() * 100,
+        'V_Ausfahrt_pct_ueber_limite': (df_all_pro_standort.V_Ausfahrt > df_all_pro_standort.Geschwindigkeit).mean() * 100,
+        'Anzahl_Messungen': len(df_all_pro_standort)
+    }
+    df_stat_pro_standort = pd.DataFrame([stat_pro_standort])
+    return df_all_pro_standort, df_stat_pro_standort
 
 
 def parse_einsatzplaene(curr_dir):
@@ -124,16 +149,17 @@ def main():
     df_einsaetze = df_einsaetze.drop(columns=['coords'])
 
     logging.info(f'Creating df_einsatz_days with one row per day and standort_id...')
-    df_einsatz_days = pd.concat([pd.DataFrame({'id_standort': row.id_Standorte, 'Zyklus': row.Zyklus, 'Smiley_Nr': row['Smiley-Nr.'], 'datum_aktiv': pd.date_range(row.Start_Vormessung, row.Ende, freq='D', normalize=True)})  # , 'Start_Vormessung': row.Start_Vormessung, 'Start_Betrieb': row.Start_Betrieb, 'Start_Nachmessung': row.Start_Nachmessung, 'Ende': row.Ende})
+    df_einsatz_days = pd.concat([pd.DataFrame({'id_standort': row.id_Standorte, 'Zyklus': row.Zyklus, 'datum_aktiv': pd.date_range(row.Start_Vormessung, row.Ende, freq='D', normalize=True)})  # , 'Start_Vormessung': row.Start_Vormessung, 'Start_Betrieb': row.Start_Betrieb, 'Start_Nachmessung': row.Start_Nachmessung, 'Ende': row.Ende})
                                  for i, row in df_einsaetze.iterrows()], ignore_index=True)
     df_einsatz_days['day_str'] = df_einsatz_days.datum_aktiv.dt.strftime('%y%m%d')
 
     shp_images_df = read_shapefile(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'GIS', 'Layer-Smiley-Standorte', 'Smiley-Standorte_Start_Ende.shp'))
 
     logging.info(f'Parsing Messdaten...')
-    df_all, any_changes = parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze)
+    df_all, stat_df, any_changes = parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze)
     if any_changes:
         odsp.publish_ods_dataset_by_id('100268')
+        odsp.publish_ods_dataset_by_id('100277')
     pass
 
 
