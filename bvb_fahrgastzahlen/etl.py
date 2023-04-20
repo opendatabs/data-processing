4e7e46a5ed10fdd68dc073948451d1ef9165338a
hesterfrederiek <hesterpieters@gmail.com>
2023-04-20 14:16:43 +0200

    Change paths

--- bvb_fahrgastzahlen/etl.py
+++ bvb_fahrgastzahlen/etl.py
@@ -2,17 +2,21 @@ import pandas as pd
 import openpyxl
 from datetime import datetime
 import logging
+import os
+import pathlib
 
 
 def main():
-    sheets = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=None , engine='openpyxl',  header=None)
+    sheets = pd.read_excel(os.path.join(pathlib.Path(__file__), '/data_orig/fahrgast.xlsx')
+                           , sheet_name=None , engine='openpyxl',  header=None)
     dat_sheet_names = []
     for key in sheets:
         if key.startswith('Zeitreihe'):
              dat_sheet_names.append(key)
     dat_sheets = []
     for sheet_name in dat_sheet_names:
-        zeitreihe_x = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=sheet_name , engine='openpyxl', header=None).T
+        zeitreihe_x = pd.read_excel(os.path.join(pathlib.Path(__file__), '/data_orig/fahrgast.xlsx'),
+                                    sheet_name=sheet_name , engine='openpyxl', header=None).T
         new_header = zeitreihe_x.iloc[0]
         zeitreihe_x=zeitreihe_x[1:]
         zeitreihe_x.columns=new_header
@@ -24,7 +28,7 @@ def main():
         dat_sheets.append(zeitreihe_x)
     fahrgast = pd.concat(dat_sheets)#.reset_index(drop=True) #Dateien werden zusammengeführt und Index (erste Spalte) wird korrigiert
     fahrgast = fahrgast [["Datum", "Fahrgäste (Einsteiger)", "Kalenderwoche"]]
-    fahrgast.to_csv('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data/bvb_fahrgastzahlen.csv', index = False)
+    fahrgast.to_csv(os.path.join(pathlib.Path(__file__), '/data/bvb_fahrgastzahlen.csv'), index = False)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
