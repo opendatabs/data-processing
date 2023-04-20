ba5c416be53610401f8a57b9785c0dee57303abb
hesterfrederiek <hesterpieters@gmail.com>
2023-04-20 14:13:17 +0200

    Mainify

--- bvb_fahrgastzahlen/etl.py
+++ bvb_fahrgastzahlen/etl.py
@@ -1,25 +1,33 @@
 import pandas as pd
 import openpyxl
 from datetime import datetime
+import logging
 
-sheets = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=None , engine='openpyxl',  header=None)
-dat_sheet_names = []
-for key in sheets:
-    if key.startswith('Zeitreihe'):
-         dat_sheet_names.append(key)
-dat_sheets = []
-for sheet_name in dat_sheet_names:
-    zeitreihe_x = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=sheet_name , engine='openpyxl', header=None).T
-    new_header = zeitreihe_x.iloc[0]
-    zeitreihe_x=zeitreihe_x[1:]
-    zeitreihe_x.columns=new_header
-    zeitreihe_x["Date"]=sheet_name.strip('Zeitreihe ')
-    zeitreihe_x["Kalenderwoche"]=zeitreihe_x["Kalenderwoche"].str.strip('KW')
-    def year_week(y,w):
-            return datetime.strptime(f'{y} {w} 1', '%G %V %u')
-    zeitreihe_x["Datum"]=zeitreihe_x.apply(lambda row: year_week(row.Date, row.Kalenderwoche), axis=1)
-    dat_sheets.append(zeitreihe_x)
-fahrgast = pd.concat(dat_sheets)#.reset_index(drop=True) #Dateien werden zusammengeführt und Index (erste Spalte) wird korrigiert
-fahrgast = fahrgast [["Datum", "Fahrgäste (Einsteiger)", "Kalenderwoche"]]
-fahrgast.to_csv('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data/bvb_fahrgastzahlen.csv', index = False)
 
+def main():
+    sheets = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=None , engine='openpyxl',  header=None)
+    dat_sheet_names = []
+    for key in sheets:
+        if key.startswith('Zeitreihe'):
+             dat_sheet_names.append(key)
+    dat_sheets = []
+    for sheet_name in dat_sheet_names:
+        zeitreihe_x = pd.read_excel('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data_orig/fahrgast.xlsx', sheet_name=sheet_name , engine='openpyxl', header=None).T
+        new_header = zeitreihe_x.iloc[0]
+        zeitreihe_x=zeitreihe_x[1:]
+        zeitreihe_x.columns=new_header
+        zeitreihe_x["Date"]=sheet_name.strip('Zeitreihe ')
+        zeitreihe_x["Kalenderwoche"]=zeitreihe_x["Kalenderwoche"].str.strip('KW')
+        def year_week(y,w):
+                return datetime.strptime(f'{y} {w} 1', '%G %V %u')
+        zeitreihe_x["Datum"]=zeitreihe_x.apply(lambda row: year_week(row.Date, row.Kalenderwoche), axis=1)
+        dat_sheets.append(zeitreihe_x)
+    fahrgast = pd.concat(dat_sheets)#.reset_index(drop=True) #Dateien werden zusammengeführt und Index (erste Spalte) wird korrigiert
+    fahrgast = fahrgast [["Datum", "Fahrgäste (Einsteiger)", "Kalenderwoche"]]
+    fahrgast.to_csv('c:/dev/workspace/data-processing/bvb_fahrgastzahlen/data/bvb_fahrgastzahlen.csv', index = False)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
+    logging.info('Job successful!')
