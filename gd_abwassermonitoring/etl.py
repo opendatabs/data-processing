515bc46b8edd3569254a3e157ca382a29ea5ac17
hesterfrederiek <hesterpieters@gmail.com>
2023-06-16 16:46:04 +0200

    upload dataset

--- gd_abwassermonitoring/etl.py
+++ gd_abwassermonitoring/etl.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from datetime import datetime
 from gd_abwassermonitoring import credentials
-from functools import reduce
 import common
 import logging
 from common import change_tracking as ct
@@ -10,7 +8,17 @@ from common import change_tracking as ct
 def main():
     df = merge_dataframes()
     df = calculate_columns(df)
-    df.to_csv('100302.csv', index=False)
+    df['Datum'] = df['Datum'].dt.strftime('%Y-%m-%d')
+    df.to_csv(credentials.path_export_file, index=False)
+    if ct.has_changed(credentials.path_export_file):
+        common.upload_ftp(credentials.path_export_file, credentials.ftp_server, credentials.ftp_user,
+                          credentials.ftp_pass, 'gd_kantonslabor/abwassermonitoring')
+        ct.update_hash_file(credentials.path_export_file)
+        logging.info("push data to ODS realtime API")
+        logging.info("push for dataset 100302")
+        push_url = credentials.ods_live_realtime_push_url
+        push_key = credentials.ods_live_realtime_push_key
+        common.ods_realtime_push_df(df, url=push_url, push_key=push_key)
     return df
 
 def make_column_dt(df, column):
@@ -19,7 +27,7 @@ def make_column_dt(df, column):
 
 def make_dataframe_bl():
     logging.info(f"import and transform BL data")
-    path = '/Users/hester/PycharmProjects/data-processing/gd_abwassermonitoring/data/Falldaten-BL/Abwassermonitoring_Influenza.csv'
+    path = credentials.path_BL
     df_bl = pd.read_csv(path, encoding="ISO-8859-1")
     # remove Gemeinde
     df_bl.drop(columns=["Gemeinde"], inplace=True)
@@ -36,7 +44,7 @@ def make_dataframe_bl():
 
 def make_dataframe_bs():
     logging.info(f"import and transform BS data")
-    path = '/Users/hester/PycharmProjects/data-processing/gd_abwassermonitoring/data/Falldaten-BS/ISM_export_influenza.xlsx'
+    path = credentials.path_BS
     df_bs = pd.read_excel(path)
     # only keep columns Testdatum [Benötigte Angaben], Serotyp/Subtyp [Erreger]
     df_bs = df_bs[['Testdatum [Benötigte Angaben]', 'Serotyp/Subtyp [Erreger]']]
@@ -54,7 +62,7 @@ def make_dataframe_bs():
 
 def make_dataframe_abwasser():
     logging.info("import and transform sewage data")
-    path = '/Users/hester/PycharmProjects/data-processing/gd_abwassermonitoring/data/Abwasserdaten/Probenraster CoroWWmonitoring.xlsx'
+    path = credentials.path_proben
     df_abwasser = pd.read_excel(path, header=2, usecols="A,B,F:AB,AP")
     logging.info(f'Remove text from numerical columns')
     numerical_columns = ['InfA (gc/PCR)', 'InfB (gc/PCR)',
@@ -92,42 +100,42 @@ def calculate_columns(df):
 
 # Realtime API bootstrap data for dataset 100302:
 #
-# {'Datum': '2023-04-15 00:00:00',
-# 'Sample Ba-Nr.': 'dito wie 15.4.',
-# 'InfA (gc/PCR)': 1.0,
-# 'InfB (gc/PCR)': 1.0,
-# 'InfA (gc/PCR)2': 43.5,
-# 'InfB (gc/PCR)2': 233.0,
-# 'RSV (gc/PCR)': 287.0, '
-# InfA (gc/L)': 17400.0,
-# 'InfB (gc/L)': 93200.0,
-# 'RSV (gc/L)': 114800.0,
-# 'InfA (gc/L) 7-d median': 33000.0,
-# 'InfB (gc/L) 7-d median': 143600.0,
-# 'RSV (gc/L) 7-d median': 114800.0,
-# "InfA (gc /100'000 P)": 528021378465.64966,
-# "InfB (gc/100'000 P)": 2828252440976.928,
-# "RSV (gc /100'000 P)": 3483727255623.9414,
-# "InfA (gc/100'000 P) 7-d median": 608804679610.9795,
-# "InfB (gc/100'000 P) 7-d median": 3739980264850.316,
-# "RSV (gc/100'000 P) 7-d median": 4243964845324.804,
-# 'InfA (gc/PMMoV)': 7.357293868921777e-05,
-# 'InfB (gc/PMMoV)': 0.0003940803382663848,
-# 'RSV (gc /PMMoV)': 0.0004854122621564482,
-# 'InfA (gc/PMMoV) 7-d median': 7.357293868921777e-05,
-# 'InfB (gc/PMMoV) 7-d median': 0.0004042848141146818,
-# 'RSV (gc/PMMoV) 7-d median': 0.0004516698172652804,
-# 'monthly RSV cases (USB/UKBB, in- & outpatients) ': 1,
-# 'Anz_pos_A_BS': 1,
-# 'Anz_pos_B_BS': 1,
-# 'Anz_pos_H1_BS': 1,
-# 'Anz.pos.A_BL': 1,
-# 'Anz.pos.B_BL': 1,
-# 'Anz.pos.all_BL': 1,
-# 'InfA_BS+BL': 1,
-# 'InfB_BS+BL': 1,
-# '7t_median_InfA': 1,
-# '7t_median_InfB': 1}
+{"Datum": "2021-08-12",
+"Sample Ba-Nr.": "dito wie 15.4.",
+"InfA (gc/PCR)": "1.0",
+"InfB (gc/PCR)": "1.0",
+"InfA (gc/PCR)2": "43.5",
+"InfB (gc/PCR)2": "233.0",
+"RSV (gc/PCR)": "287.0",
+"InfA (gc/L)": "17400.0",
+"InfB (gc/L)": "93200.0",
+"RSV (gc/L)": "114800.0",
+"InfA (gc/L) 7-d median": "33000.0",
+"InfB (gc/L) 7-d median": "143600.0",
+"RSV (gc/L) 7-d median": "114800.0",
+"InfA (gc /100'000 P)": "528021378465.64966",
+"InfB (gc/100'000 P)": "2828252440976.928",
+"RSV (gc /100'000 P)": "3483727255623.9414",
+"InfA (gc/100'000 P) 7-d median": "608804679610.9795",
+"InfB (gc/100'000 P) 7-d median": "3739980264850.316",
+"RSV (gc/100'000 P) 7-d median": "4243964845324.804",
+"InfA (gc/PMMoV)": "7.357293868921777",
+"InfB (gc/PMMoV)": "0.0003940803382663848",
+"RSV (gc /PMMoV)": "0.0004854122621564482",
+"InfA (gc/PMMoV) 7-d median": "7.357293868921777",
+"InfB (gc/PMMoV) 7-d median":" 0.0004042848141146818",
+"RSV (gc/PMMoV) 7-d median": "0.0004516698172652804",
+"monthly RSV cases (USB/UKBB, in- & outpatients) ": "1",
+"Anz_pos_A_BS": "1",
+"Anz_pos_B_BS": "1",
+"Anz_pos_H1_BS": "1",
+"Anz.pos.A_BL": "1",
+"Anz.pos.B_BL": "1",
+"Anz.pos.all_BL": "1",
+"InfA_BS+BL": "1",
+"InfB_BS+BL": "1",
+"7t_median_InfA": "1",
+"7t_median_InfB": "1"}
 
 
 
