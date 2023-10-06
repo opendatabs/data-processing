1034122f4493dfd9f27f629aadb431975f266e74
Orhan Saeedi <orhan.saeedi@bs.ch>
2023-10-06 18:51:00 +0200

    Update kapo_smileys

--- kapo_smileys/etl.py
+++ kapo_smileys/etl.py
@@ -4,6 +4,8 @@ import os
 import shapefile  # library pyshp
 import common
 import pandas as pd
+import pytz
+from datetime import timedelta
 import numpy as np
 from common import change_tracking as ct
 import ods_publish.etl_id as odsp
@@ -23,13 +25,16 @@ def read_shapefile(shp_path):
 
 def parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze):
     any_changes = False
-    messdaten_folders = glob.glob(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'Datenablage', '*'))
+    messdaten_folders = glob.glob(os.path.join(curr_dir, 'data_orig', 'Datenablage', '*'))
     messdaten_dfs = []
     stat_dfs = []
     for folder in messdaten_folders:
-        df_all_pro_standort, df_stat_pro_standort = parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsaetze)
-        messdaten_dfs.append(df_all_pro_standort)
-        stat_dfs.append(df_stat_pro_standort)
+        if any(os.listdir(folder)):
+            df_all_pro_standort, df_stat_pro_standort = parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsaetze)
+            messdaten_dfs.append(df_all_pro_standort)
+            stat_dfs.append(df_stat_pro_standort)
+        else:
+            logging.info(f'No data in folder {folder}...')
 
     all_df = pd.concat(messdaten_dfs)
     export_file_all = os.path.join(curr_dir, 'data', 'all_data.csv')
@@ -50,19 +55,29 @@ def parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze):
     return all_df, stat_df, any_changes
 
 
+def is_dst(datetime, timezone):
+    try:
+        return timezone.localize(datetime).dst() != timedelta(0)
+    except (pytz.NonExistentTimeError, pytz.AmbiguousTimeError):
+        return True
+
+
 def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze):
     logging.info(f'Working through folder {folder}...')
-    tagesdaten_files = glob.glob(os.path.join(folder, '**', '*.TXT'), recursive=True)
-    # tagesdaten_files = glob.glob(os.path.join(CURR_DIR, 'data_orig', 'Smiley_Testdaten', 'Datenablage', '**', '*.TXT'), recursive=True)
+    tagesdaten_files = glob.glob(os.path.join(folder, '*.TXT'), recursive=True)
+    id_standort = int(os.path.basename(folder).split('_')[0])
     messdaten_dfs_pro_standort = []
     for f in tagesdaten_files:
         logging.info(f'Parsing Messdaten File {f}...')
-        path_list = f.split(os.path.sep)
-        id_standort = int(path_list[path_list.index('DATA') - 2].split('_')[0])
         # p = re.compile(r'Datenablage\\\\(?P<idstandort>\d+)_')
-        df = (pd.read_csv(f, sep=' ', names=['Datum', 'Zeit', 'V_Einfahrt', 'dummy', 'V_Ausfahrt'], parse_dates=[['Datum', 'Zeit']], infer_datetime_format=True, keep_date_col=True)
-              .rename(columns={'Datum_Zeit': 'Messung_Timestamp', 'Datum': 'Messung_Datum', 'Zeit': 'Messung_Zeit'})
+        df = (pd.read_csv(f, sep=' ', names=['Datum', 'Zeit', 'V_Einfahrt', 'dummy', 'V_Ausfahrt'])
+              .rename(columns={'Datum': 'Messung_Datum', 'Zeit': 'Messung_Zeit'})
               .drop(columns=['dummy']))
+        df['Messung_Timestamp'] = pd.to_datetime(df.Messung_Datum + 'T' + df.Messung_Zeit, format='%d.%m.%yT%H:%M:%S')
+        # TODO: There are non-existent times. 
+        # Assumption: Does not switch to DST. Have to ask, but until than, add one hour if DST
+        df['is_dst'] = df['Messung_Timestamp'].apply(lambda x: is_dst(x, pytz.timezone('Europe/Zurich')))
+        df.loc[df['is_dst'],'Messung_Timestamp'] =  df['Messung_Timestamp'] + pd.Timedelta(hours=1)
         df.Messung_Timestamp = df.Messung_Timestamp.dt.tz_localize('Europe/Zurich', ambiguous='infer')
         df['id_standort'] = id_standort
         day_str = os.path.basename(f).split('.')[0]
@@ -70,8 +85,8 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
         df['V_Delta'] = df.V_Ausfahrt - df.V_Einfahrt
         # Determining Zyklus and Smiley_Nr of measurement
         df_m1 = pd.merge(df_einsatz_days, df, how='right', on=['id_standort', 'day_str']).drop(columns=['datum_aktiv', 'day_str'])
-        df_m = pd.merge(df_m1, df_einsatze, how='left', left_on=['id_standort', 'Zyklus'], right_on=['id_Standorte', 'Zyklus'])
-        df_m = df_m.drop(columns=['id_Standorte'])
+        df_m = pd.merge(df_m1, df_einsatze, how='left', left_on=['id_standort', 'Zyklus'], right_on=['id_Standort', 'Zyklus'])
+        df_m = df_m.drop(columns=['id_Standort', 'is_dst'])
         df_m['Phase'] = np.where(df_m.Messung_Timestamp < df_m.Start_Vormessung, 'Vor Vormessung',
                                  np.where(df_m.Messung_Timestamp < df_m.Start_Betrieb, 'Vormessung',
                                           np.where(df_m.Messung_Timestamp < df_m.Start_Nachmessung, 'Betrieb',
@@ -79,6 +94,7 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
                                  )
         logging.info(f'Removing measurements with phase "Vor Vormessung"...')
         df_m = df_m[df_m.Phase != 'Vor Vormessung']
+        # TODO: Why do we not remove the measurements with phase "Nach Ende"
 
         messdaten_dfs_pro_standort.append(df_m)
         export_file_single = os.path.join(curr_dir, 'data', f'{day_str}_{id_standort}.csv')
@@ -121,10 +137,13 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
 
 
 def parse_einsatzplaene(curr_dir):
-    einsatzplan_files = glob.glob(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'Einsatzplan', 'Zyklus_*.xlsx'))
+    einsatzplan_files = glob.glob(os.path.join(curr_dir, 'data_orig', 'Einsatzplan', 'Zyklus_*.xlsx'))
     einsatzplan_dfs = []
     for f in einsatzplan_files:
+        # Throws error with openpyxl versions bigger than 3.0.10 since files are filtered
         df = pd.read_excel(f, skiprows=1, parse_dates=[['Datum_VM', 'Uhrzeit_VM'], ['Datum_SB', 'Uhrzeit_SB'], ['Datum_NM', 'Uhrzeit_NM'], ['Datum_Ende', 'Uhrzeit_Ende']])
+        df.replace(['', ' ', 'nan nan'], np.nan, inplace=True)
+        df = df.dropna(subset=['Smiley-Nr.'])
         filename = os.path.basename(f)
         zyklus = int(filename.split('_')[1])
         jahr = int(filename.split('_')[2].split('.')[0])
@@ -133,8 +152,9 @@ def parse_einsatzplaene(curr_dir):
         df = df.rename(columns={'Datum_VM_Uhrzeit_VM': 'Start_Vormessung', 'Datum_SB_Uhrzeit_SB': 'Start_Betrieb', 'Datum_NM_Uhrzeit_NM': 'Start_Nachmessung', 'Datum_Ende_Uhrzeit_Ende': 'Ende'})
         for col in ['Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende']:
             logging.info(f'Localizing timestamp in col {col}...')
-            df[col] = df[col].dt.tz_localize('Europe/Zurich', ambiguous='infer')
-        df = df[['id_Standorte', 'Strassenname', 'Geschwindigkeit', 'Halterung', 'Ort', 'Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende', 'Zyklus', 'Jahr']]
+            df[col] = pd.to_datetime(df[col], format='%Y-%m-%d 00:00:00 %H:%M:%S')
+            df[col] = df[col].dt.tz_localize('Europe/Zurich', ambiguous='infer', nonexistent='NaT')
+        df = df[['id_Standort', 'Strassenname', 'Geschwindigkeit', 'Halterung', 'Ort', 'Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende', 'Zyklus', 'Jahr']]
         df = df.rename(columns={'Ort': 'Ort_Abkuerzung', 'Jahr': 'Messung_Jahr'})
         df['Ort'] = np.where(df.Ort_Abkuerzung == 'BS', 'Basel',
                              np.where(df.Ort_Abkuerzung == 'Bt', 'Bettingen',
@@ -148,23 +168,17 @@ def main():
     curr_dir = os.path.dirname(os.path.realpath(__file__))
     logging.info(f'Parsing Einsatzplaene...')
     df_einsaetze = parse_einsatzplaene(curr_dir)
-
-    shp_coords_df = read_shapefile(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'GIS', 'Layer-Smiley-Standorte', 'Smiley-Standorte.shp'))
-    df_einsaetze = pd.merge(df_einsaetze, shp_coords_df[['idstandort', 'coords']], how='left', left_on='id_Standorte', right_on='idstandort').drop(columns=['idstandort'])
-    # put coords in separate columns
-    df_coords1 = pd.DataFrame(df_einsaetze.coords.to_list(), columns=['coords'])
-    df_coords = pd.DataFrame(df_coords1.coords.to_list(), columns=['lon', 'lat'])
-    df_einsaetze['lon_lv95'] = df_coords.lon
-    df_einsaetze['lat_lv95'] = df_coords.lat
-    df_einsaetze = df_einsaetze.drop(columns=['coords'])
-
+    req = common.requests_get('https://data.bs.ch/api/explore/v2.1/catalog/datasets/100286/exports/json?lang=de&timezone=Europe%2FBerlin')
+    file = req.json()
+    shp_coords_df = pd.DataFrame.from_dict(file)
+    df_einsaetze = pd.merge(df_einsaetze, shp_coords_df[['idstandort', 'geo_point_2d', 'geo_shape']], how='left', left_on='id_Standort', right_on='idstandort').drop(columns=['idstandort'])
     logging.info(f'Creating df_einsatz_days with one row per day and standort_id...')
-    df_einsatz_days = pd.concat([pd.DataFrame({'id_standort': row.id_Standorte, 'Zyklus': row.Zyklus, 'datum_aktiv': pd.date_range(row.Start_Vormessung, row.Ende, freq='D', normalize=True)})  # , 'Start_Vormessung': row.Start_Vormessung, 'Start_Betrieb': row.Start_Betrieb, 'Start_Nachmessung': row.Start_Nachmessung, 'Ende': row.Ende})
+    # TODO: concatenate entries over Zyklen which belong together.
+    # For now delete anything which has a 'np.nan' value in a date.
+    df_einsaetze = df_einsaetze.dropna(subset=['Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende'])
+    df_einsatz_days = pd.concat([pd.DataFrame({'id_standort': row.id_Standort, 'Zyklus': row.Zyklus, 'datum_aktiv': pd.date_range(row.Start_Vormessung, row.Ende, freq='D', normalize=True)})  # , 'Start_Vormessung': row.Start_Vormessung, 'Start_Betrieb': row.Start_Betrieb, 'Start_Nachmessung': row.Start_Nachmessung, 'Ende': row.Ende})
                                  for i, row in df_einsaetze.iterrows()], ignore_index=True)
     df_einsatz_days['day_str'] = df_einsatz_days.datum_aktiv.dt.strftime('%y%m%d')
-
-    shp_images_df = read_shapefile(os.path.join(curr_dir, 'data_orig', 'Smiley_Testdaten', 'GIS', 'Layer-Smiley-Standorte', 'Smiley-Standorte_Start_Ende.shp'))
-
     logging.info(f'Parsing Messdaten...')
     df_all, stat_df, any_changes = parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze)
     if any_changes:
