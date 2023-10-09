73ed8685aa780dcb79da5958acdbac9b42a5dd43
Orhan Saeedi <orhan.saeedi@bs.ch>
2023-10-09 16:57:23 +0200

    Update kapo_smileys

--- kapo_smileys/etl.py
+++ kapo_smileys/etl.py
@@ -55,11 +55,13 @@ def parse_messdaten(curr_dir, df_einsatz_days, df_einsaetze):
     return all_df, stat_df, any_changes
 
 
-def is_dst(datetime, timezone):
+def is_dt(datetime, timezone):
     try:
-        return timezone.localize(datetime).dst() != timedelta(0)
+        return timezone.localize(datetime).dst() == timedelta(0)
     except (pytz.NonExistentTimeError, pytz.AmbiguousTimeError):
         return True
+    except ValueError:
+        return False
 
 
 def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze):
@@ -75,10 +77,12 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
               .drop(columns=['dummy']))
         df['Messung_Timestamp'] = pd.to_datetime(df.Messung_Datum + 'T' + df.Messung_Zeit, format='%d.%m.%yT%H:%M:%S')
         # TODO: There are non-existent times. 
-        # Assumption: Does not switch to DST. Have to ask, but until than, add one hour if DST
-        df['is_dst'] = df['Messung_Timestamp'].apply(lambda x: is_dst(x, pytz.timezone('Europe/Zurich')))
-        df.loc[df['is_dst'],'Messung_Timestamp'] =  df['Messung_Timestamp'] + pd.Timedelta(hours=1)
-        df.Messung_Timestamp = df.Messung_Timestamp.dt.tz_localize('Europe/Zurich', ambiguous='infer')
+        # Assumption: Does not switch to DT. Have to ask, but until than, subtract one hour if DT or non-existent time
+        df['is_dt'] = df['Messung_Timestamp'].apply(lambda x: is_dt(x, pytz.timezone('Europe/Zurich')))
+        df.loc[df['is_dt'], 'Messung_Timestamp'] = df['Messung_Timestamp'] - pd.Timedelta(hours=1)
+        df.Messung_Timestamp = df.Messung_Timestamp.dt.tz_localize('Europe/Zurich', ambiguous='infer', nonexistent=timedelta(hours=-1))
+        df.Messung_Datum = df.Messung_Timestamp.dt.date
+        df.Messung_Zeit = df.Messung_Timestamp.dt.time
         df['id_standort'] = id_standort
         day_str = os.path.basename(f).split('.')[0]
         df['day_str'] = day_str
@@ -86,7 +90,7 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
         # Determining Zyklus and Smiley_Nr of measurement
         df_m1 = pd.merge(df_einsatz_days, df, how='right', on=['id_standort', 'day_str']).drop(columns=['datum_aktiv', 'day_str'])
         df_m = pd.merge(df_m1, df_einsatze, how='left', left_on=['id_standort', 'Zyklus'], right_on=['id_Standort', 'Zyklus'])
-        df_m = df_m.drop(columns=['id_Standort', 'is_dst'])
+        df_m = df_m.drop(columns=['id_Standort', 'is_dt'])
         df_m['Phase'] = np.where(df_m.Messung_Timestamp < df_m.Start_Vormessung, 'Vor Vormessung',
                                  np.where(df_m.Messung_Timestamp < df_m.Start_Betrieb, 'Vormessung',
                                           np.where(df_m.Messung_Timestamp < df_m.Start_Nachmessung, 'Betrieb',
@@ -110,30 +114,38 @@ def parse_single_messdaten_folder(curr_dir, folder, df_einsatz_days, df_einsatze
         raise RuntimeError(f'More than 1 ({df_all_pro_standort.Zyklus.unique()}) Zyklus found in 1 data folder ({folder}!)')
 
     # Calculate statistics for this data folder
-    min_timestamp = df_all_pro_standort.Messung_Timestamp.min()
-    max_timestamp = df_all_pro_standort.Messung_Timestamp.max()
-    messdauer_h = (max_timestamp - min_timestamp) / pd.Timedelta(hours=1)
-    anz_messungen = len(df_all_pro_standort)
-    dtv = anz_messungen / messdauer_h * 24
-    df_all_v = pd.DataFrame(pd.concat([df_all_pro_standort.V_Einfahrt, df_all_pro_standort.V_Ausfahrt], ignore_index=True), columns=['V'])
-    id_standort = int(df_all_pro_standort.id_standort.iloc[0])
-    zyklus = int(df_all_pro_standort.Zyklus.iloc[0])
-    stat_pro_standort = {
-        'idstandort': id_standort,
-        'Zyklus': zyklus,
-        'V_max': max(df_all_v.V),
-        'V_min': min(df_all_v.V),
-        'V_50': np.median(df_all_v.V),
-        'V_85': np.percentile(df_all_v.V, 85),
-        'V_Einfahrt_pct_ueber_limite': (df_all_pro_standort.V_Einfahrt > df_all_pro_standort.Geschwindigkeit).mean() * 100,
-        'V_Ausfahrt_pct_ueber_limite': (df_all_pro_standort.V_Ausfahrt > df_all_pro_standort.Geschwindigkeit).mean() * 100,
-        'Anzahl_Messungen': anz_messungen,
-        'Messdauer_h': messdauer_h,
-        'dtv': dtv,
-        'link_einzelmessungen': f'https://data.bs.ch/explore/dataset/100268/table/?refine.id_standort={id_standort}&refine.zyklus={zyklus}'
-    }
-    df_stat_pro_standort = pd.DataFrame([stat_pro_standort])
-    return df_all_pro_standort, df_stat_pro_standort
+    dfs_stat_pro_standort = []
+    for phase in [['Vormessung'], ['Betrieb'], ['Nachmessung'], ['Vormessung', 'Betrieb', 'Nachmessung']]:
+        df_phase = df_all_pro_standort[df_all_pro_standort['Phase'].isin(phase)]
+        min_timestamp = df_phase.Messung_Timestamp.min()
+        max_timestamp = df_phase.Messung_Timestamp.max()
+        messdauer_h = (max_timestamp - min_timestamp) / pd.Timedelta(hours=1)
+        anz_messungen = len(df_phase)
+        dtv = anz_messungen / messdauer_h * 24
+        df_all_v = pd.DataFrame(pd.concat([df_phase.V_Einfahrt, df_phase.V_Ausfahrt], ignore_index=True), columns=['V'])
+        id_standort = int(df_phase.id_standort.iloc[0])
+        zyklus = int(df_phase.Zyklus.iloc[0])
+        strassenname = df_phase.Strassenname.iloc[0]
+        stat_pro_standort = {
+            'Zyklus': zyklus,
+            'Phase': 'Gesamt' if len(phase) > 1 else phase[0],
+            'idstandort': id_standort,
+            'Strassenname': strassenname,
+            'V_max': max(df_all_v.V),
+            'V_min': min(df_all_v.V),
+            'V_50': np.median(df_all_v.V),
+            'V_85': np.percentile(df_all_v.V, 85),
+            'V_Einfahrt_pct_ueber_limite': (df_phase.V_Einfahrt > df_phase.Geschwindigkeit).mean() * 100,
+            'V_Ausfahrt_pct_ueber_limite': (df_phase.V_Ausfahrt > df_phase.Geschwindigkeit).mean() * 100,
+            'Anzahl_Messungen': anz_messungen,
+            'Messdauer_h': messdauer_h,
+            'dtv': dtv,
+            'link_einzelmessungen': f'https://data.bs.ch/explore/dataset/100268/table/?refine.id_standort={id_standort}&refine.zyklus={zyklus}' if len(phase) > 1 else f'https://data.bs.ch/explore/dataset/100268/table/?refine.id_standort={id_standort}&refine.zyklus={zyklus}&refine.phase={phase}'
+        }
+        df_stat_pro_standort = pd.DataFrame([stat_pro_standort])
+        dfs_stat_pro_standort.append(df_stat_pro_standort)
+    df_stats_pro_standort = pd.concat(dfs_stat_pro_standort)
+    return df_all_pro_standort, df_stats_pro_standort
 
 
 def parse_einsatzplaene(curr_dir):
@@ -153,7 +165,9 @@ def parse_einsatzplaene(curr_dir):
         for col in ['Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende']:
             logging.info(f'Localizing timestamp in col {col}...')
             df[col] = pd.to_datetime(df[col], format='%Y-%m-%d 00:00:00 %H:%M:%S')
-            df[col] = df[col].dt.tz_localize('Europe/Zurich', ambiguous='infer', nonexistent='NaT')
+            df['is_dt'] = df[col].apply(lambda x: is_dt(x, pytz.timezone('Europe/Zurich')))
+            df.loc[df['is_dt'], col] =  df[col] - pd.Timedelta(hours=1)
+            df[col] = df[col].dt.tz_localize('Europe/Zurich', ambiguous='infer', nonexistent='NaT').drop(columns=['is_dt'])
         df = df[['id_Standort', 'Strassenname', 'Geschwindigkeit', 'Halterung', 'Ort', 'Start_Vormessung', 'Start_Betrieb', 'Start_Nachmessung', 'Ende', 'Zyklus', 'Jahr']]
         df = df.rename(columns={'Ort': 'Ort_Abkuerzung', 'Jahr': 'Messung_Jahr'})
         df['Ort'] = np.where(df.Ort_Abkuerzung == 'BS', 'Basel',
