acfc20c7d0b341156aace5c45030fa5314d30617
Orhan Saeedi <orhan.saeedi@bs.ch>
2023-11-08 12:29:43 +0100

    Add openpyxl to read excel-file

--- itbs_klv/etl.py
+++ itbs_klv/etl.py
@@ -35,7 +35,7 @@ def get_leistungen():
     all_leistungen_path = os.path.join(credentials.data_orig_path, 'alle_Leistungen.xlsx')
     open(all_leistungen_path, 'wb').write(req.content)
 
-    df_leist = pd.read_excel(all_leistungen_path)
+    df_leist = pd.read_excel(all_leistungen_path, engine='openpyxl')
     columns_of_interest = ['LeistungId', 'Aktiv', 'Departement', 'Dienststelle',
                            'Weitere Gliederung OE', 'Identifikations Nr.', 'Kantonaler Name',
                            'Empfänger der Leistung', 'Aktivität Leistungserbringer',
@@ -53,7 +53,7 @@ def get_gebuehren():
     all_gebuehren_path = os.path.join(credentials.data_orig_path, 'alle_aktiven_Gebuehren.xlsx')
     open(all_gebuehren_path, 'wb').write(req.content)
 
-    df_geb = pd.read_excel(all_gebuehren_path)
+    df_geb = pd.read_excel(all_gebuehren_path, engine='openpyxl')
     columns_of_interest = ['Diensstelle', 'Gegenstand der Gebühr', 'Rechtliche Grundlage', 
                            'Höhe der Gebühr(en) CHF', 'Benchmark', 'Leistung', 'Departement', 'WeitereGliederungOE']
     return df_geb[columns_of_interest]
