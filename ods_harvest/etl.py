8ea79ef9e940fa357e58c5609a044b507ea9fd67
Orhan Saeedi <orhan.saeedi@bs.ch>
2023-12-27 10:14:10 +0100

    Change from QueryAPIKey to HeaderAPIKey

--- ods_harvest/etl.py
+++ ods_harvest/etl.py
@@ -9,7 +9,8 @@ ods_harvester_ids = sys.argv[1].split(',')
 def wait_for_idle(harvester_id):
     while True:
         print(f'Checking status of harvester "{harvester_id}"...')
-        resp = common.requests_get(url=f'https://basel-stadt.opendatasoft.com/api/management/v2/harvesters/{harvester_id}/', params={'apikey': credentials.api_key})
+        resp = common.requests_get(url=f'https://basel-stadt.opendatasoft.com/api/management/v2/harvesters/{harvester_id}/',
+                                   headers={'Authorization': f'Apikey {credentials.api_key}'})
         handle_http_errors(resp)
         status = resp.json()['status']
         print(f'Harvester "{harvester_id}" is "{status}".')
