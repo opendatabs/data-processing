1ec7c193784dc5298c387782be5ad4d134d97730
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-02-02 14:36:08 +0100

    Reformat file

--- parkendd/etl.py
+++ parkendd/etl.py
@@ -35,10 +35,13 @@ def main():
     normalized['published'] = normalized['last_downloaded']
 
     logging.info(f'Creating lots file and saving as {lots_file_name}...')
-    lots = normalized[['address', 'id',  'lot_type', 'name', 'total', 'last_downloaded', 'last_updated', 'coords.lat', 'coords.lng', 'title', 'id2', 'link', 'published']]
+    lots = normalized[
+        ['address', 'id', 'lot_type', 'name', 'total', 'last_downloaded', 'last_updated', 'coords.lat', 'coords.lng',
+         'title', 'id2', 'link', 'published']]
     lots.to_csv(lots_file_name, index=False)
     if ct.has_changed(lots_file_name):
-        common.upload_ftp(lots_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'parkendd/csv/lots')
+        common.upload_ftp(lots_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                          'parkendd/csv/lots')
         odsp.publish_ods_dataset_by_id('100044')
         ct.update_hash_file(lots_file_name)
 
@@ -47,8 +50,10 @@ def main():
     values = normalized[['published', 'free', 'id', 'id2']]
     values.to_csv(values_file_name, index=False)
     folder = datetime.now().strftime('%Y-%m')
-    common.ensure_ftp_dir(credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'parkendd/csv/values/{folder}')
-    common.upload_ftp(values_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, f'parkendd/csv/values/{folder}')
+    common.ensure_ftp_dir(credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                          f'parkendd/csv/values/{folder}')
+    common.upload_ftp(values_file_name, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                      f'parkendd/csv/values/{folder}')
     odsp.publish_ods_dataset_by_id('100014')
 
     logging.info('Job successful!')
