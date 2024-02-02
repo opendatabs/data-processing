b227655c7d736620a915010706b7dd989a255683
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-02-02 17:16:17 +0100

    Reformat stata_parzellen.etl

--- stata_parzellen/etl.py
+++ stata_parzellen/etl.py
@@ -14,9 +14,15 @@ def main():
     if ct.has_changed(parzellen_data_file):
         logging.info(f'Reading data from 4 datasets...')
         df = pd.read_csv(parzellen_data_file, dtype={'WOV_ID': 'str', 'BEZ_ID': 'str', 'BLO_ID': 'str'})
-        df_wohnviertel = common.pandas_read_csv(StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100042/download/?format=csv').text), sep=';', dtype={'wov_id': 'str'})[['wov_id', 'wov_label', 'wov_name', 'gemeinde_name']]
-        df_bezirk = common.pandas_read_csv(StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100039/download/?format=csv').text), sep=';', dtype={'bez_id': 'str'})[['bez_id', 'bez_label', 'bez_name']]
-        df_block = common.pandas_read_csv(StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100040/download/?format=csv').text), sep=';', dtype={'blo_id': 'str'})[['blo_id', 'blo_label']]
+        df_wohnviertel = common.pandas_read_csv(
+            StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100042/download/?format=csv').text),
+            sep=';', dtype={'wov_id': 'str'})[['wov_id', 'wov_label', 'wov_name', 'gemeinde_name']]
+        df_bezirk = common.pandas_read_csv(
+            StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100039/download/?format=csv').text),
+            sep=';', dtype={'bez_id': 'str'})[['bez_id', 'bez_label', 'bez_name']]
+        df_block = common.pandas_read_csv(
+            StringIO(common.requests_get('https://data.bs.ch/explore/dataset/100040/download/?format=csv').text),
+            sep=';', dtype={'blo_id': 'str'})[['blo_id', 'blo_label']]
         logging.info(f'Merging datasets...')
         df_export = (df.merge(df_wohnviertel, left_on='WOV_ID', right_on='wov_id', how='left')
                      .merge(df_bezirk, left_on='BEZ_ID', right_on='bez_id', how='left')
@@ -25,7 +31,8 @@ def main():
         logging.info(f'Exporting, uploading and updating data in ODS...')
         export_filename = os.path.join(CURR_DIR, 'data', 'Liegenschaften_Parzellen_Names.csv')
         df_export.to_csv(export_filename, index=False)
-        common.upload_ftp(export_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass, 'gva/parzellen')
+        common.upload_ftp(export_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                          'gva/parzellen')
         odsp.publish_ods_dataset_by_id('100202')
         ct.update_hash_file(parzellen_data_file)
     pass
