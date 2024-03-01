8a9175d23b9b465f6f4f1dee81be8d4ad98cbf23
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-03-01 17:15:07 +0100

    Update zri_gesetzessammlung

--- zri_gesetzessammlung/etl.py
+++ zri_gesetzessammlung/etl.py
@@ -13,8 +13,10 @@ def main():
     df_tols.to_csv(path_export, index=False)
     common.update_ftp_and_odsp(path_export, 'zrd_gesetzessammlung', '100354')
 
-    get_recent_changes(process_all=True)
-
+    df_rc = get_recent_changes(process_all=True)
+    path_export = os.path.join(credentials.data_path, '100355_recent_changes.csv')
+    df_rc.to_csv(path_export, index=False)
+    common.update_ftp_and_odsp(path_export, 'zrd_gesetzessammlung', '100355')
 
 
 def get_texts_of_law():
@@ -56,19 +58,44 @@ def get_full_path(df, index, column_name):
 
 
 def get_recent_changes(process_all=False):
+    df_recent_changes = pd.DataFrame()
     r = common.requests_get('http://lexfind/api/fe/de/entities/6/recent-changes')
     r.raise_for_status()
     recent_changes = r.json()
-    df_rc = pd.json_normalize(recent_changes, record_path='recent_changes')
-    common.ods_realtime_push_df(df_rc, credentials.push_url)
+    df = pd.json_normalize(recent_changes, record_path='recent_changes')
+    df = process_recent_changes(df)
+    common.ods_realtime_push_df(df, credentials.push_url)
+    df_recent_changes = df
 
     while True and process_all:
         domain_suffix = recent_changes['next_batch']
+        if domain_suffix is None:
+            break
         r = common.requests_get('http://lexfind' + domain_suffix)
         r.raise_for_status()
         recent_changes = r.json()
-        df_rc = pd.json_normalize(recent_changes, record_path='recent_changes')
-        common.ods_realtime_push_df(df_rc, credentials.push_url)
+        df = pd.json_normalize(recent_changes, record_path='recent_changes')
+        df = process_recent_changes(df)
+        common.ods_realtime_push_df(df, credentials.push_url)
+        df_recent_changes = pd.concat([df_recent_changes, df])
+    return df_recent_changes
+
+
+def process_recent_changes(df):
+    # dta_urls are stored as a list of dictionaries
+    df = df.explode('text_of_law.dta_urls').reset_index()
+    df = pd.concat([df.drop(['text_of_law.dta_urls'], axis=1),
+                    df['text_of_law.dta_urls'].apply(pd.Series).add_prefix('tols_dta_')], axis=1)
+    df = df.explode('text_of_law_version.dtah_urls').reset_index()
+    df = pd.concat([df.drop(['text_of_law_version.dtah_urls'], axis=1),
+                    df['text_of_law_version.dtah_urls'].apply(pd.Series).add_prefix('tolsv_dtah_')], axis=1)
+    df.columns = df.columns.str.replace('.', '_')
+    # Date columns from %d.%m.%Y to %Y-%m-%d (string)
+    date_columns = ['change_date', 'text_of_law_version_info_badge_date',
+                    'text_of_law_version_version_active_since', 'text_of_law_version_family_active_since',
+                    'text_of_law_version_version_inactive_since', 'text_of_law_version_version_found_at']
+    df[date_columns] = df[date_columns].apply(lambda x: pd.to_datetime(x, format='%d.%m.%Y').dt.strftime('%Y-%m-%d'))
+    return df
 
 
 if __name__ == '__main__':
