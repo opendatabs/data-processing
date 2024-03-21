84b200ced632dfd5a230a625c606a9d87e34d2b6
jb3_2 <jonas.bieri@gmail.com>
2024-03-21 18:13:01 +0100

    Add link to Allmendbewilligungen, remove geometry

--- tba_baustellen/etl.py
+++ tba_baustellen/etl.py
@@ -11,7 +11,6 @@ from common import change_tracking as ct
 import pandas as pd
 
 
-
 def main():
     r = common.requests_get(url=credentials.url, auth=HTTPBasicAuth(credentials.user, credentials.pw))
     if len(r.text) == 0:
@@ -19,7 +18,21 @@ def main():
         raise RuntimeError('No data retrieved from API.')
     else:
         df = pd.read_json(r.text)
-        df_export = df[['id', 'projekt_name', 'projekt_beschrieb', 'projekt_info', 'projekt_link', 'datum_von', 'datum_bis', 'geometry']]
+        df_export = df[['id', 'projekt_name', 'projekt_beschrieb', 'projekt_info', 'projekt_link', 'datum_von', 'datum_bis']]
+        df_export.datum_von = pd.to_datetime(df_export['datum_von'], format='%d.%m.%Y', errors='raise').dt.strftime('%Y-%m-%d')
+        df_export.datum_bis = pd.to_datetime(df_export['datum_bis'], format='%d.%m.%Y', errors='raise').dt.strftime('%Y-%m-%d')
+        df_export['allmendbewilligungen'] = "https://data.bs.ch/explore/dataset/100018/table/?refine.belgartbez=Baustelle&q=begehrenid=" + df_export.id.astype(str)
+
+        # data_allm = []
+        # for index, row in df_export.iterrows():
+        #     r2 = common.requests_get(f"https://data.bs.ch/api/explore/v2.1/catalog/datasets/100018/records?where=belgartbez%20like%20%22Baustelle%22%20and%20begehrenid%20%3D%{row.id}&limit=20")
+        #     bew_count = r2.json()['total_count']
+        #     if bew_count == 1:
+        #         data_allm.append([row.id, r2.json()['results'][0]['geo_shape']['geometry']])
+        #     else:
+        #         logging.error(f'Number of results from ODS API Call <> 1: {bew_count}')
+        #         raise RuntimeError(f'Number of results from ODS API Call <> 1: {bew_count}')
+
         curr_dir = os.path.dirname(os.path.realpath(__file__))
         export_filename = f"{curr_dir}/data/baustellen.csv"
         df_export.to_csv(export_filename, index=False)
