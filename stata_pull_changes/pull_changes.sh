0b770d81396c5db9903a2258bd0090b348f1e3c7
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-04-04 18:06:07 +0200

    Set proxy

--- stata_pull_changes/pull_changes.sh
+++ stata_pull_changes/pull_changes.sh
@@ -1,3 +1,4 @@
+/bin/bash /code/data-processing/stata_pull_requests/set_proxy.sh
 git config --global --add safe.directory /code/data-processing
 git config --global --add safe.directory /code/dags-airflow2
 # Assumes both data-processing and dags-airflow2 are in the same directory
