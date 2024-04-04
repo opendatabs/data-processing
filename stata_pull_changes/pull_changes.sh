41abee2cb65c77e1da846235308e6f40a5f0d51b
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-04-04 17:39:12 +0200

    Label it as safe directory

--- stata_pull_changes/pull_changes.sh
+++ stata_pull_changes/pull_changes.sh
@@ -1,3 +1,5 @@
+git config --global --add safe.directory /code/data-processing
+git config --global --add safe.directory /code/dags-airflow2
 # Assumes both data-processing and dags-airflow2 are in the same directory
 cd /code/data-processing
 git pull
