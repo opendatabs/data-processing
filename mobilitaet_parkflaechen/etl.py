d0273a053e537577f1a5db8a57dddb7af9513946
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-04-09 09:21:01 +0200

    Change source

--- mobilitaet_parkflaechen/etl.py
+++ mobilitaet_parkflaechen/etl.py
@@ -1,6 +1,5 @@
 import geopandas as gpd
 import pandas as pd
-import numpy as np
 import ods_publish.etl_id as odsp
 import datetime
 import logging
@@ -8,6 +7,8 @@ import zipfile
 import common
 import os
 import io
+from io import StringIO
+from requests.auth import HTTPBasicAuth
 from mobilitaet_parkflaechen import credentials
 from common import change_tracking as ct
 
@@ -24,49 +25,85 @@ def download_spatial_descriptors(ods_id):
     return gdf.to_crs('EPSG:2056')  # Change to a suitable projected CRS
 
 
-def main():
-    list_path = os.path.join(credentials.data_path, 'list_directories.txt')
-    directories = common.list_directories(credentials.data_orig_path, list_path)
-    if ct.has_changed(list_path):
-        newest_folder = max(directories, key=lambda d: datetime.datetime.strptime(d, '%Y-%m-%d'))
-        logging.info(f'Newest folder is {newest_folder}')
-        path_to_shp = os.path.join(credentials.data_orig_path, newest_folder, 'Parkflaechen_vollstaendig.shp')
-        logging.info(f'Reading {path_to_shp}...')
-        gdf = gpd.read_file(path_to_shp)
-        gdf['AKTUELL'] = np.where(
-            (gdf['GILT_BIS'].isnull()) | (pd.to_datetime(gdf['GILT_BIS']) > datetime.datetime.now()),
-            'Ja', 'Nein')
+def create_diff_files(path_to_file):
+    logging.info('Creating diff files...')
+    # Load last version of the file
+    path_to_last = os.path.join(credentials.data_path, 'parkflaechen_last_version.csv')
+    df_new = pd.read_csv(path_to_file)
+    if os.path.exists(path_to_last):
+        df_last = pd.read_csv(path_to_last)
+        # Find new rows if any
+        new_rows = ct.find_new_rows(df_last, df_new, 'id')
+        if len(new_rows) > 0:
+            path_export = os.path.join(credentials.data_path, f'parkflaechen_new_{datetime.date.today()}.csv')
+            new_rows.to_csv(path_export, index=False)
+            common.upload_ftp(path_export, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                              'mobilitaet/parkflaechen/diff_files')
+        # Find modified rows if any
+        deprecated_rows, updated_rows = ct.find_modified_rows(df_last, df_new, 'id')
+        if len(deprecated_rows) > 0:
+            path_export = os.path.join(credentials.data_path, f'parkflaechen_deprecated_{datetime.date.today()}.csv')
+            deprecated_rows.to_csv(path_export, index=False)
+            common.upload_ftp(path_export, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                              'mobilitaet/parkflaechen/diff_files')
+            path_export = os.path.join(credentials.data_path, f'parkflaechen_updated_{datetime.date.today()}.csv')
+            updated_rows.to_csv(path_export, index=False)
+            common.upload_ftp(path_export, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                              'mobilitaet/parkflaechen/diff_files')
+        # Find deleted rows if any
+        deleted_rows = ct.find_deleted_rows(df_last, df_new, 'id')
+        if len(deleted_rows) > 0:
+            path_export = os.path.join(credentials.data_path, f'parkflaechen_deleted_{datetime.date.today()}.csv')
+            deleted_rows.to_csv(path_export, index=False)
+            common.upload_ftp(path_export, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
+                              'mobilitaet/parkflaechen/diff_files')
+    # Save current version of the as the last version
+    df_new.to_csv(path_to_last, index=False)
 
+
+def main():
+    r = common.requests_get(url=credentials.url, auth=HTTPBasicAuth(credentials.user, credentials.pw))
+    if len(r.text) == 0:
+        logging.error('No data retrieved from API!')
+        raise RuntimeError('No data retrieved from API.')
+    else:
+        data = StringIO(r.text.replace('SRID=4326;', ''))
+        df = pd.read_csv(data, sep=';', index_col=False)
+        # Save df into geopandas dataframe and take column geometry as geometry
+        gdf = gpd.GeoDataFrame(df, geometry=gpd.GeoSeries.from_wkt(df['geometry']))
+        gdf.set_geometry('geometry', inplace=True)
+        gdf = gdf.set_crs('EPSG:4326')
+        gdf = gdf.to_crs('EPSG:2056')  # Change to a suitable projected CRS
         logging.info("Calculate PLZ, Wohnviertel and Wohnbezirk for each parking lot based on centroid...")
         gdf['centroid'] = gdf['geometry'].centroid
         gdf_plz = download_spatial_descriptors('100016')
-        gdf['PLZ'] = gdf['centroid'].apply(lambda x: gdf_plz[gdf_plz.contains(x)]['plz'].values[0])
+        gdf['plz'] = gdf['centroid'].apply(lambda x: gdf_plz[gdf_plz.contains(x)]['plz'].values[0])
         gdf_viertel = download_spatial_descriptors('100042')
-        gdf['WOV_ID'] = gdf['centroid'].apply(lambda x: gdf_viertel[gdf_viertel.contains(x)]['wov_id'].values[0])
-        gdf['WOV_NAME'] = gdf['centroid'].apply(lambda x: gdf_viertel[gdf_viertel.contains(x)]['wov_name'].values[0])
+        gdf['wov_id'] = gdf['centroid'].apply(lambda x: gdf_viertel[gdf_viertel.contains(x)]['wov_id'].values[0])
+        gdf['wov_name'] = gdf['centroid'].apply(lambda x: gdf_viertel[gdf_viertel.contains(x)]['wov_name'].values[0])
         gdf_bezirke = download_spatial_descriptors('100039')
-        gdf['BEZ_ID'] = gdf['centroid'].apply(lambda x: gdf_bezirke[gdf_bezirke.contains(x)]['bez_id'].values[0])
-        gdf['BEZ_NAME'] = gdf['centroid'].apply(lambda x: gdf_bezirke[gdf_bezirke.contains(x)]['bez_name'].values[0])
+        gdf['bez_id'] = gdf['centroid'].apply(lambda x: gdf_bezirke[gdf_bezirke.contains(x)]['bez_id'].values[0])
+        gdf['bez_name'] = gdf['centroid'].apply(lambda x: gdf_bezirke[gdf_bezirke.contains(x)]['bez_name'].values[0])
 
-        gdf['GEO_SHAPE'] = gdf['geometry'].to_crs('EPSG:4326')
-        gdf['CENTROID'] = gdf['centroid'].to_crs('EPSG:4326')
+        gdf['geometry'] = gdf['geometry'].to_crs('EPSG:4326')
+        gdf['centroid'] = gdf['centroid'].to_crs('EPSG:4326')
 
-        columns_of_interest = ['GID', 'GILT_VON', 'GILT_BIS', 'SOBJ_KZ', 'ANZAHL', 'SOPFT_TYP', 'STST_STR', 'TARIF_C1',
-                               'SOPFG_GEB', 'GEBPFLICHT', 'MAXPARKZ', 'KEINL', 'AKTUELL', 'PLZ', 'WOV_ID', 'WOV_NAME',
-                               'BEZ_ID', 'BEZ_NAME']  # , 'GEO_SHAPE', 'CENTROID']
+        columns_of_interest = ['id', 'gilt_von', 'anzahl_parkfelder', 'typ', 'strasse', 'tarif_id', 'tarif_code',
+                               'id_parkuhr', 'mob_nr', 'plz', 'wov_id', 'wov_name', 'bez_id', 'bez_name',
+                               'geometry', 'centroid']
         # Filter on PLZ that start with 40 (Basel)
-        gdf = gdf[gdf['PLZ'].str.startswith('40')]
+        gdf = gdf[gdf['plz'].str.startswith('40')]
         gdf = gdf[columns_of_interest]
 
         path_export = os.path.join(credentials.data_path, 'export', '100329_parkflaechen.csv')
         logging.info(f'Exporting data to {path_export}...')
         gdf.to_csv(path_export, index=False)
         if ct.has_changed(path_export):
+            create_diff_files(path_export)
             common.upload_ftp(path_export, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
                               'mobilitaet/parkflaechen')
             odsp.publish_ods_dataset_by_id('100329')
             ct.update_hash_file(path_export)
-        ct.update_hash_file(list_path)
 
 
 if __name__ == "__main__":
