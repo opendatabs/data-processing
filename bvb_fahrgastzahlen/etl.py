96a383f1199ac854ff22605446ea91e09434a25b
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-05-02 11:32:47 +0200

    Add year filter

--- bvb_fahrgastzahlen/etl.py
+++ bvb_fahrgastzahlen/etl.py
@@ -12,7 +12,7 @@ def main():
     path_export = os.path.join(credentials.path_new, 'BVB_monthly.csv')
     save_the_file(df=df, directory=path_export)
     common.update_ftp_and_odsp(path_export, 'bvb/fahrgastzahlen', '100075')
-    
+
 
 def get_the_new_file(directory, sheet_name):
     # Liste der Excel-Dateien im Ordner
@@ -30,32 +30,34 @@ def get_the_new_file(directory, sheet_name):
         # Überprüfe, ob das aktuelle Datum neuer ist als das bisherige neueste Datum
         if latest_date is None or datetime_obj > latest_date:
             latest_date = datetime_obj
-            latest_file = file      
-    df = pd.read_excel(os.path.join(directory, latest_file),sheet_name=sheet_name)
+            latest_file = file
+    df = pd.read_excel(os.path.join(directory, latest_file), sheet_name=sheet_name)
     return df
 
 
 def transform_the_file(df):
+    # Zeile 2023 löschen
+    df = df[df['Fahrgäste (Einsteiger)'] > 2023]
     # DataFrame umformen (melt)
     value_vars = df.columns[1:]
-    df = df.melt(id_vars ='Fahrgäste (Einsteiger)', value_vars = value_vars )
+    df = df.melt(id_vars='Fahrgäste (Einsteiger)', value_vars=value_vars)
     df.columns = ['Year', 'Month', 'Fahrgäste (Einsteiger)']  # Spaltennamen aktualisieren
 
     month_mapping = {
-    'Januar': '01', 'Februar': '02', 'März': '03', 'April': '04', 'Mai': '05', 'Juni': '06',
-    'Juli': '07', 'August': '08', 'September': '09', 'Oktober': '10', 'November': '11', 'Dezember': '12'}
+        'Januar': '01', 'Februar': '02', 'März': '03', 'April': '04', 'Mai': '05', 'Juni': '06',
+        'Juli': '07', 'August': '08', 'September': '09', 'Oktober': '10', 'November': '11', 'Dezember': '12'}
 
     df['Month'] = df['Month'].map(month_mapping)
 
     df = df.sort_values(["Year", "Month"])
     df.insert(2, 'Granularität', 'Monat')
     df.insert(3, 'Startdatum Kalenderwoche/Monat', df['Year'].astype(str) + '-' + df['Month'] + '-01')
-    
+
     df['Kalenderwoche'] = ''
     df['Datum der Monatswerte'] = df['Year'].astype(str) + '-' + df['Month'] + '-01'
 
     df = df.drop(['Year', 'Month'], axis=1)
-    
+
     # Zeilen mit NaN-Werten löschen
     df = df.dropna()
     return df
@@ -67,8 +69,7 @@ def save_the_file(df, directory):
 
 
 if __name__ == "__main__":
-     logging.basicConfig(level=logging.DEBUG)
-     logging.info(f'Executing {__file__}...')
-     main()
-     logging.info('Job successful!')
-     
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f'Executing {__file__}...')
+    main()
+    logging.info('Job successful!')
