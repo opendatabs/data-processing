7dc8c45fbfe3880e5d926e4f065562624daf8824
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-05-16 15:24:39 +0200

    Remove unnecessary import

--- tba_baustellen/etl.py
+++ tba_baustellen/etl.py
@@ -7,7 +7,7 @@ import pandas as pd
 import geopandas as gpd
 import zipfile
 import io
-from datetime import datetime, timedelta
+from datetime import datetime
 # import matplotlib.pyplot as plt
 
 
