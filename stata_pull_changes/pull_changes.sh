ec464a6265bc8ca2c2fb8c4e431b7837a54105c4
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-07-05 17:31:34 +0200

    Add a directory startercode-generator-bs

--- stata_pull_changes/pull_changes.sh
+++ stata_pull_changes/pull_changes.sh
@@ -1,7 +1,10 @@
 git config --global --add safe.directory /code/data-processing
 git config --global --add safe.directory /code/dags-airflow2
-# Assumes both data-processing and dags-airflow2 are in the same directory
+git config --global --add safe.directory /code/startercode-generator-bs
+# Assumes both data-processing, dags-airflow2 and startercode-generator-bs are in the same directory
 cd /code/data-processing
 git pull
 cd /code/dags-airflow2
+git pull
+cd /code/startercode-generator-bs
 git pull
