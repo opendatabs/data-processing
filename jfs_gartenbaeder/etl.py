762759d3ee61c5def071bcc09a15f25a7d3a76aa
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-08-16 10:27:18 +0200

    Add second dataset

--- jfs_gartenbaeder/etl.py
+++ jfs_gartenbaeder/etl.py
@@ -29,7 +29,7 @@ def main():
             data.append([name, temp, time])
 
     # Creat data Frame
-    df = pd.DataFrame(data, columns=['Name', 'Temperatur', 'Zeitpunkt'])
+    df_aktuell = pd.DataFrame(data, columns=['Name', 'Temperatur', 'Zeitpunkt'])
 
     # List of desired swimming pools
     desired_pools = [
@@ -42,21 +42,37 @@ def main():
         "St. Jakob Familienbad"
     ]
     # Filtering the data frame rows
-    df = df[df['Name'].apply(lambda x: any(pool in x for pool in desired_pools))]
+    df_aktuell = df_aktuell[df_aktuell['Name'].apply(lambda x: any(pool in x for pool in desired_pools))]
     # Extract only the numbers from the 'Temperatur' column
-    df['Temperatur'] = df['Temperatur'].str.extract(r'(\d+)').astype(float)
-    df.loc[7, 'Zeitpunkt'] = df.loc[8, 'Zeitpunkt']
+    df_aktuell['Temperatur'] = df_aktuell['Temperatur'].str.extract(r'(\d+)').astype(float)
+    df_aktuell.loc[7, 'Zeitpunkt'] = df_aktuell.loc[8, 'Zeitpunkt']
     # Apply the function to the 'Zeitpunkt' column
-    df['Zeitpunkt'] = pd.to_datetime(df['Zeitpunkt'].apply(convert_datetime)).dt.tz_localize('Europe/Zurich')
-    path_export = os.path.join(credentials.path_new, '100384_gartenbaeder_tempe.csv')
+    df_aktuell['Zeitpunkt'] = pd.to_datetime(df_aktuell['Zeitpunkt'].apply(convert_datetime)).dt.tz_localize(
+        'Europe/Zurich')
+    df_aktuell = df_aktuell.dropna()
+    df_aktuell['Zeitpunkt_Job'] = pd.to_datetime(datetime.now()).tz_localize('Europe/Zurich')
+    path_export = os.path.join(credentials.path_new, '100388_gartenbaeder_temp_live.csv')
+    df_aktuell.to_csv(path_export, index=False)
+    common.update_ftp_and_odsp(path_export, '/jfs/gartenbaeder', '100388')
+
+    common.download_ftp(['100384_gartenbaeder_temp_alle.csv'], common.credentials.ftp_server,
+                        common.credentials.ftp_user, common.credentials.ftp_pass,
+                        '/jfs/gartenbaeder', credentials.path_new, '')
+    df = pd.read_csv(os.path.join(credentials.path_new, '100384_gartenbaeder_temp_alle.csv'))
+    df = pd.concat([df, df_aktuell])
+    df = df.drop_duplicates()
+    path_export = os.path.join(credentials.path_new, '100384_gartenbaeder_temp_alle.csv')
     df.to_csv(path_export, index=False)
     common.update_ftp_and_odsp(path_export, '/jfs/gartenbaeder', '100384')
 
 
 def convert_datetime(datum_str):
-    datum = datetime.strptime(datum_str[4:-4], "%d.%m.%Y, %H:%M")
-    datum = datum.strftime("%Y-%m-%dT%H:%M:%S")
-    return datum
+    try:
+        datum = datetime.strptime(datum_str[4:-4], "%d.%m.%Y, %H:%M")
+        datum = datum.strftime("%Y-%m-%dT%H:%M:%S")
+        return datum
+    except ValueError:
+        return 'NaT'
 
 
 if __name__ == "__main__":
