01606e0e0e4b5135076a605672fe19c02ef8448a
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-09-23 14:27:12 +0200

    Resolve bug where each event would consistently last one day too long.

--- ed_schulferien/etl.py
+++ ed_schulferien/etl.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+from datetime import timedelta
 
 import vobject
 import csv
@@ -67,11 +68,17 @@ def process_ics_file(file_path: str, csv_writer: csv.writer) -> None:
         name = clean_name(event.summary.value)
         
         start_date = event.dtstart.value
+
         end_date = event.dtend.value
+        if end_date.weekday() != 6:
+            end_date -= timedelta(days=1)
+        
         year = start_date.year
 
-        start_date_str = start_date.strftime('%Y-%m-%d')
-        end_date_str = end_date.strftime('%Y-%m-%d')
+        # Timestamps and 'if end_date.weekday() != 6' are added because of the ODS bug where a timestamp is introduced
+        # when exporting automatically generated ics file. TODO: Can be simplified once this ods-bug is fixed
+        start_date_str = start_date.strftime('%Y-%m-%d 00:00:00')
+        end_date_str = end_date.strftime('%Y-%m-%d 23:59:00')
 
         csv_writer.writerow([year, name, start_date_str, end_date_str])
 
