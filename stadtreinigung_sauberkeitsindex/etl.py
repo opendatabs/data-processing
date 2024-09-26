3fa65d40d7a53c20d00818f702fa864598d2c63a
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-09-26 11:53:34 +0200

    Clean up how datenstand is extracted from filename, and other small cleanup

--- stadtreinigung_sauberkeitsindex/etl.py
+++ stadtreinigung_sauberkeitsindex/etl.py
@@ -15,21 +15,21 @@ def main():
     if len(r.text) == 0:
         logging.error('No data retrieved from API!')
         raise RuntimeError('No data retrieved from API.')
-    else:
-        curr_dir = os.path.dirname(os.path.realpath(__file__))
-        export_filename = f"{curr_dir}/data/data_{datetime.now().strftime('%Y-%m')}.csv"
-        with open(export_filename, 'w') as file:
-            file.write(r.text)
-        df = add_datenstand(export_filename)
-        df.to_csv(export_filename, encoding='cp1252', index=False)
-        common.update_ftp_and_odsp(export_filename, 'stadtreinigung/sauberkeitsindex/roh', '100288')
+    
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    export_filename = f"{curr_dir}/data/data_{datetime.now().strftime('%Y-%m')}.csv"
+    with open(export_filename, 'w') as file:
+        file.write(r.text)
+    df = add_datenstand(export_filename)
+    df.to_csv(export_filename, encoding='cp1252', index=False)
+    common.update_ftp_and_odsp(export_filename, 'stadtreinigung/sauberkeitsindex/roh', '100288')
 
 
 def add_datenstand(path_csv):
     result = from_path(path_csv)
     enc = result.best().encoding
     df = pd.read_csv(path_csv, encoding=enc, sep=';')
-    df['datenstand'] = pd.to_datetime(path_csv.split('/')[-1].split('.')[0].split('_')[1])
+    df['datenstand'] = pd.to_datetime(os.path.basename(path_csv).split('_')[1].split('.')[0])
     return df
 
 
