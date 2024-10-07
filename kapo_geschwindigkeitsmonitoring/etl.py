a30d549e85bb38e61bf2569a20410d8415cc89d4
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-10-07 14:35:05 +0200

    Change to Datum_Zeit

--- kapo_geschwindigkeitsmonitoring/etl.py
+++ kapo_geschwindigkeitsmonitoring/etl.py
@@ -224,7 +224,7 @@ def create_measurements_df(df_meta_raw, df_metadata_per_direction):
 def push_new_rows(df, filename):
     df_old = pd.read_csv(filename)
     df.to_csv(filename, index=False)
-    common.ods_realtime_push_complete_update(df, df_old, id_columns=['Messung-ID', 'Richtung ID', 'Timestamp'],
+    common.ods_realtime_push_complete_update(df, df_old, id_columns=['Messung-ID', 'Richtung ID', 'Datum_Zeit'],
                                              url=credentials.push_url_100097)
 
 
