d673420ca905ceca2c7bc903cb4d11096451ad3e
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-11-27 11:29:39 +0100

    Move fetching parkendd data through API into a separate function, add another function to scrape the info from the parkleitsystem website (not yet fully working), and replace a string concatenation of a path by a os.path.join

--- parkendd/etl.py
+++ parkendd/etl.py
@@ -1,3 +1,4 @@
+import os
 import json
 import logging
 from datetime import datetime
@@ -6,9 +7,9 @@ import pandas as pd
 import common
 from common import change_tracking as ct
 import ods_publish.etl_id as odsp
+from bs4 import BeautifulSoup
 
-
-def main():
+def fetch_data_from_parkendd_api() -> pd.DataFrame:
     api_url = 'https://api.parkendd.de/Basel'
     logging.info(f'Getting latest data from {api_url}...')
     response = common.requests_get(url=api_url)
@@ -21,7 +22,6 @@ def main():
     # resp_file.write(pretty_resp)
     # resp_file.close()
 
-    lots_file_name = f'{credentials.path}csv/lots/parkendd-lots.csv'
     logging.info(f'Processing data...')
     for lot in parsed['lots']:
         lot['last_downloaded'] = parsed['last_downloaded']
@@ -33,6 +33,65 @@ def main():
     normalized['link'] = "https://www.parkleitsystem-basel.ch/parkhaus/" + normalized['id2']
     normalized['description'] = 'Anzahl freie Parkplätze: ' + normalized['free'].astype(str)
     normalized['published'] = normalized['last_downloaded']
+
+    return normalized
+
+
+def scrape_data_from_parkleitsystem() -> pd.DataFrame:
+    url_to_scrape_from = "https://www.parkleitsystem-basel.ch/"
+    logging.info(f'Scraping data from {url_to_scrape_from}...')
+    
+    response = common.requests_get(url_to_scrape_from)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    parking_header = soup.find('h3', string='Freie Parkplätze').parent
+    date_str = str(parking_header.find('p').contents[0]).strip()
+    time_str = parking_header.find('span', class_='stempel_zeit').string.strip()
+
+    timestamp = datetime.strptime(f"{date_str} {time_str}", '%d.%m.%Y %H:%M:%S')
+    formatted_timestamp_last_updated = timestamp.strftime('%Y-%m-%dT%H:%M:%S')
+
+    formatted_timestamp_now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    
+    lots_data = []
+    for section in soup.find_all('section', class_='middle'):
+        for table in section.find_all('table'):
+            for row in table.find_all('tr'):
+                cells = row.find_all('td')
+                if not cells:
+                    continue
+                lot_data = {
+                    'name': row.find('td', class_='parkh_name').get_text(strip=True),
+                    'free': int(row.find('td', class_='parkh_belegung').get_text(strip=True)),
+                    'status': row.find('td', class_='parkh_status').get_text(strip=True),
+                    'last_updated': formatted_timestamp_last_updated,
+                    'last_downloaded': formatted_timestamp_now
+                }
+                lots_data.append(lot_data)
+    
+    normalized_scraped = pd.DataFrame(lots_data)
+    normalized_scraped['title'] = "Parkhaus " + normalized_scraped['name']
+    normalized_scraped['id2'] = normalized_scraped['name'].str.lower().str.replace(' ', '')
+    normalized_scraped['link'] = "https://www.parkleitsystem-basel.ch/parkhaus/" + normalized_scraped['id2']
+    normalized_scraped['description'] = 'Anzahl freie Parkplätze: ' + normalized_scraped['free'].astype(str)
+    normalized_scraped['published'] = normalized_scraped['last_downloaded']
+    
+    # Reorder columns to match the desired order
+    column_order = ['address', 'forecast', 'free', 'id', 'lot_type', 'name', 'state', 'total', 
+                    'last_downloaded', 'last_updated', 'coords.lat', 'coords.lng', 'title', 
+                    'id2', 'link', 'description', 'published']
+    normalized_scraped = normalized_scraped.reindex(columns=column_order)
+    
+    return normalized_scraped
+
+
+def main():
+    #normalized = fetch_data_from_parkendd_api()
+    #print(normalized.head())
+    normalized_scraped = scrape_data_from_parkleitsystem()
+    exit()
+
+    lots_file_name = os.path.join(credentials.path, 'csv', 'lots', 'parkendd-lots.csv')
 
     logging.info(f'Creating lots file and saving as {lots_file_name}...')
     lots = normalized[
