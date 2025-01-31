891bcb28e56dd020e3f9a3d0b31ca3efcbf8edf4
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-01-31 17:55:10 +0100

    Change to realtime push

--- mobilitaet_mikromobilitaet/etl.py
+++ mobilitaet_mikromobilitaet/etl.py
@@ -1,24 +1,22 @@
 import os
 import io
-import common
 import logging
 import pandas as pd
 import geopandas as gpd
 from owslib.wfs import WebFeatureService
+
+import common
 from mobilitaet_mikromobilitaet import credentials
 
 
-# Function to create Map_links
+# Function to create Map_links (unchanged)
 def create_map_links(geometry, p1, p2):
-    # check whether the data is a geo point or geo shape
     logging.info(f'the type of the geometry is {geometry.geom_type}')
-    # geometry_types = gdf.iloc[0][geometry].geom_type
     if geometry.geom_type == 'Polygon':
         centroid = geometry.centroid
     else:
         centroid = geometry
 
-    #  create a Map_links
     lat, lon = centroid.y, centroid.x
     Map_links = f'https://opendatabs.github.io/map-links/?lat={lat}&lon={lon}&p1={p1}&p2={p2}'
     return Map_links
@@ -28,45 +26,92 @@ def main():
     url_wfs = 'https://wfs.geo.bs.ch/'
     wfs = WebFeatureService(url=url_wfs, version='2.0.0', timeout=120)
 
-    shapes_to_load = ['XS_Bolt', 'XS_Carvelo', 'XS_Velospot', 'XS_Voi',
-                      'XS_Bird', 'XS_Lime', 'XS_PickEBike', 'XS_PickEMoped']
+    shapes_to_load = [
+        'XS_Bolt',
+        'XS_Carvelo',
+        'XS_Velospot',
+        'XS_Voi',
+        'XS_Bird',
+        'XS_Lime',
+        'XS_PickEBike',
+        'XS_PickEMoped'
+    ]
 
-    gdf_result = gpd.GeoDataFrame()
+    # 1) Load all new data into gdf_result
+    gdf_current = gpd.GeoDataFrame()
     for shapefile in shapes_to_load:
-        # Retrieve and save the geodata for each layer name in shapes_to_load
         response = wfs.getfeature(typename=shapefile)
         gdf = gpd.read_file(io.BytesIO(response.read()))
-        gdf_result = pd.concat([gdf_result, gdf])
+        gdf_current = pd.concat([gdf_current, gdf])
 
-    logging.info(f"Create Map urls")
-    # Extract params from redirect
+    # 2) Add the 'Map Links'
+    logging.info("Create Map urls")
     tree_groups = 'Geteilte Mikromobilität'
-    tree_group_layers_ = 'Geteilte Mikromobilität=XS_Bird,XS_Bolt,XS_Carvelo,XS_Lime,XS_PickEBike,XS_PickEMoped,XS_Velospot,XS_Voi'
-    gdf_result['Map Links'] = gdf_result.apply(lambda row: create_map_links(row['geometry'], tree_groups, tree_group_layers_), axis=1, result_type='expand')
+    tree_group_layers_ = ('Geteilte Mikromobilität='
+                          'XS_Bird,XS_Bolt,XS_Carvelo,XS_Lime,'
+                          'XS_PickEBike,XS_PickEMoped,XS_Velospot,XS_Voi')
+    gdf_current['Map Links'] = gdf_current.apply(
+        lambda row: create_map_links(row['geometry'], tree_groups, tree_group_layers_),
+        axis=1,
+        result_type='expand'
+    )
 
-    gdf_result = gdf_result.to_crs(epsg=4326)
-    gdf_result['timestamp'] = pd.to_datetime('now').replace(second=0, microsecond=0)
+    # 3) Convert to EPSG:4326 and add a current timestamp localize in Europe/Zurich
+    gdf_current = gdf_current.to_crs(epsg=4326)
+    gdf_current['timestamp'] = pd.to_datetime('now').replace(second=0, microsecond=0).tz_localize('Europe/Zurich')
+    gdf_current = gdf_current.drop(columns='gml_id')
 
-    filename = 'aktuelle_verfuegbarkeit.gpkg'
-    path_export = os.path.join(credentials.data_path, filename)
-    gdf_result.to_file(path_export, driver='GPKG')
-    common.update_ftp_and_odsp(path_export, '/mobilitaet/mikromobilitaet', '100415')
+    # 4) Export the "aktuelle Verfügbarkeit" data into FTP and ODS
+    filename_current = 'aktuelle_verfuegbarkeit.gpkg'
+    path_export_current = os.path.join(credentials.data_path, filename_current)
+    # If file does not exist, gdf_previous will be empty
+    gdf_previous = gpd.read_file(path_export_current)  # Load the previous data for step 5
+    gdf_current.to_file(path_export_current, driver='GPKG')
+    common.update_ftp_and_odsp(path_export_current, '/mobilitaet/mikromobilitaet', '100415')
+    # 4.1) Also save it into archive
+    folder = pd.Timestamp.now().strftime('%Y-%m')
+    common.ensure_ftp_dir(common.credentials.ftp_server,
+                          common.credentials.ftp_user,
+                          common.credentials.ftp_pass,
+                          f'mobilitaet/mikromobilitaet/{folder}')
+    filename_ts = pd.Timestamp.now().strftime('%Y-%m-%d_%H-%M')
+    path_export_archive = os.path.join(credentials.data_path, 'archive', f'{filename_ts}.gpkg')
+    gdf_current.to_file(path_export_archive, driver='GPKG')
+    common.upload_ftp(path_export_archive,
+                      common.credentials.ftp_server,
+                      common.credentials.ftp_user,
+                      common.credentials.ftp_pass,
+                      f'mobilitaet/mikromobilitaet/archiv/{folder}')
+    os.remove(path_export_archive)
 
-    # Read Zeithreihe
-    common.download_ftp(['zeitreihe_verfuegbarkeit.gpkg'], common.credentials.ftp_server,
-                        common.credentials.ftp_user, common.credentials.ftp_pass,
-                        '/mobilitaet/mikromobilitaet', credentials.data_path, '')
-    filename = 'zeitreihe_verfuegbarkeit.gpkg'
-    path_export = os.path.join(credentials.data_path, filename)
-    gdf_zeitreihe = gpd.read_file(path_export)
+    # 5) Compare gdf_previous and gdf_current for geometry changes > 100m
+    # or if the previous geometry is missing (new bike)
+    gdf_previous_2056 = gdf_previous.to_crs(epsg=2056)
+    gdf_current_2056 = gdf_current.to_crs(epsg=2056)
 
-    # Merge the two geodataframes and drop duplicates
-    gdf_merged = pd.concat([gdf_result, gdf_zeitreihe], ignore_index=True)
-    gdf_merged = gdf_merged.drop_duplicates()
+    merged = gdf_current_2056.merge(
+        gdf_previous_2056[['xs_bike_id', 'geometry']],
+        on='xs_bike_id',
+        how='left',
+        suffixes=('_current', '_previous')
+    )
+    merged['distance_m'] = merged.geometry_current.distance(merged.geometry_previous)
+    moved_over_100m = merged[(merged['distance_m'] > 100) | (merged['geometry_previous'].isna())].copy()
+    moved_ids = moved_over_100m['xs_bike_id'].unique()
+    gdf_current_moved = gdf_current[gdf_current['xs_bike_id'].isin(moved_ids)]
+    logging.info(f"Filtered gdf_current down to {len(gdf_current_moved)} records with movement > 100m.")
 
-    # Export the merged geodataframe into path_export
-    gdf_merged.to_file(path_export, driver='GPKG')
-    common.update_ftp_and_odsp(path_export, '/mobilitaet/mikromobilitaet', '100416')
+    # 6) Push the moved data to ODS and FTP
+    common.download_ftp(['zeitreihe_verfuegbarkeit.gpkg'],
+                        common.credentials.ftp_server,
+                        common.credentials.ftp_user,
+                        common.credentials.ftp_pass,
+                        '/jfs/gartenbaeder',
+                        credentials.data_path, '')
+
+    gdf_current_moved['geo_point_2d'] = gdf_current_moved['geometry'].apply(lambda x: x.wkt).str.replace('POINT ', '').str.replace('(', '').str.replace(')', '')
+    df_to_push = gdf_current_moved.drop(columns=['geometry', 'Map Links']).copy()
+    common.ods_realtime_push_df(df_to_push, credentials.push_url)
 
 
 if __name__ == "__main__":
