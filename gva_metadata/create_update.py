3f17260b6f8901a2f7c641913525640c7ac48f60
rstam-aloush <rstam.aloush@bs.ch>
2025-02-03 10:25:40 +0100

    Modify create_update.py

--- gva_metadata/create_update.py
+++ gva_metadata/create_update.py
@@ -3,6 +3,7 @@ import pandas as pd
 from gva_metadata import credentials
 import os
 import logging
+import common
 
 # Base URL of your Open DataSoft instance
 base_url = "https://data.bs.ch/api/automation/v1.0/datasets/"
@@ -15,20 +16,18 @@ headers = {
 }
 
 
-def main():
-    file_name = "gva_metadata.csv"
-    file_path = os.path.join(credentials.file_path, file_name)
-    metadata_df = pd.read_csv(file_path, sep=";", encoding="utf-8")
-    # Reformat date to the correct format
-    if "modified" in metadata_df.columns:
-        metadata_df["modified"] = pd.to_datetime(metadata_df["modified"], format="%d.%m.%Y").dt.strftime("%Y-%m-%d")
+file_name = "gva_metadata.csv"
+file_path = os.path.join(credentials.file_path, file_name)
+metadata_df = pd.read_csv(file_path, sep=";", encoding="utf-8")
+# Reformat date to the correct format
+if "modified" in metadata_df.columns:
+    metadata_df["modified"] = pd.to_datetime(metadata_df["modified"], format="%d.%m.%Y").dt.strftime("%Y-%m-%d")
 
-    # Go through CSV data and process records
-    for _, row in metadata_df.iterrows():
-        dataset_id = row["title"].lower().replace(" ", "_")  # Generate ID from title
-        api_url = f"{base_url}{dataset_id}"
-        # Metadaten-Payload
-        payload = {
+# Go through CSV data and process records
+for _, row in metadata_df.iterrows():
+    dataset_id = row["title"].lower().replace(" ", "_")  # Generate ID from title
+    # Metadaten-Payload
+    payload = {
             "dataset_id": dataset_id,
             "is_restricted": False,
             "metadata": {
@@ -38,27 +37,26 @@ def main():
                     "language": {"value": row["language"]},
                     "attributions": {
                         "value": [row["attributions"]]},
-                    "tags":  {"value": row["tags"].split(";") if pd.notna(row["tags"]) else []},
                     "modified": {"value": row["modified"]},
                 },
                 "custom": {
-                    "tags": {"value": ["Tag1", "Tag"]}
+                    "tags": {"value": row["tags"].split(";") if pd.notna(row["tags"]) else []}
                 }
             },
-        }
+    }
 
-        # Check whether the record exists
-        check_url = f"{get_url}/{dataset_id}"
-        check_response = requests.get(check_url, headers=headers, proxies=credentials.proxies)
-        if check_response.status_code == 200:
-            response = requests.put(api_url, headers=headers, json=payload, proxies=credentials.proxies)
-            logging.info("🔄 Aktualisiert")
-        else:
-            response = requests.post(base_url, headers=headers, json=payload, proxies=credentials.proxies)
-            logging.info("🆕 Erstellt")
+    dataset_uid = common.get_ods_uid_by_id(dataset_id, common.credentials)
+    # Check whether the record exists
+    create_url = f"{base_url}{dataset_id}"
+    check_url = f"{get_url}{dataset_id}"
+    update_url = f"{base_url}{dataset_uid}"
+    check_response = requests.get(check_url, headers=headers, proxies=common.credentials.proxies)
+    if check_response.status_code == 200:
 
-    if __name__ == "__main__":
-        logging.basicConfig(level=logging.DEBUG)
-        logging.info(f'Executing {__file__}...')
-        main()
-        logging.info('🎉 Job successful!')
+        response = requests.put(update_url, headers=headers, json=payload, proxies=common.credentials.proxies)
+    else:
+        response = requests.post(base_url, headers=headers, json=payload, proxies=common.credentials.proxies)
+        print("Datensatz erstellt wurde")
+
+
+
