c9a4360bdb2609d2e784d17f0ca0a1936596125e
Renato Farruggio <renato.farruggio@bluewin.ch>
2025-02-06 16:57:02 +0100

    Decrease hours_after_end from 100 to 10 again

--- parlamentsdienst_gr_abstimmungen/etl.py
+++ parlamentsdienst_gr_abstimmungen/etl.py
@@ -548,7 +548,7 @@ def main():
     # UNCOMMENT to process older poll data
     # poll_dfs.append((handle_polls_json(process_archive=True, df_unique_session_dates=df_unique_session_dates), 'archiv_json'))
 
-    if is_session_now(ical_file_path, hours_before_start=4, hours_after_end=100):
+    if is_session_now(ical_file_path, hours_before_start=4, hours_after_end=10):
         poll_dfs.append(
             (handle_polls_json(process_archive=False, df_unique_session_dates=df_unique_session_dates), 'aktuell_json'))
 
