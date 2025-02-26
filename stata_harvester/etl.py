d7f142c48b6fd500387fc6939eb658016a021a30
osaeedi <orhan.saeedi@bs.ch>
2025-02-26 16:43:25 +0100

    Define path

--- stata_harvester/etl.py
+++ stata_harvester/etl.py
@@ -12,11 +12,12 @@ DATA_ORIG_PATH = os.getenv("DATA_ORIG_PATH")
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info(f'Executing {__file__}...')
-    logging.info(f'Checking if /OpendataSoft_Export_Stata.csv has changed...')
+    logging.info(f'Checking if harvester file has changed...')
     if ct.has_changed(os.path.join(DATA_ORIG_PATH, 'OpendataSoft_Export_Stata.csv')):
         logging.info('File has changed, uploading to FTP...')
         for file in os.listdir(DATA_ORIG_PATH):
-            common.upload_ftp(file, os.getenv("FTP_SERVER"), os.getenv("FTP_USER"), os.getenv("FTP_PASS"), 'harvesters/stata/ftp-csv')
+            common.upload_ftp(os.path.join(DATA_ORIG_PATH, file), os.getenv("FTP_SERVER"), os.getenv("FTP_USER"), os.getenv("FTP_PASS"), 
+                              'harvesters/stata/ftp-csv')
         ct.update_hash_file(os.path.join(DATA_ORIG_PATH, 'OpendataSoft_Export_Stata.csv'))
     else:
         logging.info('File has not changed, skipping upload...')
