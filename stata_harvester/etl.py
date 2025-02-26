0c9811e2b5c223c0db3362342cbc08d27b533530
osaeedi <orhan.saeedi@bs.ch>
2025-02-26 16:39:30 +0100

    Implement FileSensor here

--- stata_harvester/etl.py
+++ stata_harvester/etl.py
@@ -2,13 +2,22 @@ import os
 import logging
 
 import common
+import common.change_tracking as ct
 from dotenv import load_dotenv
 
 load_dotenv()
 
+DATA_ORIG_PATH = os.getenv("DATA_ORIG_PATH")
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info(f'Executing {__file__}...')
-    for file in os.listdir(os.getenv("DATA_ORIG_PATH")):
-        common.upload_ftp(file, os.getenv("FTP_SERVER"), os.getenv("FTP_USER"), os.getenv("FTP_PASS"), 'harvesters/stata/ftp-csv')
+    logging.info(f'Checking if /OpendataSoft_Export_Stata.csv has changed...')
+    if ct.has_changed(os.path.join(DATA_ORIG_PATH, 'OpendataSoft_Export_Stata.csv')):
+        logging.info('File has changed, uploading to FTP...')
+        for file in os.listdir(DATA_ORIG_PATH):
+            common.upload_ftp(file, os.getenv("FTP_SERVER"), os.getenv("FTP_USER"), os.getenv("FTP_PASS"), 'harvesters/stata/ftp-csv')
+        ct.update_hash_file(os.path.join(DATA_ORIG_PATH, 'OpendataSoft_Export_Stata.csv'))
+    else:
+        logging.info('File has not changed, skipping upload...')
     logging.info('Job successful!')
