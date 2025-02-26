d3f2c30045f1e4a5947a2f36d098c6d336e6d604
osaeedi <orhan.saeedi@bs.ch>
2025-02-26 16:47:17 +0100

    Just process last one

--- staka_regierungsratsbeschluesse/etl.py
+++ staka_regierungsratsbeschluesse/etl.py
@@ -197,7 +197,7 @@ def scrape_detail_page(url, expected_date):
 
 
 def main():
-    just_process_last_sitzung = False
+    just_process_last_sitzung = True
 
     all_data = []
     page_number = 1
