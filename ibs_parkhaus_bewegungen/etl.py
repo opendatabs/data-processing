8ef91586aeaf02b33e4dd3c004547e46cea22994
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 16:59:53 +0200

    Refactor ibs_parkhaus_bewegungen

--- ibs_parkhaus_bewegungen/etl.py
+++ ibs_parkhaus_bewegungen/etl.py
@@ -1,45 +1,51 @@
 import logging
 import os
 import pathlib
-import pandas as pd
-import ods_publish.etl_id as odsp
+
 import common
-from ibs_parkhaus_bewegungen import credentials
-from common import change_tracking as ct
+import pandas as pd
 
 
 def main():
     dfs = []
     # Iterate over credentials.data_file_root and read the data files
-    for file in os.listdir(credentials.data_file_root):
-        file_name = os.path.join(credentials.data_file_root, file)
-        logging.info(f'Parsing file {file_name}...')
-        engine = "openpyxl" if file_name.endswith('.xlsx') else "xlrd"
-        df = pd.read_excel(file_name, skiprows=7, usecols='A:D', header=None, engine=engine,
-                           names=['title', 'timestamp_text', 'einfahrten', 'ausfahrten'])
-        df = df.dropna(subset=['ausfahrten'])
-        df['timestamp'] = pd.to_datetime(df.timestamp_text, format='%Y-%m-%d %H:%M:%S').dt.tz_localize(
-            tz='Europe/Zurich', ambiguous=True).dt.tz_convert('UTC')
-        logging.info(f'Adding rows with no data...')
-        df = df.set_index('timestamp').asfreq('1H').reset_index()
-        df['timestamp_text'] = df['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
-        df[['einfahrten', 'ausfahrten']] = df[['einfahrten', 'ausfahrten']].fillna(0)
-        df.title = pd.read_excel(file_name, skiprows=1, usecols='A', header=None, engine=engine).iloc[0, 0]
+    for file in os.listdir("data_orig"):
+        file_name = os.path.join("data_orig", file)
+        logging.info(f"Parsing file {file_name}...")
+        engine = "openpyxl" if file_name.endswith(".xlsx") else "xlrd"
+        df = pd.read_excel(
+            file_name,
+            skiprows=7,
+            usecols="A:D",
+            header=None,
+            engine=engine,
+            names=["title", "timestamp_text", "einfahrten", "ausfahrten"],
+        )
+        df = df.dropna(subset=["ausfahrten"])
+        df["timestamp"] = (
+            pd.to_datetime(df.timestamp_text, format="%Y-%m-%d %H:%M:%S")
+            .dt.tz_localize(tz="Europe/Zurich", ambiguous=True)
+            .dt.tz_convert("UTC")
+        )
+        logging.info("Adding rows with no data...")
+        df = df.set_index("timestamp").asfreq("1H").reset_index()
+        df["timestamp_text"] = df["timestamp"].dt.strftime("%Y-%m-%d %H:%M:%S")
+        df[["einfahrten", "ausfahrten"]] = df[["einfahrten", "ausfahrten"]].fillna(0)
+        df.title = pd.read_excel(
+            file_name, skiprows=1, usecols="A", header=None, engine=engine
+        ).iloc[0, 0]
         dfs.append(df)
     all_df = pd.concat(dfs)
     all_df = all_df.convert_dtypes()
-    export_filename = os.path.join(pathlib.Path(__file__).parent, 'data', 'parkhaus_bewegungen.csv')
+    export_filename = os.path.join(
+        pathlib.Path(__file__).parent, "data", "parkhaus_bewegungen.csv"
+    )
     all_df.to_csv(export_filename, index=False)
-    if ct.has_changed(export_filename):
-        common.upload_ftp(export_filename, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
-                          credentials.ftp_path)
-        odsp.publish_ods_dataset_by_id('100198')
-        ct.update_hash_file(export_filename)
-    pass
+    common.update_ftp_and_odsp(export_filename, "ibs_parkhaus_bewegungen", "100198")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    logging.info(f'Executing {__file__}...')
+    logging.info(f"Executing {__file__}...")
     main()
-    logging.info(f'Job successfully completed!')
+    logging.info("Job successfully completed!")
