fc5614c2e8b4679b398a6a7fe82df76ac6f5ffee
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-25 09:20:59 +0200

    Refactor ods_harvest

--- ods_harvest/dummy_credentials.py
+++
@@ -1,4 +0,0 @@
-api_key = 'dummy_ods_api_key'
-proxy = 'https://USER:PASSWORD@PROXYSERVER:PORT/'
-ods_user = 'USERNAME@BS.CH'
-ods_password = 'PASSWORD'
--- ods_harvest/etl.py
+++ ods_harvest/etl.py
@@ -1,48 +1,55 @@
-from ods_harvest import credentials
-import time
-import common
-
 import sys
-ods_harvester_ids = sys.argv[1].split(',')
+import time
+
+import common
+from common import ODS_API_KEY
+
+ods_harvester_ids = sys.argv[1].split(",")
 
 
 def wait_for_idle(harvester_id):
     while True:
         print(f'Checking status of harvester "{harvester_id}"...')
-        resp = common.requests_get(url=f'https://data.bs.ch/api/automation/v1.0/harvesters/{harvester_id}/',
-                                   headers={'Authorization': f'apikey {credentials.api_key}'})
+        resp = common.requests_get(
+            url=f"https://data.bs.ch/api/automation/v1.0/harvesters/{harvester_id}/",
+            headers={"Authorization": f"apikey {ODS_API_KEY}"},
+        )
         handle_http_errors(resp)
-        status = resp.json()['status']
+        status = resp.json()["status"]
         print(f'Harvester "{harvester_id}" is "{status}".')
-        if status == 'idle':
+        if status == "idle":
             break
         else:
             seconds = 10
-            print(f'Waiting {seconds} seconds before trying again...')
+            print(f"Waiting {seconds} seconds before trying again...")
             time.sleep(seconds)
 
 
 def handle_http_errors(resp):
     if resp.status_code == 200:
-        print(f'ODS command successful: HTTP Code {resp.status_code}')
+        print(f"ODS command successful: HTTP Code {resp.status_code}")
     else:
-        print(f'Problem with ODS command: HTTP Code {resp.status_code}')
-        raise RuntimeError('Problem with OpenDataSoft Management API: ' + resp.text)
+        print(f"Problem with ODS command: HTTP Code {resp.status_code}")
+        raise RuntimeError("Problem with OpenDataSoft Management API: " + resp.text)
 
 
 for harv_id in ods_harvester_ids:
     wait_for_idle(harv_id)
     print(f'Sending harvester "{harv_id}" the "start" signal...')
-    response = common.requests_post(f'https://data.bs.ch/api/automation/v1.0/harvesters/{harv_id}/start/',
-                                   headers={'Authorization': f'apikey {credentials.api_key}'})
+    response = common.requests_post(
+        f"https://data.bs.ch/api/automation/v1.0/harvesters/{harv_id}/start/",
+        headers={"Authorization": f"apikey {ODS_API_KEY}"},
+    )
     handle_http_errors(response)
     wait_for_idle(harv_id)
 
     print(f'Sending harvester "{harv_id}" the "publish" signal...')
-    response = common.requests_post(f'https://data.bs.ch/api/automation/v1.0/harvesters/{harv_id}/publish/',
-                                   headers={'Authorization': f'apikey {credentials.api_key}'})
+    response = common.requests_post(
+        f"https://data.bs.ch/api/automation/v1.0/harvesters/{harv_id}/publish/",
+        headers={"Authorization": f"apikey {ODS_API_KEY}"},
+    )
     handle_http_errors(response)
     wait_for_idle(harv_id)
 
 
-print('Job successful!')
+print("Job successful!")
