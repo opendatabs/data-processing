4fdf1e7800e569337c2c155852d2a5b5b0f80cbd
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-28 07:43:13 +0200

    Refactor code structure for improved readability and maintainability in wiese

--- tba_wiese/etl.py
+++ tba_wiese/etl.py
@@ -1,50 +1,67 @@
 import logging
 import os
 import pathlib
+
 import common
 import pandas as pd
-from tba_wiese import credentials
-from zoneinfo import ZoneInfo
+from common import FTP_PASS, FTP_SERVER, FTP_USER
+
+URL = os.getenv("HTTPS_URL_TBA_WIESE")
+USER = os.getenv("HTTPS_USER_TBA")
+PASS = os.getenv("HTTPS_PASS_TBA")
+ODS_PUSH_URL = os.getenv("ODS_PUSH_URL_100269")
 
 
 def main():
     # Comment out to upload backup
     # upload_backup()
-    r = common.requests_get(url=credentials.url, auth=(credentials.username, credentials.password))
+    r = common.requests_get(url=URL, auth=(USER, PASS))
     data = r.json()
-    df = pd.DataFrame.from_dict([data])[['datum', 'temperatur']]
-    df['timestamp'] = pd.to_datetime(df.datum, format="%Y-%m-%d %H:%M:%S").dt.tz_localize(ZoneInfo('Etc/GMT-1')).dt.tz_convert('UTC')
-    df['timestamp_text'] = df.timestamp.dt.strftime('%Y-%m-%dT%H:%M:%S%z')
-    df_export = df[['timestamp_text', 'temperatur']]
-    common.ods_realtime_push_df(df_export, credentials.ods_push_url)
-    filename = f"{df_export.loc[0].timestamp_text.replace(':', ' - ').replace(' ', '')}.csv"
+    df = pd.DataFrame.from_dict([data])[["datum", "temperatur"]]
+    df["timestamp"] = pd.to_datetime(
+        df.datum, format="%Y-%m-%d %H:%M:%S"
+    ).dt.tz_localize("Europe/Zurich")
+    df["timestamp_text"] = df.timestamp.dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+    df_export = df[["timestamp_text", "temperatur"]]
+    common.ods_realtime_push_df(df_export, ODS_PUSH_URL)
+    filename = (
+        f"{df_export.loc[0].timestamp_text.replace(':', ' - ').replace(' ', '')}.csv"
+    )
     folder = filename[:7]
-    filepath = os.path.join(os.path.dirname(__file__), 'data', filename)
+    filepath = os.path.join(os.path.dirname(__file__), "data", filename)
     df_export.to_csv(filepath, index=False)
-    common.ensure_ftp_dir(credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
-                          f'tba/wiese/temperatur/{folder}')
-    common.update_ftp_and_odsp(filepath, f'tba/wiese/temperatur/{folder}', '100269')
+    common.ensure_ftp_dir(
+        FTP_SERVER, FTP_USER, FTP_PASS, f"tba/wiese/temperatur/{folder}"
+    )
+    common.update_ftp_and_odsp(filepath, f"tba/wiese/temperatur/{folder}", "100269")
     pass
 
 
 def upload_backup():
-    data_path = os.path.join(pathlib.Path(__file__).parent.absolute(), 'data')
+    data_path = os.path.join(pathlib.Path(__file__).parent.absolute(), "data")
     # Iterate over month starting from january 2023 to now with while loop
-    date = pd.Timestamp('2023-01-01')
+    date = pd.Timestamp("2023-01-01")
     while date < pd.Timestamp.now():
-        folder = date.strftime('%Y-%m')
-        list_files = common.download_ftp([], credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
-                                         f'tba/wiese/temperatur/{folder}', data_path, '*.csv')
+        folder = date.strftime("%Y-%m")
+        list_files = common.download_ftp(
+            [],
+            FTP_SERVER,
+            FTP_USER,
+            FTP_PASS,
+            f"tba/wiese/temperatur/{folder}",
+            data_path,
+            "*.csv",
+        )
         for file in list_files:
-            file_path = file['local_file']
+            file_path = file["local_file"]
             df = pd.read_csv(file_path)
-            common.ods_realtime_push_df(df, credentials.ods_push_url)
+            common.ods_realtime_push_df(df, ODS_PUSH_URL)
         date = date + pd.DateOffset(months=1)
     quit()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    logging.info(f'Executing {__file__}...')
+    logging.info(f"Executing {__file__}...")
     main()
-    logging.info('Job successful!')
+    logging.info("Job successful!")
--- tba_wiese/etl_correct_wrong_format.py
+++ tba_wiese/etl_correct_wrong_format.py
@@ -1,31 +1,44 @@
 import logging
 import os
+
 import common
 import pandas as pd
-from tba_wiese import credentials
+from common import FTP_PASS, FTP_SERVER, FTP_USER
+
+ODS_PUSH_URL = os.getenv("ODS_PUSH_URL_100269")
 
 
 def main():
-    data_path = '.\data\\temperatur_wrong_format'
+    data_path = ".\data\\temperatur_wrong_format"
     # Iterate over all files in the folder
     df = pd.DataFrame()
     for file in os.listdir(data_path):
         df = pd.read_csv(os.path.join(data_path, file))
-        df['timestamp_text'] = pd.to_datetime(df.timestamp_text, format="%Y-%d-%mT%H:%M:%S%z").dt.strftime('%Y-%m-%dT%H:%M:%S%z')
-        common.ods_realtime_push_df(df, credentials.ods_push_url)
-        filename = f"{df.loc[0].timestamp_text.replace(':', ' - ').replace(' ', '')}.csv"
+        df["timestamp_text"] = pd.to_datetime(
+            df.timestamp_text, format="%Y-%d-%mT%H:%M:%S%z"
+        ).dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+        common.ods_realtime_push_df(df, ODS_PUSH_URL)
+        filename = (
+            f"{df.loc[0].timestamp_text.replace(':', ' - ').replace(' ', '')}.csv"
+        )
         folder = filename[:7]
-        filepath = os.path.join(os.path.dirname(__file__), 'data', filename)
+        filepath = os.path.join(os.path.dirname(__file__), "data", filename)
         df.to_csv(filepath, index=False)
-        common.ensure_ftp_dir(credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
-                              f'tba/wiese/temperatur/{folder}')
-        common.upload_ftp(filepath, credentials.ftp_server, credentials.ftp_user, credentials.ftp_pass,
-                          f'tba/wiese/temperatur/{folder}')
+        common.ensure_ftp_dir(
+            FTP_SERVER,
+            FTP_USER,
+            FTP_PASS,
+            f"tba/wiese/temperatur/{folder}",
+        )
+        common.upload_ftp(
+            filepath,
+            remote_path=f"tba/wiese/temperatur/{folder}",
+        )
     pass
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    logging.info(f'Executing {__file__}...')
+    logging.info(f"Executing {__file__}...")
     main()
-    logging.info('Job successful!')
+    logging.info("Job successful!")
