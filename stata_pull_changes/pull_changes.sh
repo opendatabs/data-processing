82f56322ce0ed00e9cc2d29b9f75d69da75c5086
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-11 17:19:30 +0200

    Add safe directories and update pull commands
    Added additional safe directories for Git configuration and updated pull commands for new directories.

--- stata_pull_changes/pull_changes.sh
+++ stata_pull_changes/pull_changes.sh
@@ -1,7 +1,18 @@
 git config --global --add safe.directory /code/data-processing
 git config --global --add safe.directory /code/dags-airflow2
+git config --global --add safe.directory /code/rsync
+git config --global --add safe.directory /code/R-data-processing/tourismusdashboard
+git config --global --add safe.directory /code/R-data-processing/stata_konoer
+git config --global --add safe.directory /code/R-data-processing/stata_erwarteter_stromverbrauch
+git config --global --add safe.directory /code/R-data-processing/stata_erwarteter_gasverbrauch
 # Assumes both data-processing and dags-airflow2 are in the same directory
 cd /code/data-processing
 git pull
 cd /code/dags-airflow2
 git pull
+cd /code/rsync
+git pull
+cd /code/R-data-processing/tourismusdashboard
+cd /code/R-data-processing/stata_konoer
+cd /code/R-data-processing/stata_erwarteter_stromverbrauch
+cd /code/R-data-processing/stata_erwarteter_gasverbrauch
