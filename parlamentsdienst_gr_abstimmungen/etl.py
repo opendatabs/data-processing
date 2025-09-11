c0c54b98fb19f24612604c8c71c4a4ccfdff02c0
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-11 09:41:29 +0200

    Adjust session time window for polling data processing to 4 hours before start and 48 hours after end

--- parlamentsdienst_gr_abstimmungen/etl.py
+++ parlamentsdienst_gr_abstimmungen/etl.py
@@ -839,7 +839,7 @@ def main():
     # UNCOMMENT to process older poll data
     # poll_dfs.append((handle_polls_json(process_archive=True, df_unique_session_dates=df_unique_session_dates), 'archiv_json'))
 
-    if utilities.is_session_now(ical_file_path, hours_before_start=48, hours_after_end=10):
+    if utilities.is_session_now(ical_file_path, hours_before_start=4, hours_after_end=48):
         poll_dfs.append(
             (
                 handle_polls_json(
