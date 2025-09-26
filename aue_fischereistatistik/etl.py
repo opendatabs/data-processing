6aabee20f5c882eebd59b6a0807197b5af36aa47
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-26 16:20:14 +0200

    feat: add Python version and pyproject.toml; refactor gewässer.py to load data from ODS API

--- aue_fischereistatistik/etl.py
+++ aue_fischereistatistik/etl.py
@@ -1,10 +1,9 @@
 import locale
 from datetime import datetime
+import logging
 
 import geopandas as gpd
 import pandas as pd
-
-from aue_fischereistatistik import credentials
 
 # When adding data for new year:
 # 1. In the new Excel file, filter out all rows that have 'zurückgesetzt' in the Bemerkungen column
@@ -16,194 +15,201 @@ from aue_fischereistatistik import credentials
 
 # datetime in German
 # MAC:
-locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
+# locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
 # Windows:
-# locale.setlocale(
-#     category=locale.LC_ALL,
-#     locale="German"  # Note: do not use "de_DE" as it doesn't work
-# )
+locale.setlocale(
+    category=locale.LC_ALL,
+    locale="German"  # Note: do not use "de_DE" as it doesn't work
+)
 
-columns = [
-    "Fischereikarte",
-    "Datum",
-    "Monat",
-    "Jahr",
-    "Gewässercode",
-    "Fischart",
-    "Länge",
-    "Kesslergrundel",
-    "Schwarzmundgrundel",
-]
-
-df = pd.DataFrame(columns=columns)
-
-
-for year in range(2010, 2022):
-    year = str(year)
-    path = f"{credentials.path_csv}/fangstatistik_{year}.csv"
-    df_year = pd.read_csv(path, encoding="utf-8", keep_default_na=False)
-    df_year["Jahr"] = year
-    # replace '0' with empty string
-    # Question: In Gewässercode: 0=unbekannt?
-    df_year[["Datum", "Monat", "Fischart", "Länge"]] = df_year[["Datum", "Monat", "Fischart", "Länge"]].replace("0", "")
-
-    # make month column complete/in same format and add day column
-    if (df_year["Monat"] == "").all():
-        # remove empty space from datum column
-        df_year["Datum"] = df_year["Datum"].str.strip()
-        # remove point/komma at end of entries in Datum column if it's there
-        df_year["Datum"] = df_year["Datum"].apply(
-            lambda x: x[:-1] if (x != "" and (x[-1] == "." or x[-1] == ",")) else x
-        )
-        df_year["Monat"] = pd.to_datetime(df_year["Datum"], format="%d.%m", errors="coerce").dt.strftime("%m")
-        # add day column
-        df_year["Tag"] = pd.to_datetime(df_year["Datum"], format="%d.%m", errors="coerce").dt.strftime("%d")
-    else:
-        # Complete month column all in same format
-        # need to correct in month column: 'juli' 'juö' 'ap' '3' 'mai' '0' ''
-        df_year["Monat"].replace("juli", "Juli", inplace=True)
-        df_year["Monat"].replace("ap", "April", inplace=True)
-        df_year["Monat"].replace("mai", "Mai", inplace=True)
-        df_year["Monat"].replace("3", "März", inplace=True)
-        df_year["Monat"].replace("juö", "Juli", inplace=True)
-        # change month names to zero-padded decimal numbers
-        df_year["Monat"] = df_year["Monat"].apply(
-            lambda x: datetime.strptime(x, "%B") if isinstance(x, str) and x != "" else pd.NaT
-        )
-        df_year["Monat"] = df_year["Monat"].dt.strftime("%m")
-        # add day column
-        if year == "2012":
-            df_year["Tag"] = pd.to_datetime(df_year["Datum"], format="%d.%m.", errors="coerce").dt.strftime("%d")
-        else:
-            df_year["Tag"] = df_year["Datum"]
-    df = pd.concat([df, df_year])
-
-# make date column
-cols = ["Jahr", "Monat", "Tag"]
-df["Datum"] = df[cols].apply(lambda x: "-".join(x.values.astype(str)), axis="columns")
-
-# Add year to month column
-df["Monat"] = df["Monat"] + " " + df["Jahr"]
-
-# correct date
-df["Datum"].replace("2020-09-31", "2020-09-30", inplace=True)
-
-# put date column in correct datetime format (thereby removing incomplete dates)
-df["Datum"] = pd.to_datetime(df["Datum"], format="%Y-%m-%d", errors="coerce")
-
-# add column Gewässer
-dict_gew = {
-    "0": "unbekannt",
-    "1": "Rhein - Basel-Stadt",
-    "2": "Rhein - Basel-Stadt",
-    "3": "Wiese - Pachtstrecke Stadt Basel",
-    "4": "Birs - Pachtstrecke Stadt Basel",
-    "5": "Neuer Teich / Mühleteich - Pachtstrecke Riehen",
-    "6": "Wiese - Pachtstrecke Riehen",
-    "7": "Wiese - Pachstrecke Riehen",
-    "8": "unbekannt",
-    "unbekannt": "unbekannt",
-}
-df["Gewässercode"] = df["Gewässercode"].astype("string")
-df["Gewässer"] = df["Gewässercode"].map(dict_gew)
-
-# remove "unbekannt" in column Länge
-df["Länge"].replace("unbekannt", "", inplace=True)
-
-# force some columns to be of integer type
-df["Kesslergrundel"] = pd.to_numeric(df["Kesslergrundel"], errors="coerce", downcast="integer").fillna(0)
-df["Schwarzmundgrundel"] = pd.to_numeric(df["Schwarzmundgrundel"], errors="coerce", downcast="integer").fillna(0)
-
-# make new column with total grundel
-df["Grundel Total"] = df["Kesslergrundel"] + df["Schwarzmundgrundel"]
-
-# filter empty rows: remove all rows that have no entry for Fischart or Grundeln
-condition = ~((df["Fischart"] == "") & (df["Grundel Total"] == 0))
-df = df[condition]
-
-
-# Correct spelling fish names
-df["Fischart"].replace("Bach/Flussforelle", "Bach-/Flussforelle", inplace=True)
-df["Fischart"].replace("Bach-/ Flussforelle", "Bach-/Flussforelle", inplace=True)
-df["Fischart"].replace("Barbe ", "Barbe", inplace=True)
-df["Fischart"].replace("Barsch (Egli)", "Egli", inplace=True)
-df["Fischart"].replace("Aesche", "Äsche", inplace=True)
-df["Fischart"].replace("Barsch", "Egli", inplace=True)
-
-# Remove Nase: they are put back into the water and therefore do not belong in these statistics
-condition = df["Fischart"] != "Nase"
-df = df[condition]
-
-# Rotfeder wieder freigelassen
-condition = df["Fischart"] != "Rotfeder"
-df = df[condition]
-
-# Names Fischereikarte as in the Fischereiverordnung
-df["Fischereikarte"] = df["Fischereikarte"].str.replace(" R$", " Rhein", regex=True)
-df["Fischereikarte"] = df["Fischereikarte"].str.replace(" W$", " Wiese", regex=True)
-df["Fischereikarte"] = df["Fischereikarte"].str.replace(" B$", " Birs", regex=True)
-df["Fischereikarte"] = df["Fischereikarte"].str.replace("Fischerkarte", "Fischereikarte")
-df["Fischereikarte"] = df["Fischereikarte"].str.replace("Jahreskarte", "Fischereikarte")
-
-dict_karten = {
-    "unbekannt": "Fischereikarte Rhein",
-    "Fischereikarte der Gemeinde Riehen": "Fischereikarte Wiese",
-    "Fischereikarte Wiese, Fischereikarte der Gemeinde Riehen": "Fischereikarte Wiese",
-    "Fischereikarte Riehen": "Fischereikarte Wiese",
-    "Galgenkarte": "Galgenkarte Rhein",
-    "Jugendfischerkarte Rhein": "Jugendfischereikarte Rhein",
-    "Jugendfischerkarte": "Jugendfischereikarte Rhein",
-    "Jugendliche Rhein": "Jugendfischereikarte Rhein",
-    "Fischereikarte E": "Fischereikarte Rhein",
-}
-
-df["Fischereikarte"].replace(dict_karten, inplace=True)
-
-
-# deal with case where Gewässer is 'unbekannt':
-# if Fischereikarte Wiese: 'Wiese - Pachtstrecke Riehen'
-# if Galgenkarte/Fischereikarte Rhein: 'Rhein - Basel-Stadt'
-df.loc[
-    ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Fischereikarte Wiese")),
-    "Gewässer",
-] = "Wiese - Pachtstrecke Riehen"
-df.loc[
-    ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Galgenkarte Rhein")),
-    "Gewässer",
-] = "Rhein - Basel-Stadt"
-df.loc[
-    ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Fischereikarte Rhein")),
-    "Gewässer",
-] = "Rhein - Basel-Stadt"
-
-
-# Add index column to keep identical rows in OpenDataSoft
-df = df.sort_values(by=["Jahr", "Monat"])
-df.reset_index(inplace=True)
-df["Laufnummer"] = df.index
-
-
-# filter columns for export
-df = df[
-    [
-        "Jahr",
-        "Monat",
+def main():
+    columns = [
         "Fischereikarte",
-        "Gewässer",
+        "Datum",
+        "Monat",
+        "Jahr",
+        "Gewässercode",
         "Fischart",
         "Länge",
         "Kesslergrundel",
         "Schwarzmundgrundel",
-        "Laufnummer",
     ]
-]
 
-df.to_csv(f"{credentials.base_path_local}/fangstatistik.csv", index=False)
+    df = pd.DataFrame(columns=columns)
 
-# Add geometry
-df_geom = gpd.read_file("gewaesser_adapted.geojson")
 
-gdf = df_geom.merge(df, on="Gewässer")
+    for year in range(2010, 2024):
+        logging.info(f"Processing data for year {year}")
+        year = str(year)
+        path = f"data_orig/fangstatistik_{year}.csv"
+        df_year = pd.read_csv(path, encoding="utf-8", keep_default_na=False)
+        df_year["Jahr"] = year
+        df_year[["Datum", "Monat", "Fischart", "Länge"]] = df_year[["Datum", "Monat", "Fischart", "Länge"]].replace("0", "")
+        df_year[["Datum", "Monat", "Fischart", "Länge"]] = df_year[["Datum", "Monat", "Fischart", "Länge"]].replace(0, "")
 
-# export geojson file
-gdf.to_file(f"{credentials.base_path_local}/fangstatistik.geojson", index=False)
+        # make month column complete/in same format and add day column
+        if (df_year["Monat"] == "").all():
+            # remove empty space from datum column
+            df_year["Datum"] = df_year["Datum"].str.strip()
+            # remove point/komma at end of entries in Datum column if it's there
+            df_year["Datum"] = df_year["Datum"].apply(
+                lambda x: x[:-1] if (x != "" and (x[-1] == "." or x[-1] == ",")) else x
+            )
+            df_year["Monat"] = pd.to_datetime(df_year["Datum"], format="%d.%m", errors="coerce").dt.strftime("%m")
+            # add day column
+            df_year["Tag"] = pd.to_datetime(df_year["Datum"], format="%d.%m", errors="coerce").dt.strftime("%d")
+        else:
+            # Complete month column all in same format
+            # need to correct in month column: 'juli' 'juö' 'ap' '3' 'mai' '0' ''
+            df_year["Monat"] = df_year["Monat"].replace("juli", "Juli")
+            df_year["Monat"] = df_year["Monat"].replace("ap", "April")
+            df_year["Monat"] = df_year["Monat"].replace("mai", "Mai")
+            df_year["Monat"] = df_year["Monat"].replace("3", "März")
+            df_year["Monat"] = df_year["Monat"].replace("juö", "Juli")
+            # change month names to zero-padded decimal numbers
+            df_year["Monat"] = df_year["Monat"].apply(
+                lambda x: datetime.strptime(x, "%B") if isinstance(x, str) and x != "" else pd.NaT
+            )
+            df_year["Monat"] = df_year["Monat"].dt.strftime("%m")
+            # add day column
+            if year == "2012":
+                df_year["Tag"] = pd.to_datetime(df_year["Datum"], format="%d.%m.", errors="coerce").dt.strftime("%d")
+            else:
+                df_year["Tag"] = df_year["Datum"]
+        df = pd.concat([df, df_year])
+    logging.info("Combining data for all years done.")
+
+    # make date column
+    cols = ["Jahr", "Monat", "Tag"]
+    df["Datum"] = df[cols].apply(lambda x: "-".join(x.values.astype(str)), axis="columns")
+
+    # Add year to month column
+    df["Monat"] = df["Monat"] + " " + df["Jahr"]
+
+    # correct date
+    df["Datum"] = df["Datum"].replace("2020-09-31", "2020-09-30")
+
+    # put date column in correct datetime format (thereby removing incomplete dates)
+    df["Datum"] = pd.to_datetime(df["Datum"], format="%Y-%m-%d", errors="coerce")
+
+    # add column Gewässer
+    dict_gew = {
+        "0": "unbekannt",
+        "1": "Rhein - Basel-Stadt",
+        "2": "Rhein - Basel-Stadt",
+        "3": "Wiese - Pachtstrecke Stadt Basel",
+        "4": "Birs - Pachtstrecke Stadt Basel",
+        "5": "Neuer Teich / Mühleteich - Pachtstrecke Riehen",
+        "6": "Wiese - Pachtstrecke Riehen",
+        "7": "Wiese - Pachstrecke Riehen",
+        "8": "unbekannt",
+        "unbekannt": "unbekannt",
+    }
+    df["Gewässercode"] = df["Gewässercode"].astype("string")
+    df["Gewässer"] = df["Gewässercode"].map(dict_gew)
+
+    # remove "unbekannt" in column Länge
+    df["Länge"] = df["Länge"].replace("unbekannt", "")
+
+    # force some columns to be of integer type
+    df["Kesslergrundel"] = pd.to_numeric(df["Kesslergrundel"], errors="coerce", downcast="integer").fillna(0)
+    df["Schwarzmundgrundel"] = pd.to_numeric(df["Schwarzmundgrundel"], errors="coerce", downcast="integer").fillna(0)
+
+    # make new column with total grundel
+    df["Grundel Total"] = df["Kesslergrundel"] + df["Schwarzmundgrundel"]
+
+    # filter empty rows: remove all rows that have no entry for Fischart or Grundeln
+    condition = ~((df["Fischart"] == "") & (df["Grundel Total"] == 0))
+    df = df[condition]
+
+
+    # Correct spelling fish names
+    df["Fischart"] = df["Fischart"].replace("Bach/Flussforelle", "Bach-/Flussforelle")
+    df["Fischart"] = df["Fischart"].replace("Bach-/ Flussforelle", "Bach-/Flussforelle")
+    df["Fischart"] = df["Fischart"].replace("Barbe ", "Barbe")
+    df["Fischart"] = df["Fischart"].replace("Barsch (Egli)", "Egli")
+    df["Fischart"] = df["Fischart"].replace("Aesche", "Äsche")
+    df["Fischart"] = df["Fischart"].replace("Barsch", "Egli")
+
+    # Remove Nase: they are put back into the water and therefore do not belong in these statistics
+    condition = df["Fischart"] != "Nase"
+    df = df[condition]
+
+    # Rotfeder wieder freigelassen
+    condition = df["Fischart"] != "Rotfeder"
+    df = df[condition]
+
+    # Names Fischereikarte as in the Fischereiverordnung
+    df["Fischereikarte"] = df["Fischereikarte"].str.replace(" R$", " Rhein", regex=True)
+    df["Fischereikarte"] = df["Fischereikarte"].str.replace(" W$", " Wiese", regex=True)
+    df["Fischereikarte"] = df["Fischereikarte"].str.replace(" B$", " Birs", regex=True)
+    df["Fischereikarte"] = df["Fischereikarte"].str.replace("Fischerkarte", "Fischereikarte")
+    df["Fischereikarte"] = df["Fischereikarte"].str.replace("Jahreskarte", "Fischereikarte")
+
+    dict_karten = {
+        "unbekannt": "Fischereikarte Rhein",
+        "Fischereikarte der Gemeinde Riehen": "Fischereikarte Wiese",
+        "Fischereikarte Wiese, Fischereikarte der Gemeinde Riehen": "Fischereikarte Wiese",
+        "Fischereikarte Riehen": "Fischereikarte Wiese",
+        "Galgenkarte": "Galgenkarte Rhein",
+        "Jugendfischerkarte Rhein": "Jugendfischereikarte Rhein",
+        "Jugendfischerkarte": "Jugendfischereikarte Rhein",
+        "Jugendliche Rhein": "Jugendfischereikarte Rhein",
+        "Fischereikarte E": "Fischereikarte Rhein",
+    }
+
+    df["Fischereikarte"] = df["Fischereikarte"].replace(dict_karten)
+
+
+    # deal with case where Gewässer is 'unbekannt':
+    # if Fischereikarte Wiese: 'Wiese - Pachtstrecke Riehen'
+    # if Galgenkarte/Fischereikarte Rhein: 'Rhein - Basel-Stadt'
+    df.loc[
+        ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Fischereikarte Wiese")),
+        "Gewässer",
+    ] = "Wiese - Pachtstrecke Riehen"
+    df.loc[
+        ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Galgenkarte Rhein")),
+        "Gewässer",
+    ] = "Rhein - Basel-Stadt"
+    df.loc[
+        ((df["Gewässer"] == "unbekannt") & (df["Fischereikarte"] == "Fischereikarte Rhein")),
+        "Gewässer",
+    ] = "Rhein - Basel-Stadt"
+
+
+    # Add index column to keep identical rows in OpenDataSoft
+    df = df.sort_values(by=["Jahr", "Monat"])
+    df.reset_index(inplace=True)
+    df["Laufnummer"] = df.index
+
+    # filter columns for export
+    df = df[
+        [
+            "Jahr",
+            "Monat",
+            "Fischereikarte",
+            "Gewässer",
+            "Fischart",
+            "Länge",
+            "Kesslergrundel",
+            "Schwarzmundgrundel",
+            "Laufnummer",
+        ]
+    ]
+
+    df.to_csv(f"data/fangstatistik.csv", index=False)
+
+    # Add geometry
+    df_geom = gpd.read_file("data/gewaesser_adapted.geojson")
+
+    gdf = df_geom.merge(df, on="Gewässer")
+
+    # export geojson file
+    gdf.to_file(f"data/fangstatistik.geojson", index=False)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
+    logging.info("Job successful.")
--- aue_fischereistatistik/gewaesser.py
+++ aue_fischereistatistik/gewaesser.py
@@ -2,20 +2,15 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import LineString, MultiLineString, Point
 from shapely.ops import linemerge, nearest_points, split
+import logging
+import common
+from dotenv import load_dotenv
+import os
 
-# Add df_gewaesser
-df_gewaesser = pd.DataFrame()
-df_gewaesser["Gewässer"] = [
-    "Rhein - Basel-Stadt",
-    "Wiese - Pachtstrecke Stadt Basel",
-    "Wiese - Pachtstrecke Riehen",
-    "Birs - Pachtstrecke Stadt Basel",
-    "Neuer Teich / Mühleteich - Pachtstrecke Riehen",
-]
+load_dotenv()
+ODS_API_KEY = os.getenv('ODS_API_KEY')
 
-# Read in relevant coordinates from Gewässernetz Basel
-gdf = gpd.read_file("data/gewaesser/gewaesser.geojson")
-
+CRS = "EPSG:4326"
 
 # function to split line at point closest to a given point
 def split_line_at_point(line, point):
@@ -29,43 +24,71 @@ def split_line_at_point(line, point):
     line2 = splitted_line.geoms[1]
     return line1, line2
 
-
-# 'Riehenteich - Pachtstrecke Riehen': Add Neuer Teich and Mühleteich, cut off at border (7.65363, 47.59551)
-# cut off piece of Neuer Teich that belongs to Wiese-Basel (7.62643, 47.57782)
-line1 = gdf[gdf["gew_name"] == "Neuer Teich"]["geometry"].iloc[0]
-line2 = gdf[gdf["gew_name"] == "Mühleteich"]["geometry"].iloc[0]
-multi_line = MultiLineString([line1, line2])
-merged_line = linemerge(multi_line)
-point_border = Point(7.65363, 47.59551)
-line_from_border, _ = split_line_at_point(merged_line, point_border)
-cutpoint = Point(7.62643, 47.57782)
-part_Wiese_Basel, line_riehenteich = split_line_at_point(line_from_border, cutpoint)
-
-# split up Wiese in Basel and Riehen part, breaking point: 47.57906, 7.62498
-line = gdf[gdf["gew_name"] == "Wiese"]["geometry"].iloc[0]
-point = Point(7.62498, 47.57906)
-Wiese_Basel, Wiese_Riehen = split_line_at_point(line, point)
-# cut of piece of Riehenteich for Wiese_Basel (7.62393, 47.57679)
-riehenteich = gdf[gdf["gew_name"] == "Riehenteich"]["geometry"].iloc[0]
-cutpoint = Point(7.62393, 47.57679)
-_, riehenteich_to_Wiese_Basel = split_line_at_point(riehenteich, cutpoint)
-# add all pieces to Wiese_Basel
-Wildschutzkanal = gdf[gdf["gew_name"] == "Wildschutzkanal"]["geometry"].iloc[0]
-multi_line = MultiLineString([Wiese_Basel, riehenteich_to_Wiese_Basel, part_Wiese_Basel, Wildschutzkanal])
-Wiese_Basel = linemerge(multi_line)
+def get_gewaesser():
+    url = "https://data.bs.ch/explore/dataset/100261/download/"
+    r = common.requests_get(url,headers={'Authorization': f'apikey {ODS_API_KEY}'}, params={
+        "format": "geojson",
+    })
+    r.raise_for_status()
+    gj = r.json()
+    gdf = gpd.GeoDataFrame.from_features(gj["features"], crs=CRS)
+    return gdf
 
 
-# Construct gdf_gewaesser
-geo_gewaesser = gpd.GeoSeries(
-    [
-        gdf[gdf["gew_name"] == "Rhein"]["geometry"].iloc[0],
-        Wiese_Basel,
-        Wiese_Riehen,
-        gdf[gdf["gew_name"] == "Birs"]["geometry"].iloc[0],
-        line_riehenteich,
+def main():
+    # Add df_gewaesser
+    df_gewaesser = pd.DataFrame()
+    df_gewaesser["Gewässer"] = [
+        "Rhein - Basel-Stadt",
+        "Wiese - Pachtstrecke Stadt Basel",
+        "Wiese - Pachtstrecke Riehen",
+        "Birs - Pachtstrecke Stadt Basel",
+        "Neuer Teich / Mühleteich - Pachtstrecke Riehen",
     ]
-)
 
-gdf_gewaesser = gpd.GeoDataFrame(df_gewaesser, geometry=geo_gewaesser)
+    logging.info("Read in Gewässernetz Basel data from ODS")
+    gdf = get_gewaesser()
+    # 'Riehenteich - Pachtstrecke Riehen': Add Neuer Teich and Mühleteich, cut off at border (7.65363, 47.59551)
+    # cut off piece of Neuer Teich that belongs to Wiese-Basel (7.62643, 47.57782)
+    line1 = gdf[gdf["gz_gewaessername"] == "Neuer Teich"]["geometry"].iloc[0]
+    line2 = gdf[gdf["gz_gewaessername"] == "Mühleteich"]["geometry"].iloc[0]
+    multi_line = MultiLineString([line1, line2])
+    merged_line = linemerge(multi_line)
+    point_border = Point(7.65363, 47.59551)
+    line_from_border, _ = split_line_at_point(merged_line, point_border)
+    cutpoint = Point(7.62643, 47.57782)
+    part_Wiese_Basel, line_riehenteich = split_line_at_point(line_from_border, cutpoint)
 
-gdf_gewaesser.to_file("gewaesser_adapted.geojson")
+    # split up Wiese in Basel and Riehen part, breaking point: 47.57906, 7.62498
+    line = gdf[gdf["gz_gewaessername"] == "Wiese"]["geometry"].iloc[0]
+    point = Point(7.62498, 47.57906)
+    Wiese_Basel, Wiese_Riehen = split_line_at_point(line, point)
+    # cut of piece of Riehenteich for Wiese_Basel (7.62393, 47.57679)
+    riehenteich = gdf[gdf["gz_gewaessername"] == "Riehenteich"]["geometry"].iloc[0]
+    cutpoint = Point(7.62393, 47.57679)
+    _, riehenteich_to_Wiese_Basel = split_line_at_point(riehenteich, cutpoint)
+    # add all pieces to Wiese_Basel
+    Wildschutzkanal = gdf[gdf["gz_gewaessername"] == "Wildschutzkanal"]["geometry"].iloc[0]
+    multi_line = MultiLineString([Wiese_Basel, riehenteich_to_Wiese_Basel, part_Wiese_Basel, Wildschutzkanal])
+    Wiese_Basel = linemerge(multi_line)
+
+
+    # Construct gdf_gewaesser
+    geo_gewaesser = gpd.GeoSeries(
+        [
+            gdf[gdf["gz_gewaessername"] == "Rhein"]["geometry"].iloc[0],
+            Wiese_Basel,
+            Wiese_Riehen,
+            gdf[gdf["gz_gewaessername"] == "Birs"]["geometry"].iloc[0],
+            line_riehenteich,
+        ]
+    )
+
+    gdf_gewaesser = gpd.GeoDataFrame(df_gewaesser, geometry=geo_gewaesser)
+
+    gdf_gewaesser.to_file("data/gewaesser_adapted.geojson")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
+    logging.info("Job completed successfully.")
