a9893e127a9cd3a4b0e954be2680937bfc980237
rstam-aloush <rstam.aloush@bs.ch>
2025-11-27 16:12:18 +0100

    feat: refactor ETL process to support multiple models and enhance data handling

--- dcc_dataspot_schemes/etl.py
+++ dcc_dataspot_schemes/etl.py
@@ -2,26 +2,78 @@ import logging
 import os
 import urllib
 from io import BytesIO
-
 import common
 import pandas as pd
 from dataspot_auth import DataspotAuth
 
+
+#models = ['Datenprodukte', 'Fachdaten', 'Referenzdaten', 'Systeme', 'Datenbankobjekte','Kennzahlen', 'Datentypen (technisch)','Datentypen (fachlich)' ]
+models = ['Datenprodukte','Datentypen (fachlich)' ]
+
+# gelöscht {    "Zusatzinformationen": 100438,}
 ods_id = {
-    "Datensaetze": 100433,
-    "Bestandteile": 100434,
-    "Sammlungen": 100435,
-    "Distributionen": 100436,
-    "Verantwortungen": 100437,
-    "Zusatzinformationen": 100438,
-    "Bereitstellungen": 100439,
+    "Fachdaten_Attribute": 100490,
+    "Fachdaten_Beziehungen": 100491,
+    "Fachdaten_Geschaeftsobjekte": 100492,
+    "Fachdaten_Verantwortungen": 100493,
+
+    "Datenbankobjekte_Attribute": 100494,
+    "Datenbankobjekte_Bereitstellungen": 100495,
+    "Datenbankobjekte_Datenobjekte": 100496,
+    "Datenbankobjekte_Zusatzinformationen": 100497,
+
+    "Datentypen_fachlich_Datentypen": 100498,
+    "Datentypen_fachlich_Zusatzinformationen": 100499,
+    "Datentypen_technisch_Datentypen": 100500,
+
+    "Kennzahlen_Verantwortungen": 100501,
+
+    "Referenzdaten_Elemente": 100502,
+    "Referenzdaten_Referenzwerte": 100503,
+    "Referenzdaten_Verantwortungen": 100504,
+
+    "Systeme_Abhaengigkeiten": 100505,
+    "Systeme_Systeme": 100506,
+    "Systeme_Verantwortungen": 100507,
+
+    "Datenprodukte_Datensaetze": 100433,
+    "Datenprodukte_Bestandteile": 100434,
+    "Datenprodukte_Sammlungen": 100435,
+    "Datenprodukte_Distributionen": 100436,
+    "Datenprodukte_Verantwortungen": 100437,
+    "Datenprodukte_Bereitstellungen": 100439,
 }
+
 S_id = pd.Series(data=ods_id)
 
+Sammlungen_seen = False
 
-def download_datennutzungskatalog_excel():
+
+# Funktion zur Normalisierung von Sheet-Namen
+def normalize_name(name: str) -> str:
+    mapping = {
+        "ä": "ae", "Ä": "Ae",
+        "ö": "oe", "Ö": "Oe",
+        "ü": "ue", "Ü": "Ue",
+        "ß": "ss"
+    }
+    for k, v in mapping.items():
+        name = name.replace(k, v)
+    name = (
+        name.replace(" ", "_")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("/", "_")
+            .replace("\\", "_")
+            .replace(":", "_")
+    )
+    return name
+
+
+# Funktion zum Herunterladen der Excel-Datei aus dem Datenkatalog
+def download_datennutzungskatalog_excel(model):
     auth = DataspotAuth()
-    url = "https://datenkatalog.bs.ch/api/prod/schemes/Datenprodukte/download?format=xlsx&language=de&status=PUBLISHED"
+    url = f"https://datenkatalog.bs.ch/api/prod/schemes/{model}/download?format=xlsx&language=de&status=PUBLISHED"
     headers = auth.get_headers()
     response = common.requests_get(url=url, headers=headers)
     return response.content
@@ -34,34 +86,8 @@ def encode_for_url(text):
     return urllib.parse.quote(text.strip())
 
 
-def main():
-    data = download_datennutzungskatalog_excel()
-    excel_data = pd.read_excel(BytesIO(data), sheet_name=None)
-    # Dictionary mit Sheet-Namen als Schlüssel und DataFrames als Werte
-    dataframes = {sheet: pd.DataFrame(data) for sheet, data in excel_data.items()}
-    # Ausgabe der Namen der Sheets
-    print("Folgende Sheets wurden geladen:", list(dataframes.keys()))
-    # Ausgabe der ersten Zeilen jedes Sheets
-    for sheet_name, df in dataframes.items():
-        if sheet_name == "__literals__":
-            continue  # ignorieren
-        df.columns = df.iloc[0]  # Setze die erste Zeile als Header
-        df = df[1:].reset_index(drop=True)  # Entferne die ursprüngliche Header-Zeile
-        if "Erstellt von" in df.columns:
-            df = df.drop(columns=["Erstellt von"])  # Entferne die Spalte "Erstellt von"
-            df.reset_index(drop=True)
-        # Allte Tabellen speichern (Ausser Datensätze und Betandteil)
-        if sheet_name not in ["Datensätze", "Bestandteile"]:
-            save_path = os.path.join("data", f"{S_id[sheet_name]}_{sheet_name}.csv")
-            df.to_csv(save_path, sep=";", index=False)
-            # In FTP Server speichern
-            common.update_ftp_and_odsp(save_path, "dataspot", S_id[sheet_name])
-        else:
-            if sheet_name == "Datensätze":
-                df_data = df
-            else:
-                df_bestand = df
-
+# Funktion zur Vorbereitung und Zusammenführung der Datensätze
+def prepare_and_merge_datasets(df_data, df_bestand):
     # Prüfen, ob "Name" in "Bestandteil von" vorkommt und dann Spalte "Bestandteile" erstellen
     df_data["Bestandteile"] = df_data["Name"].apply(
         lambda name: "https://data.bs.ch/explore/dataset/100434/table/?refine.bestandteil_von=" + encode_for_url(name)
@@ -76,18 +102,68 @@ def main():
     columns = list(df_data.columns)
     # Bestimme die letzte Spalte
     last_column = columns[-1]
-
     # Neue Reihenfolge: Erste Spalte bleibt gleich, dann die letzte Spalte, dann der Rest
     new_order = [columns[0], last_column] + columns[1:-1]
     # Wende die neue Spaltenreihenfolge auf das DataFrame an
     df_data = df_data[new_order]
+    return df_data, df_bestand
 
-    save_path = os.path.join("data", f"{S_id['Datensaetze']}_Datensaetze.csv")
-    df_data.to_csv(save_path, sep=";", index=False)
-    common.update_ftp_and_odsp(save_path, "dataspot", S_id["Datensaetze"], unpublish_first=True)
-    save_path = os.path.join("data", f"{S_id['Bestandteile']}_Bestandteile.csv")
-    df_bestand.to_csv(save_path, sep=";", index=False)
-    common.update_ftp_and_odsp(save_path, "dataspot", S_id["Bestandteile"], unpublish_first=True)
+
+# Funktion zum Speichern der Datenprodukte als CSV-Dateien
+def save_datenprodukte(dataframes, model):
+    global Sammlungen_seen
+    # Ausgabe der Namen der Sheets
+    logging.info("Folgende Sheets wurden geladen:", list(dataframes.keys()))
+    for sheet, df in dataframes.items():
+        sheet = normalize_name(sheet)
+        model = normalize_name(model)
+        if sheet == "__literals__":
+            continue  # ignorieren
+        if sheet == "Sammlungen":
+            if Sammlungen_seen == True:
+                continue # ignorieren
+            Sammlungen_seen = True
+        df.columns = df.iloc[0]  # Setze die erste Zeile als Header
+        df = df[1:].reset_index(drop=True)  # Entferne die ursprüngliche Header-Zeile
+        # Entferne die Spalte "Verantwortlich", falls vorhanden
+        if "Verantwortlich" in df.columns:
+            df = df.drop(columns=["Verantwortlich"])
+        if "Erstellt von" in df.columns:
+            df = df.drop(columns=["Erstellt von"])  # Entferne die Spalte "Erstellt von"
+            df.reset_index(drop=True)
+            # Allte Tabellen speichern (Ausser Datensätze und Betandteil)
+        if sheet not in ["Datensaetze", "Bestandteile"]:
+            save_path = os.path.join("data", f'{S_id[f'{model}_{sheet}']}_{model}_{sheet}.csv')
+            df.to_csv(save_path, sep=";", index=False)
+            # In FTP Server speichern
+            common.update_ftp_and_odsp(save_path, "dataspot", S_id[f'{model}_{sheet}'])
+        else:
+            if sheet == "Datensaetze":
+                df_data = df
+            else:
+                df_bestand = df
+
+    # Speichern der bearbeiteten Datensätze und Bestandteile
+    if "df_data" in locals() and "df_bestand" in locals():
+        df_data,df_bestand = prepare_and_merge_datasets(df_data, df_bestand)
+        save_path = os.path.join("data", f'{S_id[f'{model}_Datensaetze']}_{model}_Datensaetze.csv')
+        df_data.to_csv(save_path, sep=";", index=False)
+        # In FTP Server speichern
+        common.update_ftp_and_odsp(save_path, "dataspot", S_id[f'{model}_Datensaetze'])
+        save_path = os.path.join("data", f'{S_id[f'{model}_Bestandteile']}_{model}_Bestandteile.csv')
+        df_bestand.to_csv(save_path, sep=";", index=False)
+        # In FTP Server speichern
+        common.update_ftp_and_odsp(save_path, "dataspot", S_id[f'{model}_Bestandteile'])
+
+
+def main():
+
+    for model in models: 
+        data = download_datennutzungskatalog_excel(model)
+        excel_data = pd.read_excel(BytesIO(data), sheet_name=None)
+        # Dictionary mit Sheet-Namen als Schlüssel und DataFrames als Werte
+        dataframes = {sheet: pd.DataFrame(data) for sheet, data in excel_data.items()}
+        save_datenprodukte(dataframes, model)
 
 
 if __name__ == "__main__":
